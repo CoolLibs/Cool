91b07309d5a076bc7cddf746866c7e7db7e1eec8
Jules Fouchy <jules.fouchy@ntymail.com>
2020-12-21 20:17:58 +0100

    Move CMake helpers into their own submodule

--- copy_folder_to_the_output_folder.cmake
+++
@@ -1,8 +0,0 @@
-function(copy_folder_to_the_output_folder _FOLDERPATH)
-    add_custom_command(
-        TARGET ${PROJECT_NAME} POST_BUILD
-        COMMAND ${CMAKE_COMMAND} -E copy_directory
-            ${CMAKE_SOURCE_DIR}/${_FOLDERPATH}
-            $<TARGET_FILE_DIR:${PROJECT_NAME}>/${_FOLDERPATH}
-    )
-endfunction()
