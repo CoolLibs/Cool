bb502e50e335132f55a84bc2ff239bb32d32f787
Jules Fouchy <jules.fouchy@ntymail.com>
2020-12-21 23:56:34 +0100

    Import from OpenGLTemplate

---
+++ src/Cool/File/File.cpp
@@ -0,0 +1,85 @@
+#include "Cool/File/File.h"
+
+#include <sys/stat.h>
+#include <fstream>
+#include <streambuf>
+
+namespace Cool::File {
+
+bool Exists(const char* filepath) {
+    struct stat buffer;
+    return (stat(filepath, &buffer) == 0);
+}
+
+std::string AbsolutePath(const char* filepath) {
+    return std::filesystem::absolute(filepath).string();
+}
+
+std::string FileName(const std::string& filepath) {
+    auto pos = filepath.find_last_of("/\\") + 1;
+    return filepath.substr(pos, filepath.size() - pos);
+}
+
+std::string FileNameWithoutExtension(const std::string& filepath) {
+    return WhithoutFileExtension(FileName(filepath));
+}
+
+std::string FileExtension(const std::string& filepath) {
+    auto pos = filepath.find_last_of(".");
+    if (pos < filepath.size())
+        return filepath.substr(pos, filepath.size());
+    else
+        return "";
+}
+
+std::string WhithoutFileExtension(const std::string& filepath) {
+    auto pos = filepath.find_last_of(".");
+    if (pos < filepath.size())
+        return filepath.substr(0, pos);
+    return filepath;
+}
+
+std::string WhithoutFileName(const std::string& filepath) {
+    if (filepath.find_last_of(".") < filepath.size()) { // There is a "." of an extension, so the thing after the last "/" must be a file name
+        auto pos = filepath.find_last_of("/");
+        return filepath.substr(0, pos);
+    }
+    return filepath;
+}
+
+void ToString(const char* filepath, std::string* dst) {
+    // Thanks to https://stackoverflow.com/questions/2602013/read-whole-ascii-file-into-c-stdstring
+    std::ifstream stream(filepath);
+    if (!stream.is_open()) {
+        Log::Warn("[MyFile::ToString] Failed to open file : '{}'", filepath);
+        return;
+    }
+    stream.seekg(0, std::ios::end);
+    dst->reserve(stream.tellg());
+    stream.seekg(0, std::ios::beg);
+    dst->assign(
+        (std::istreambuf_iterator<char>(stream)),
+        std::istreambuf_iterator<char>()
+    );
+    stream.close();
+}
+
+bool CreateFoldersIfDoesntExist(const char* folderPath) {
+    if (!Exists(folderPath)) {
+        try {
+            std::filesystem::create_directories(folderPath);
+            return true;
+        }
+        catch (std::exception e) {
+            Log::Warn(e.what());
+            return false;
+        }
+    }
+    return true;
+}
+
+bool CreateFoldersForFileIfDoesntExist(const char* folderAndFilePath) {
+    return CreateFoldersIfDoesntExist(WhithoutFileName(std::string(folderAndFilePath)).c_str());
+}
+
+} // namespace Cool
---
+++ src/Cool/File/File.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#include <filesystem>
+
+namespace Cool::File {
+
+/// <summary>
+/// Returns true iff filepath corresponds to an existing file or folder
+/// </summary>
+bool Exists(const char* filepath);
+/// <summary>
+/// Converts a relative or absolute path to an absolute path
+/// </summary>
+std::string AbsolutePath(const char* filepath);
+/// <summary>
+/// Returns the name of the file + its extension (removes parent folders)
+/// </summary>
+std::string FileName(const std::string& filepath);
+/// <summary>
+/// Returns the name of the file (removes parent folders and extension)
+/// </summary>
+std::string FileNameWithoutExtension(const std::string& filepath);
+/// <summary>
+/// Returns the extension of the file, preceded by a dot : something like ".png"
+/// </summary>
+std::string FileExtension(const std::string& filepath);
+/// <summary>
+/// Returns the filepath but whithout the extension.
+/// </summary>
+std::string WhithoutFileExtension(const std::string& filepath);
+/// <summary>
+/// Returns the folders in the path, removes the file name if there is one at the end
+/// </summary>
+std::string WhithoutFileName(const std::string& filepath);
+/// <summary>
+/// Loads the content of a file into a std::string. Should be used like this : \c "std::string myString Cool::File::ToString(filepath, &myString)".
+/// We use this ugly API for performance reasons.
+/// </summary>
+/// <param name="filepath">Path to the file to be loaded</param>
+/// <param name="dst">Pointer to the string to be filled with the content of the file</param>
+void ToString(const char* filepath, std::string* dst);
+/// <summary>
+/// Recursively creates all the folders so that at the end folderPath is a valid folder path. /!\ There shouldn't be any file name at the end of folderPath ; it there is, then use CreateFoldersForFileIfDoesntExist() instead.
+/// Returns true iff the folders now exist (be it because they already existed or because they have been successfully created)
+/// </summary>
+bool CreateFoldersIfDoesntExist(const char* folderPath);
+/// <summary>
+/// Recursively creates all the folders so that at the end folderAndFilePath is a valid file path.
+/// Returns true iff the folders now exist (be it because they already existed or because they have been successfully created)
+/// </summary>
+bool CreateFoldersForFileIfDoesntExist(const char* folderAndFilePath);
+/// <summary>
+/// Root directory where the program was started from.
+/// Use this to describe your ressources by an absolute path, especially if you play at runtime with the directories and your relative paths get messed up.
+/// </summary>
+const std::string RootDir = std::filesystem::current_path().string();
+
+} // namespace Cool::File
