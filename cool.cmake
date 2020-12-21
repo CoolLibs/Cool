72306a4608e40e501a682225ae9707bbe428da65
Jules Fouchy <jules.fouchy@ntymail.com>
2020-12-21 02:53:31 +0100

    Cleanup Cmake file

--- cool.cmake
+++ cool.cmake
@@ -1,18 +1,21 @@
 # NB : You need to set PATH_TO_COOL before including this file
 
+# Include OpenGL
 find_package(OpenGL REQUIRED)
 
-# On windows
+# Include SDL
 if (WIN32) 
     set(SDL2_INCLUDE_DIRS ${PATH_TO_COOL}/lib/SDL2-2.0.10/include)
     set(SDL2_LIBRARIES ${PATH_TO_COOL}/lib/SDL2-2.0.10/lib/x64/SDL2.lib;
                        ${PATH_TO_COOL}/lib/SDL2-2.0.10/lib/x64/SDL2main.lib)
-# On linux
 else()
     find_package(SDL2 REQUIRED)
 endif()
 
+# Include glad
 add_library(GLAD STATIC ${PATH_TO_COOL}/lib/glad/src/glad.c)
+
+# Include DearImGui
 file(GLOB_RECURSE IMGUI_SOURCES ${PATH_TO_COOL}/lib/imgui/*)
 add_library(IMGUI STATIC ${IMGUI_SOURCES})
 
@@ -21,37 +24,36 @@ include_directories(
     ${SDL2_INCLUDE_DIRS}
     ${PATH_TO_COOL}/lib/glad/include
     ${PATH_TO_COOL}/lib/imgui
-    ${PATH_TO_COOL}/lib
     ${PATH_TO_COOL}/lib/glm
     ${PATH_TO_COOL}/lib/spdlog/include
+    ${PATH_TO_COOL}/lib
     ${PATH_TO_COOL}/src
 )
 
-target_link_libraries(
-    ${PROJECT_NAME}
-    ${OPENGL_LIBRARIES}
-    ${SDL2_LIBRARIES}
-    GLAD
-    IMGUI
+# Add a post build operation to copy IMGUI.ini to the output folder (where the executable is created)
+add_custom_command(
+    TARGET ${PROJECT_NAME} POST_BUILD
+    COMMAND ${CMAKE_COMMAND} -E copy
+        ${CMAKE_SOURCE_DIR}/imgui.ini
+        $<TARGET_FILE_DIR:${PROJECT_NAME}>/imgui.ini
 )
 
-if (UNIX)
-    target_link_libraries(${PROJECT_NAME} -ldl)
-endif()
-
-if (WIN32) # Copy .dll to output folder
+# Add a post build operation to copy the SDL dll to the output folder (where the executable is created)
+if (WIN32)
     add_custom_command(
         TARGET ${PROJECT_NAME} POST_BUILD
         COMMAND ${CMAKE_COMMAND} -E copy_if_different
         "${PATH_TO_COOL}/lib/SDL2-2.0.10/lib/x64/SDL2.dll"
         $<TARGET_FILE_DIR:${PROJECT_NAME}>
     )
+else()
+    target_link_libraries(${PROJECT_NAME} -ldl)
 endif()
 
-# Copy imgui.ini to output folder
-add_custom_command(
-    TARGET ${PROJECT_NAME} POST_BUILD
-    COMMAND ${CMAKE_COMMAND} -E copy
-        ${CMAKE_SOURCE_DIR}/imgui.ini
-        $<TARGET_FILE_DIR:${PROJECT_NAME}>/imgui.ini
+# Add libraries to the project
+target_link_libraries(${PROJECT_NAME}
+    ${OPENGL_LIBRARIES}
+    ${SDL2_LIBRARIES}
+    GLAD
+    IMGUI
 )
