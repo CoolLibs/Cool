b33f425961e250f12d3f1eadc16ba1be1ecaf64f
Jules Fouchy <jules.fouchy@ntymail.com>
2020-12-21 02:22:23 +0100

    CMake : merge "before" and "after" into one file actually

--- cool.cmake
+++ cool.cmake
@@ -1,4 +1,5 @@
-# You need to set PATH_TO_COOL
+# NB : You need to set PATH_TO_COOL before including this file
+
 find_package(OpenGL REQUIRED)
 
 # On windows
@@ -24,4 +25,33 @@ include_directories(
     ${PATH_TO_COOL}/lib/glm
     ${PATH_TO_COOL}/lib/spdlog/include
     ${PATH_TO_COOL}/src
+)
+
+target_link_libraries(
+    ${PROJECT_NAME}
+    ${OPENGL_LIBRARIES}
+    ${SDL2_LIBRARIES}
+    GLAD
+    IMGUI
+)
+
+if (UNIX)
+    target_link_libraries(${PROJECT_NAME} -ldl)
+endif()
+
+if (WIN32) # Copy .dll to output folder
+    add_custom_command(
+        TARGET ${PROJECT_NAME} POST_BUILD
+        COMMAND ${CMAKE_COMMAND} -E copy_if_different
+        "${PATH_TO_COOL}/lib/SDL2-2.0.10/lib/x64/SDL2.dll"
+        $<TARGET_FILE_DIR:${PROJECT_NAME}>
+    )
+endif()
+
+# Copy imgui.ini to output folder
+add_custom_command(
+    TARGET ${PROJECT_NAME} POST_BUILD
+    COMMAND ${CMAKE_COMMAND} -E copy
+        ${CMAKE_SOURCE_DIR}/imgui.ini
+        $<TARGET_FILE_DIR:${PROJECT_NAME}>/imgui.ini
 )
