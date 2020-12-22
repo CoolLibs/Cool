4a39934b92096231cf89fa49a62dd275c6c6ed48
Jules Fouchy <jules.fouchy@ntymail.com>
2020-12-22 14:36:19 +0100

    GLCall : add documenting comment

--- src/Cool/App/internal/GLCall.h
+++ src/Cool/App/internal/GLCall.h
@@ -3,6 +3,8 @@
 #ifndef NDEBUG
 
 namespace Cool {
+    /// Wrap all you OpenGL calls with this macro : it will add debug checks.
+    /// It is not strictly necessary though because modern OpenGL debugging is enabled too. It's only to help those who don't have the advandced debugging available on their machine.
     #define GLCall(x) CoolGlDebug::clearFromPreviousErrors(); x; assert(!CoolGlDebug::checkForErrors(#x, __FILE__, __LINE__))
 }
 
