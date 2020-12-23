19a6d3378f73ac199369fb0e0b20ed3b8f52d817
Jules Fouchy <jules.fouchy@ntymail.com>
2020-12-23 19:06:07 +0100

    Adapt translation and rotation speed now that MouseInCentimeters is actually in centimeters

--- src/Cool/Camera/internal/CameraState_Rotation.cpp
+++ src/Cool/Camera/internal/CameraState_Rotation.cpp
@@ -13,7 +13,7 @@ CameraState_Rotation::CameraState_Rotation(Camera& camera)
 {}
 
 void CameraState_Rotation::update() {
-	glm::vec2 delta = (Input::MouseInCentimeters() - m_initialMousePos) * 0.0001f;
+	glm::vec2 delta = (Input::MouseInCentimeters() - m_initialMousePos) * 0.3f;
 	m_camera.m_angleGround = m_initialAngleGround + delta.x;
 	m_camera.m_angleUp     = m_initialAngleUp     + delta.y;
 	m_camera.onTransformChanged();
--- src/Cool/Camera/internal/CameraState_Translation.cpp
+++ src/Cool/Camera/internal/CameraState_Translation.cpp
@@ -12,7 +12,7 @@ CameraState_Translation::CameraState_Translation(Camera& camera)
 {}
 
 void CameraState_Translation::update() {
-	glm::vec2 delta = (m_initialMousePos - Input::MouseInCentimeters()) * 0.0001f;
+	glm::vec2 delta = (m_initialMousePos - Input::MouseInCentimeters()) * 0.3f;
 	m_camera.m_lookAt = m_initialLookAt + m_camera.xAxis() * delta.x - m_camera.yAxis() * delta.y;
 	m_camera.onTransformChanged();
 }
