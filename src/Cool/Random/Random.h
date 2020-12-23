50b0a81881e6e046aa8f8d459948d6651fe67fae
Jules Fouchy <jules.fouchy@ntymail.com>
2020-12-23 19:23:28 +0100

    Import Code

---
+++ src/Cool/Random/Random.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <random>
+
+namespace Cool {
+
+class Random {
+public:
+	/// <summary>
+	/// Returns a random float between 0 and 1
+	/// </summary>
+	static float get0to1();
+	/// <summary>
+	/// Returns a random float between -1 and 1
+	/// </summary>
+	static float getMinus1to1();
+
+private:
+	static std::random_device trueRandomGenerator;
+	static std::default_random_engine generator;
+	static std::uniform_real_distribution<float> distribution_0to1;
+	static std::uniform_real_distribution<float> distribution_minus1to1;
+
+	MyRand() = delete;
+	~MyRand() = delete;
+};
+
+} // namespace Cool
