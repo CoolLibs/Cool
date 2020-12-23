0e63c4a0139f0e122b107721362776ada9d071aa
Jules Fouchy <jules.fouchy@ntymail.com>
2020-12-23 17:27:15 +0100

    Cleanup

--- src/Cool/Log/Log.h
+++ src/Cool/Log/Log.h
@@ -4,7 +4,6 @@
 
 namespace Cool::Log {
 	/// <summary>
-	/// 
 	/// </summary>
 	/// <param name="spdlogPattern">: Sets the log messages pattern ; see here to configure it : https://github.com/gabime/spdlog/wiki/3.-Custom-formatting#pattern-flags</param>
 	inline void Initialize(const char* spdlogPattern = "%^[%e] %n: %v%$") {
