ab421e4659c3fb3c66f090fcc61126f09c6a03d9
Jules Fouchy <jules.fouchy@ntymail.com>
2020-12-23 16:05:49 +0100

    Move fullscreen.vert shader here

--- IncludeMe.cmake
+++ IncludeMe.cmake
@@ -2,3 +2,11 @@
 include_directories(
     ${PATH_TO_COOL}/Renderer_Fullscreen/src
 )
+
+# Add a post build operation to copy fullscreen.vert to the output folder (where the executable is created)
+add_custom_command(
+    TARGET ${PROJECT_NAME} POST_BUILD
+    COMMAND ${CMAKE_COMMAND} -E copy
+        ${PATH_TO_COOL}/Renderer_Fullscreen/fullscreen.vert
+        $<TARGET_FILE_DIR:${PROJECT_NAME}>/Cool/Renderer_Fullscreen/fullscreen.vert
+)
