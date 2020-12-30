608e922fb4914f4e5b9c1d00c4da86ff0a1363e1
Jules Fouchy <jules.fouchy@ntymail.com>
2020-12-30 22:26:32 +0100

    Add <functional> to the cmake

--- IncludeMe.cmake
+++ IncludeMe.cmake
@@ -2,3 +2,8 @@
 include_directories(
     ${PATH_TO_COOL}/FileWatcher/src
 )
+
+# More infos on precompiled headers : https://www.youtube.com/watch?v=eSI4wctZUto&ab_channel=TheCherno
+target_precompile_headers(${PROJECT_NAME} PRIVATE
+    <functional>
+)
