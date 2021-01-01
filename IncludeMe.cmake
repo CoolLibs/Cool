ec2071f03e4192e83ffc1106f2e9696c655616e1
Jules Fouchy <jules.fouchy@ntymail.com>
2021-01-01 22:14:29 +0100

    Import code

---
+++ IncludeMe.cmake
@@ -0,0 +1,6 @@
+
+add_compile_definitions(__COOL_TIME)
+
+include_directories(
+    ${PATH_TO_COOL}/Time/src
+)
---
+++ src/Cool/Time/Internal/Time.cpp
@@ -0,0 +1,39 @@
+#include "../Time.h"
+
+#include "Clock_Realtime.h"
+//#include "Constants/Textures.h"
+//#include <Cool/ImGui/ImGui.h>
+
+std::unique_ptr<Clock> Time::m_clock = nullptr;
+
+void Time::Initialize() {
+    Clock_Realtime::Initialize();
+    m_clock = std::make_unique<Clock_Realtime>();
+}
+
+void Time::Update() {
+    m_clock->update();
+}
+
+float Time::deltaTime() {
+    return m_clock->deltaTime();
+}
+
+float Time::time() {
+    return m_clock->time();
+}
+
+void Time::setTime(float t) {
+    m_clock->setTime(t);
+}
+
+//void Time::ImGui() {
+//    if (CoolImGui::ButtonWithIcon(m_clock->isPlaying() ? Textures::Pause() : Textures::Play())) {
+//        m_clock->togglePlayPause();
+//    }
+//    ImGui::SameLine();
+//    float t = time();
+//    if (ImGui::DragFloat("seconds", &t, 0.5f, 0.f, 0.f, "%.2f")) {
+//        setTime(t);
+//    }
+//}
