0256700d9c91525a958970e506f8fa0bb0320c12
Jules Fouchy <jules.fouchy@ntymail.com>
2021-01-03 00:47:20 +0100

    Add the ExportImage window

--- src/Cool/Exporter.cpp
+++ src/Cool/Exporter.cpp
@@ -4,6 +4,7 @@
 #include <Cool/OpenGL/FrameBuffer.h>
 #include <Cool/File/File.h>
 #include <Cool/ExportImage/AsPNG.h>
+#include <Cool/ImGui/ImGui.h>
 
 namespace Cool {
 
@@ -24,10 +25,48 @@ void Exporter::endImageExport(FrameBuffer& frameBuffer) {
 	frameBuffer.unbind();
 	// Write png
 	File::CreateFoldersIfDoesntExist(m_folderPath.c_str());
-	ExportImage::AsPNG((m_folderPath + "/" + m_fileName).c_str(), size.width(), size.height(), data);
+	ExportImage::AsPNG((m_folderPath + "/" + m_fileName + ".png").c_str(), size.width(), size.height(), data);
 	delete[] data;
 	//
 	RenderState::setIsExporting(false);
+}
+
+void Exporter::ImGuiMenuItems() {
+	if (ImGui::Button("Image")) {
+		m_bOpenImageExport = true;
+	}
+	if (ImGui::Button("Image Sequence")) {
+		m_bOpenImageSequenceExport = true;
+	}
+}
+
+bool Exporter::ImGuiExportImageWindow() {
+	bool bMustExport = false;
+	if (m_bOpenImageExport) {
+		ImGui::Begin("Export an Image", &m_bOpenImageExport);
+		// Resolution
+		bool bUsed = false;
+		ImGui::Text("Resolution : "); ImGui::SameLine();
+		ImGui::PushItemWidth(50);
+		unsigned int w = static_cast<unsigned int>(RenderState::getExportSize().x);
+		unsigned int h = static_cast<unsigned int>(RenderState::getExportSize().y);
+		bUsed |= CoolImGui::InputUInt("W", &w); ImGui::SameLine();
+		bUsed |= CoolImGui::InputUInt("H", &h);
+		ImGui::PopItemWidth();
+		if (bUsed)
+			RenderState::setExportSize(static_cast<int>(w), static_cast<int>(h));
+		// File and Folders
+		ImGui::InputText("File Name", &m_fileName);
+		ImGui::InputText("Path", &m_folderPath);
+		// Validation
+		ImGui::NewLine();
+		bMustExport = ImGui::Button("Export as PNG");
+		if (bMustExport)
+			m_bOpenImageExport = false;
+		//
+		ImGui::End();
+	}
+	return bMustExport;
 }
 
 } // namespace Cool
--- src/Cool/Exporter.h
+++ src/Cool/Exporter.h
@@ -16,9 +16,15 @@ public:
 	//void update();
 	//void endImageSequenceExport();
 
+	void ImGuiMenuItems();
+	bool ImGuiExportImageWindow();
+
 private:
 	std::string m_folderPath;
-	std::string m_fileName = "test.png";
+	std::string m_fileName = "test";
+
+	bool m_bOpenImageExport = false;
+	bool m_bOpenImageSequenceExport = false;
 };
 
 } // namespace Cool
