adeffd811af7e898b360eb3787fadd83dd7b8fa2
Jules Fouchy <jules.fouchy@ntymail.com>
2021-01-03 02:00:59 +0100

    Add documenting comments

--- src/Cool/Exporter.h
+++ src/Cool/Exporter.h
@@ -9,20 +9,43 @@ public:
 	Exporter();
 	~Exporter() = default;
 
+	/// <summary>
+	/// Call this before your rendering code.
+	/// </summary>
 	void beginImageExport();
+	/// <summary>
+	/// Call this after your rendering code.
+	/// </summary>
+	/// <param name="frameBuffer">The frame buffer that your renderer has just rendered to.</param>
 	void endImageExport(FrameBuffer& frameBuffer);
 
 	//void beginImageSequenceExport();
 	//void update();
 	//void endImageSequenceExport();
 
-	std::string imageOutputPath();
-
+	/// <summary>
+	/// The buttons to open the different export windows.
+	/// </summary>
 	void ImGuiMenuItems();
+	/// <summary>
+	/// The window with the image export parameters
+	/// </summary>
+	/// <returns>true iff you should then export an image. (By calling beginImageExport(), then your rendering code, then endImageExport()</returns>
 	bool ImGuiExportImageWindow();
 
+	/// <summary>
+	/// Open or closes the window with the image export parameters.
+	/// </summary>
+	/// <param name="bOpen"></param>
 	void setIsExportImageWindowOpen(bool bOpen);
+	/// <summary>
+	/// Open or closes the window with the image sequence export parameters.
+	/// </summary>
+	/// <param name="bOpen"></param>
 	inline void setIsExportImageSequenceWindowOpen(bool bOpen) { m_bOpenImageSequenceExport = bOpen; }
+
+private:
+	std::string imageOutputPath();
 
 private:
 	std::string m_folderPath;
