09d8f7b8124e77bcbc43ca666d7ecd817bb42938
Jules Fouchy <jules.fouchy@ntymail.com>
2021-01-13 19:23:33 +0100

    [ComputeShader] Templatize the work group sizes

--- src/Cool/OpenGL/ComputeShader.h
+++ src/Cool/OpenGL/ComputeShader.h
@@ -4,19 +4,52 @@
 
 namespace Cool {
 
+template<unsigned int WorkGroupSizeX = 256, unsigned int WorkGroupSizeY = 1, unsigned int WorkGroupSizeZ = 1>
 class ComputeShader {
 public:
 	ComputeShader() = default;
-	ComputeShader(const char* filePath);
-	ComputeShader(ComputeShader&& o) noexcept;
-	~ComputeShader() = default;
-	void operator=(ComputeShader&& o);
+    ComputeShader(const char* filePath)
+        : m_shader({ ShaderCode(
+            ShaderType::Compute,
+            filePath)
+          })
+    {}
+    ComputeShader(ComputeShader&& o) noexcept
+        : m_shader(std::move(o.m_shader))
+    {}
+    void operator=(ComputeShader&& o) {
+        m_shader = std::move(o.m_shader);
+    }
 
-	void createProgramFromFile(const char* filePath);
-	void createProgramFromCode(const std::string& sourceCode);
+    inline Shader& get() { return m_shader; }
 
-	void compute(unsigned int nbComputations);
-	inline Shader& get() { return m_shader; }
+    void createProgramFromFile(const char* filePath) {
+        m_shader.createProgram({ ShaderCode(
+            ShaderType::Compute,
+            filePath)
+        });
+    }
+
+    void createProgramFromCode(const std::string& sourceCode) {
+        m_shader.createProgram({ ShaderCode::FromCode(
+            ShaderType::Compute,
+            sourceCode
+        )});
+    }
+
+    void compute(unsigned int nbComputationsX, unsigned int nbComputationsY = 1, unsigned int nbComputationsZ = 1) {
+        static constexpr unsigned int WorkGroupSize = WorkGroupSizeX * WorkGroupSizeY * WorkGroupSizeZ;
+        m_shader.bind();
+        m_shader.setUniform1i("NumberOfComputationsX", nbComputationsX);
+        m_shader.setUniform1i("NumberOfComputationsY", nbComputationsY);
+        m_shader.setUniform1i("NumberOfComputationsZ", nbComputationsZ);
+        GLCall(glDispatchCompute(
+            nbComputationsX / WorkGroupSizeX + ((nbComputationsX % WorkGroupSizeX == 0) ? 0 : 1),
+            nbComputationsY / WorkGroupSizeY + ((nbComputationsY % WorkGroupSizeY == 0) ? 0 : 1),
+            nbComputationsZ / WorkGroupSizeZ + ((nbComputationsZ % WorkGroupSizeZ == 0) ? 0 : 1)
+        ));
+        GLCall(glMemoryBarrier(GL_SHADER_STORAGE_BARRIER_BIT | GL_VERTEX_ATTRIB_ARRAY_BARRIER_BIT));
+    }
 
 private:
 	Shader m_shader;
