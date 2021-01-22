44dbf44069bfdb44ae1a03ef66c55d5a6e9f5b42
Jules Fouchy <jules.fouchy@ntymail.com>
2021-01-22 19:16:03 +0100

    Use free functions instead of a wrapper class

---
+++ src/Cool/Serialization/JsonFile.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <Cool/Log/Log.h>
+#include <Cool/File/File.h>
+
+namespace Cool::Serialization {
+
+template <typename T>
+void ToJSON(const T& data, const char* filePath) {
+	if (File::Exists(filePath)) {
+		std::ifstream is(filePath);
+		//{
+			try {
+				cereal::JSONInputArchive archive(is);
+				archive(
+					data
+				);
+			}
+			catch (std::exception e) {
+				Log::Release::Warn("Corrupted {} file. Starting with default values instead. \n{}", filePath, e.what());
+			}
+		//}
+	}
+	else {
+		Log::Release::Warn("{} not found. Starting with default values instead.", filePath);
+	}
+}
+
+template <typename T>
+void FromJSON(T& data, const char* filePath) {
+	File::CreateFoldersForFileIfDoesntExist(filePath);
+	std::ofstream os(filePath);
+	{
+		cereal::JSONOutputArchive archive(os);
+		archive(
+			data
+		);
+	}
+}
+
+} // namespace Cool
