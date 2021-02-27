8c10a8252a333e1cef9b48458d8903e9e9b13b12
Jules Fouchy <jules.fouchy@ntymail.com>
2021-02-27 15:45:14 +0100

    [Presets] Make sure there is no dot in the name while renaming

--- src/Cool/Params/internal/Presets.h
+++ src/Cool/Params/internal/Presets.h
@@ -140,26 +140,31 @@ private:
 	void on_rename_popup_close() {
 		_rename_popup_open_this_frame = false;
 		if (_rename_popup_open_last_frame) {
-			compute_current_preset_idx(); // Could have been changed by playing a record
-			if (_current_preset_idx != -1) {
-				const std::string new_path = full_path(_new_preset_name);
-				if (!File::Exists(new_path)) {
-					std::filesystem::rename(
-						full_path(_current_preset_name),
-						new_path
-					);
-					_current_preset_name = _new_preset_name;
-					_presets[_current_preset_idx].name = _new_preset_name;
-					sort();
+			if (_new_preset_name.find(".") == std::string::npos) {
+				compute_current_preset_idx(); // Could have been changed
+				if (_current_preset_idx != -1) {
+					const std::string new_path = full_path(_new_preset_name);
+					if (!File::Exists(new_path)) {
+						std::filesystem::rename(
+							full_path(_current_preset_name),
+							new_path
+						);
+						_current_preset_name = _new_preset_name;
+						_presets[_current_preset_idx].name = _new_preset_name;
+						sort();
+					}
+					else {
+						boxer::show(("\"" + _new_preset_name + "\" already exists.").c_str(), "Renaming failed", boxer::Style::Warning);
+					}
 				}
 				else {
-					boxer::show(("\"" + _new_preset_name + "\" already exists.").c_str(), "Renaming failed", boxer::Style::Warning);
+					boxer::show("Actually this preset isn't saved on your computer, it was just set by a record.\nYou will now be able to \"Save settings\" if you want to.", "Renaming failed", boxer::Style::Info);
+					find_placeholder_name();
+					set_to_placeholder_setting();
 				}
 			}
 			else {
-				boxer::show("Actually this preset isn't saved on your computer, it was just set by a record.\nYou will now be able to \"Save settings\" if you want to.", "Renaming failed", boxer::Style::Info);
-				find_placeholder_name();
-				set_to_placeholder_setting();
+				boxer::show("You can't have dots (.) in the name.", "Renaming failed", boxer::Style::Warning);
 			}
 		}
 	}
