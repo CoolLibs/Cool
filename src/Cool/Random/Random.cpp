587d09dd3b9e10ff347132b0b4720b564e393716
Jules Fouchy <jules.fouchy@ntymail.com>
2021-02-27 22:55:44 +0100

    Add getAnyLongInt()

--- src/Cool/Random/Random.cpp
+++ src/Cool/Random/Random.cpp
@@ -6,6 +6,7 @@ std::random_device Random::trueRandomGenerator;
 std::default_random_engine Random::generator(trueRandomGenerator());
 std::uniform_real_distribution<float> Random::distribution_0to1(0.0f, 1.0f);
 std::uniform_real_distribution<float> Random::distribution_minus1to1(-1.0f, 1.0f);
+std::uniform_int_distribution<long int> Random::distribution_long_int(std::numeric_limits<long int>::min(), std::numeric_limits<long int>::max());
 
 float Random::get0to1() {
 	return distribution_0to1(generator);
@@ -13,6 +14,10 @@ float Random::get0to1() {
 
 float Random::getMinus1to1() {
 	return distribution_minus1to1(generator);
+}
+
+long int Random::getAnyLongInt() {
+	return distribution_long_int(generator);
 }
 
 } // namespace Cool
--- src/Cool/Random/Random.h
+++ src/Cool/Random/Random.h
@@ -10,16 +10,23 @@ public:
 	/// Returns a random float between 0 and 1
 	/// </summary>
 	static float get0to1();
+
 	/// <summary>
 	/// Returns a random float between -1 and 1
 	/// </summary>
 	static float getMinus1to1();
+
+	/// <summary>
+	/// Returns a random long int
+	/// </summary>
+	static long int getAnyLongInt();
 
 private:
 	static std::random_device trueRandomGenerator;
 	static std::default_random_engine generator;
 	static std::uniform_real_distribution<float> distribution_0to1;
 	static std::uniform_real_distribution<float> distribution_minus1to1;
+	static std::uniform_int_distribution<long int> distribution_long_int;
 
 	Random() = delete;
 	~Random() = delete;
