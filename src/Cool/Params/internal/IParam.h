45ce4c463c4984e59a7699eaae30ecd863ca8620
Jules Fouchy <jules.fouchy@ntymail.com>
2021-02-27 18:27:46 +0100

    Record Param value changes in history

--- src/Cool/Params/internal/IParam.h
+++ src/Cool/Params/internal/IParam.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "../ParamsHistory.h"
+
 namespace Cool::Internal {
 
 class IParam {
@@ -19,23 +21,58 @@ template <typename T>
 class Param : public IParam {
 public:
 	Param(std::string_view name, const T& default_value = T(0))
-		: IParam(name), _value(default_value)
+		: IParam(name), _value(default_value), _value_before_edit(default_value)
 	{}
 	inline T& operator* () { return  _value; }
 	inline T* operator->() { return &_value; }
 
 protected:
 	T _value;
+	T _value_before_edit;
+
+protected:
+	void push_change_in_history_if_edit_ended() {
+		if (ImGui::IsItemDeactivatedAfterEdit()) {
+			push_change_in_history();
+			_value_before_edit = _value; // ready for next edit
+		}
+	}
+
+private:
+	void push_change_in_history() {
+		ParamsHistory::Get().begin_undo_group();
+		T val = _value;
+		T prev_val = _value_before_edit;
+		ParamsHistory::Get().add_action({
+			[&, val]()
+			{
+				_value = val;
+			},
+			[&, prev_val]()
+			{
+				_value = prev_val;
+			}
+		});
+		ParamsHistory::Get().end_undo_group();
+	}
 
 private:
 	//Serialization
 	friend class cereal::access;
 	template<class Archive>
-	void serialize(Archive& archive)
+	void save(Archive& archive) const
 	{
 		archive(
 			cereal::make_nvp(name(), _value)
 		);
+	}
+	template<class Archive>
+	void load(Archive& archive)
+	{
+		archive(
+			_value
+		);
+		_value_before_edit = _value;
 	}
 };
 
--- src/Cool/Params/internal/Param_Color.cpp
+++ src/Cool/Params/internal/Param_Color.cpp
@@ -7,7 +7,9 @@ Color::Color(std::string_view name, const glm::vec3& default_value)
 {}
 
 bool Color::ImGui() {
-	return ImGui::ColorEdit3(name().c_str(), glm::value_ptr(_value), ImGuiColorEditFlags_NoInputs);
+	bool b = ImGui::ColorEdit3(name().c_str(), glm::value_ptr(_value), ImGuiColorEditFlags_NoInputs);
+	push_change_in_history_if_edit_ended();
+	return b;
 }
 
 } // namespace Cool
