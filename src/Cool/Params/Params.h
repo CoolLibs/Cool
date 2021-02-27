bd10669375b9096e15f6e2e8be01459c8668057a
Jules Fouchy <jules.fouchy@ntymail.com>
2021-02-27 13:29:15 +0100

    Add the final Params class

---
+++ src/Cool/Params/Params.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "internal/Param.h" // So that people only need to include this one file
+#include "internal/Presets.h"
+
+namespace Cool {
+
+template <typename ParamValues>
+class Params {
+public:
+	Params(const std::string& file_extension, const std::string& folder_path)
+		: _presets(file_extension, folder_path)
+	{}
+	bool ImGui() {
+		bool b = false;
+		if (_values.ImGui()) {
+			_presets.setToPlaceholderSetting();
+			b = true;
+		}
+		b |= _presets.ImGui(&_values);
+		return b;
+	}
+	inline ParamValues& operator* () { return  _values; }
+	inline ParamValues* operator->() { return &_values; }
+
+private:
+	ParamValues _values;
+	Presets<ParamValues> _presets;
+
+private:
+	//Serialization
+	friend class cereal::access;
+	template<class Archive>
+	void serialize(Archive& archive)
+	{
+		archive(
+			_values,
+			_presets
+		);
+	}
+};
+
+} // namespace Cool
