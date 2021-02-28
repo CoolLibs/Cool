737381296fa9ae4922740739f50a62eba4c44084
Jules Fouchy <jules.fouchy@ntymail.com>
2021-02-28 14:59:04 +0100

    [Param_Float] Actually don't serialize the range

--- src/Cool/Params/internal/Param_Float.h
+++ src/Cool/Params/internal/Param_Float.h
@@ -29,28 +29,6 @@ protected:
 private:
 	float _min_value;
 	float _max_value;
-
-private:
-	//Serialization
-	friend class cereal::access;
-	template<class Archive>
-	void save(Archive& archive) const
-	{
-		archive(
-			cereal::base_class<Internal::Param<float>>(this),
-			cereal::make_nvp("Min Value", _min_value),
-			cereal::make_nvp("Max Value", _max_value)
-		);
-	}
-	template<class Archive>
-	void load(Archive& archive)
-	{
-		archive(
-			cereal::base_class<Internal::Param<float>>(this),
-			_min_value,
-			_max_value
-		);
-	}
 };
 
 } // namespace Cool::Param
