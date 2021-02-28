e301490821c31d857a5e2a71adc1d752e179c452
Jules Fouchy <jules.fouchy@ntymail.com>
2021-02-28 14:53:09 +0100

    Param_Float now has extensible range

--- src/Cool/Params/internal/Param_Float.h
+++ src/Cool/Params/internal/Param_Float.h
@@ -12,12 +12,45 @@ public:
 
 protected:
 	bool ImGuiWidget() override {
-		return ImGui::SliderFloat(name().c_str(), &_value, _min_value, _max_value);
+		bool b = ImGui::SliderFloat(name().c_str(), &_value, _min_value, _max_value);
+		ImGui::PushID(this);
+		if (ImGui::BeginPopupContextItem()) {
+			ImGui::DragFloat("", &_min_value);
+			ImGui::SameLine();
+			ImGui::Text("to");
+			ImGui::SameLine();
+			ImGui::DragFloat(" ", &_max_value);
+			ImGui::EndPopup();
+		}
+		ImGui::PopID();
+		return b;
 	}
 
 private:
 	float _min_value;
 	float _max_value;
+
+private:
+	//Serialization
+	friend class cereal::access;
+	template<class Archive>
+	void save(Archive& archive) const
+	{
+		archive(
+			cereal::base_class<Internal::Param<float>>(this),
+			cereal::make_nvp("Min Value", _min_value),
+			cereal::make_nvp("Max Value", _max_value)
+		);
+	}
+	template<class Archive>
+	void load(Archive& archive)
+	{
+		archive(
+			cereal::base_class<Internal::Param<float>>(this),
+			_min_value,
+			_max_value
+		);
+	}
 };
 
 } // namespace Cool::Param
