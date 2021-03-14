b2eecc931b0813a5ffd48f36fa2ce824b40cff8b
Jules Fouchy <jules.fouchy@ntymail.com>
2021-03-14 20:51:40 +0100

    Add assert to check that start / stop is used properly

--- src/Cool/MultiThread/internal/ThreadPool.cpp
+++ src/Cool/MultiThread/internal/ThreadPool.cpp
@@ -12,15 +12,18 @@ ThreadPool::~ThreadPool() {
 	}
 }
 
-bool ThreadPool::has_idle_threads() {
+bool ThreadPool::has_idle_threads() {;
+	assert(_running && "You must call start() before working with the thread pool.");
 	return _queue.size() < _nb_threads;
 }
 
 bool ThreadPool::has_finished_all_jobs() {
+	assert(_running && "You must call start() before working with the thread pool.");
 	return _queue.size() == 0;
 }
 
 void ThreadPool::start() {
+	assert(!_running && "You already called start.");
 	_threads.reserve(_nb_threads);
 	for (size_t i = 0; i < _nb_threads; ++i) {
 		_threads.emplace_back([this]() { check_for_jobs(); });
@@ -29,6 +32,7 @@ void ThreadPool::start() {
 }
 
 void ThreadPool::stop() {
+	assert(_running && "You already called stop or you forgot to call start.");
 	while (!has_finished_all_jobs()) {} // Wait for the jobs queue to be empty
 	_running = false;
 	_condition.notify_all(); // Wake up all threads and let them realize that _running == false
@@ -39,6 +43,7 @@ void ThreadPool::stop() {
 }
 
 void ThreadPool::push_job(std::function<void()> job) {
+	assert(_running && "You must call start() before working with the thread pool.");
 	{
 		std::unique_lock<std::mutex> lock(_queue_mutex);
 		_queue.emplace_back(std::move(job));
