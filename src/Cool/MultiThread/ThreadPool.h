b51705fccc588349a8e3c43e8263c60f69f79bd9
Jules Fouchy <jules.fouchy@ntymail.com>
2021-03-14 20:48:52 +0100

    Documentation

--- src/Cool/MultiThread/ThreadPool.h
+++ src/Cool/MultiThread/ThreadPool.h
@@ -2,15 +2,44 @@
 
 namespace Cool {
 
+/// <summary>
+/// Manages a given number of threads and give them jobs
+/// </summary>
 class ThreadPool {
-public:
+public:	
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="nb_threads">Number of threads to create in the pool</param>
 	ThreadPool(size_t nb_threads);
 	~ThreadPool();
 
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <returns>true iff some threads are idle and can accept a job</returns>
 	bool has_idle_threads();
+
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <returns>true iff all threads are idle</returns>
 	bool has_finished_all_jobs();
+
+	/// <summary>
+	/// Starts the pool : creates the threads
+	/// </summary>
 	void start();
+
+	/// <summary>
+	/// Stops the pool : finishes all jobs and then destroys the threads
+	/// </summary>
 	void stop();
+
+	/// <summary>
+	/// Add a job to the queue.
+	/// </summary>
+	/// <param name="job"></param>
 	void push_job(std::function<void()> job);
 
 private:
