50e8d733e362b7de88160d591eb31265648ab675
Jules Fouchy <jules.fouchy@ntymail.com>
2021-03-25 21:56:33 +0100

    Add Param::Dir3

---
+++ src/Cool/Params/internal/Param_Dir3.h
@@ -0,0 +1,75 @@
+#pragma once
+
+#include "IParam.h"
+#include <Cool/ImGui/ImGui.h>
+
+namespace Cool::Internal {
+
+class Dir3Angles : public Internal::ParamForPrimitiveType<glm::vec2> {
+public:
+	Dir3Angles(std::string_view name, float default_angle_ground = 0.f, float default_angle_up = 0.f)
+		: ParamForPrimitiveType(name, glm::vec2(default_angle_ground, default_angle_up))
+	{}
+
+	bool ImGuiWidget() override {
+		ImGui::PushID(this);
+		bool b = false;
+		ImGui::Text("%s :", name().c_str());
+		b |= CoolImGui::AngleWheel("Angle Ground", &_value[0]);
+		b |= CoolImGui::AngleWheel("Angle Up", &_value[1]);
+		ImGui::PopID();
+		return b;
+	}
+};
+
+} // namespace Cool::Internal
+
+namespace Cool::Param {
+
+class Dir3 : public Cool::Internal::IParam {
+public:
+	Dir3(std::string_view name, float default_angle_ground = 0.f, float default_angle_up = 0.f)
+		: _angles(name, default_angle_ground, default_angle_up)
+	{}
+
+	inline const glm::vec3 operator* () const { return compute_direction(); }
+
+	glm::vec3 compute_direction() const {
+		return glm::vec3(
+			 cos(_angles->y) * cos(_angles->x),
+			-cos(_angles->y) * sin(_angles->x),
+			-sin(_angles->y)
+		);
+	}
+
+	bool ImGui(Action on_edit_ended, std::function<void()> on_value_change = []() {}) override {
+		return _angles.ImGui(on_edit_ended, on_value_change);
+	}
+
+#ifdef __COOL_OPENGL
+	void set_uniform_in_shader(Shader& shader) override {
+		shader.setUniform(name().c_str(), operator*());
+	}
+#endif
+
+	inline const std::string& name() const override { return _angles.name(); }
+
+protected:
+	bool ImGuiWidget() override {
+		return _angles.ImGuiWidget();
+	}
+
+private:
+	//Serialization
+	friend class cereal::access;
+	template<class Archive>
+	void serialize(Archive& archive)
+	{
+		archive(_angles);
+	}
+
+private:
+	Internal::Dir3Angles _angles;
+};
+
+} // namespace Cool::Param
