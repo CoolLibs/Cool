4ca495c1b41a3fd1fd0d0c5e26ff31a444f5884d
Jules Fouchy <jules.fouchy@ntymail.com>
2021-04-21 19:45:02 +0200

    Add NfdFileFilter

---
+++ src/Cool/App/internal/NfdFileFilter.cpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "../NfdFileFilter.h"
+
+namespace Cool {
+
+const std::vector<nfdfilteritem_t> NfdFileFilter::Image = {
+	{ "Image", "png,jpg,jpeg,tif,tiff" },
+	{ "png", "png" },
+	{ "jpeg", "jpg,jpeg" },
+	{ "tiff", "tif,tiff" },
+};
+const std::vector<nfdfilteritem_t> NfdFileFilter::Png  = { NfdFileFilter::Image[1] };
+const std::vector<nfdfilteritem_t> NfdFileFilter::Jpeg = { NfdFileFilter::Image[2] };
+const std::vector<nfdfilteritem_t> NfdFileFilter::Tiff = { NfdFileFilter::Image[3] };
+
+const std::vector<nfdfilteritem_t> NfdFileFilter::Shader = {
+	{ "Shader", "frag,fs,vert,vs,comp,cs,glsl,hlsl" },
+	{ "Fragment Shader", "frag,fs,glsl,hlsl" },
+	{ "Vertex Shader", "vert,vs,glsl,hlsl" },
+	{ "Compute Shader", "comp,cs,glsl,hlsl" },
+};
+const std::vector<nfdfilteritem_t> NfdFileFilter::FragmentShader = { NfdFileFilter::Shader[1] };
+const std::vector<nfdfilteritem_t> NfdFileFilter::VertexShader   = { NfdFileFilter::Shader[2] };
+const std::vector<nfdfilteritem_t> NfdFileFilter::ComputeShader  = { NfdFileFilter::Shader[3] };
+
+} // namespace Cool
