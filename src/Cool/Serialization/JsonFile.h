abf264bfb5f376fdcb84fecbb72337726998d7e9
Jules Fouchy <jules.fouchy@ntymail.com>
2021-05-13 12:46:38 +0200

    Add documentation

--- src/Cool/Serialization/JsonFile.h
+++ src/Cool/Serialization/JsonFile.h
@@ -7,6 +7,12 @@
 
 namespace Cool::Serialization {
 
+/// <summary>
+/// Loads data from a JSON file. The type T needs to implement the Cereal serialization functions described here : https://uscilab.github.io/cereal/serialization_functions.html
+/// </summary>
+/// <typeparam name="T">Any type that implements the Cereal serialization functions described here : https://uscilab.github.io/cereal/serialization_functions.html </typeparam>
+/// <param name="data">A reference to the data to initialize</param>
+/// <param name="filePath">The path to the JSON file. If it doesn't exist this function will do nothing.</param>
 template <typename T>
 void FromJSON(T& data, std::string_view filePath) {
 	if (File::Exists(filePath)) {
@@ -26,6 +32,14 @@ void FromJSON(T& data, std::string_view filePath) {
 	}
 }
 
+
+/// <summary>
+/// Saves data to a JSON file. The type T needs to implement the Cereal serialization functions described here : https://uscilab.github.io/cereal/serialization_functions.html
+/// </summary>
+/// <typeparam name="T">Any type that implements the Cereal serialization functions described here : https://uscilab.github.io/cereal/serialization_functions.html </typeparam>
+/// <param name="data">The data to save</param>
+/// <param name="filePath">The path to the JSON file. It will be created if it doesn't exist already.</param>
+/// <param name="fieldName">An optional name that will be given to data inside the JSON file (for readability purposes).</param>
 template <typename T>
 void ToJSON(T& data, std::string_view filePath, std::string_view fieldName = "value0") {
 	File::CreateFoldersForFileIfDoesntExist(filePath);
