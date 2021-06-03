f814f3e2b8870657a7506aac4957a0b3994ef748
Jules Fouchy <jules.fouchy@ntymail.com>
2021-06-03 19:37:29 +0200

    File is now a class, and RootDir is a private member ; it must be configured by the user at initialization time

--- src/Cool/File/File.cpp
+++ src/Cool/File/File.cpp
@@ -4,27 +4,32 @@
 #include <fstream>
 #include <streambuf>
 
-namespace Cool::File {
+namespace Cool {
 
-bool Exists(std::string_view filepath) {
+std::string File::_root_dir;
+#ifndef NDEBUG
+bool File::_root_dir_is_initialized = false;
+#endif
+
+bool File::Exists(std::string_view filepath) {
     struct stat buffer;
     return (stat(filepath.data(), &buffer) == 0);
 }
 
-std::string AbsolutePath(std::string_view filepath) {
+std::string File::AbsolutePath(std::string_view filepath) {
     return std::filesystem::absolute(filepath).string();
 }
 
-std::string FileName(const std::string& filepath) {
+std::string File::FileName(const std::string& filepath) {
     auto pos = filepath.find_last_of("/\\") + 1;
     return filepath.substr(pos, filepath.size() - pos);
 }
 
-std::string FileNameWithoutExtension(const std::string& filepath) {
+std::string File::FileNameWithoutExtension(const std::string& filepath) {
     return WhithoutFileExtension(FileName(filepath));
 }
 
-std::string FileExtension(const std::string& filepath) {
+std::string File::FileExtension(const std::string& filepath) {
     auto pos = filepath.find_last_of(".");
     if (pos < filepath.size())
         return filepath.substr(pos, filepath.size());
@@ -32,14 +37,14 @@ std::string FileExtension(const std::string& filepath) {
         return "";
 }
 
-std::string WhithoutFileExtension(const std::string& filepath) {
+std::string File::WhithoutFileExtension(const std::string& filepath) {
     auto pos = filepath.find_last_of(".");
     if (pos < filepath.size())
         return filepath.substr(0, pos);
     return filepath;
 }
 
-std::string WhithoutFileName(const std::string& filepath) {
+std::string File::WhithoutFileName(const std::string& filepath) {
     if (filepath.find_last_of(".") < filepath.size()) { // There is a "." of an extension, so the thing after the last "/" must be a file name
         auto pos = filepath.find_last_of("/\\");
         return filepath.substr(0, pos);
@@ -47,7 +52,7 @@ std::string WhithoutFileName(const std::string& filepath) {
     return filepath;
 }
 
-void ToString(std::string_view filepath, std::string* dst) {
+void File::ToString(std::string_view filepath, std::string* dst) {
     // Thanks to https://stackoverflow.com/questions/2602013/read-whole-ascii-file-into-c-stdstring
     std::ifstream stream(filepath);
     if (!stream.is_open()) {
@@ -64,7 +69,7 @@ void ToString(std::string_view filepath, std::string* dst) {
     stream.close();
 }
 
-bool CreateFoldersIfDoesntExist(std::string_view folderPath) {
+bool File::CreateFoldersIfDoesntExist(std::string_view folderPath) {
     if (!Exists(folderPath)) {
         try {
             std::filesystem::create_directories(folderPath);
@@ -78,8 +83,24 @@ bool CreateFoldersIfDoesntExist(std::string_view folderPath) {
     return true;
 }
 
-bool CreateFoldersForFileIfDoesntExist(std::string_view folderAndFilePath) {
+bool File::CreateFoldersForFileIfDoesntExist(std::string_view folderAndFilePath) {
     return CreateFoldersIfDoesntExist(WhithoutFileName(std::string(folderAndFilePath)).c_str());
+}
+
+void File::initialize_root_dir(std::string_view path) {
+#ifndef NDEBUG
+    assert(!_root_dir_is_initialized);
+    _root_dir_is_initialized = true;
+#endif
+    _root_dir = path;
+    Log::Info("Initialized File::root_dir as \"{}\"", _root_dir);
+}
+
+const std::string& File::root_dir() {
+#ifndef NDEBUG
+    assert(_root_dir_is_initialized);
+#endif
+    return _root_dir;
 }
 
 } // namespace Cool
--- src/Cool/File/File.h
+++ src/Cool/File/File.h
@@ -2,57 +2,68 @@
 
 #include <filesystem>
 
-namespace Cool::File {
+namespace Cool {
 
-/// <summary>
-/// Returns true iff filepath corresponds to an existing file or folder
-/// </summary>
-bool Exists(std::string_view filepath);
-/// <summary>
-/// Converts a relative or absolute path to an absolute path
-/// </summary>
-std::string AbsolutePath(std::string_view filepath);
-/// <summary>
-/// Returns the name of the file + its extension (removes parent folders)
-/// </summary>
-std::string FileName(const std::string& filepath);
-/// <summary>
-/// Returns the name of the file (removes parent folders and extension)
-/// </summary>
-std::string FileNameWithoutExtension(const std::string& filepath);
-/// <summary>
-/// Returns the extension of the file, preceded by a dot : something like ".png"
-/// </summary>
-std::string FileExtension(const std::string& filepath);
-/// <summary>
-/// Returns the filepath but whithout the extension.
-/// </summary>
-std::string WhithoutFileExtension(const std::string& filepath);
-/// <summary>
-/// Returns the folders in the path, removes the file name if there is one at the end
-/// </summary>
-std::string WhithoutFileName(const std::string& filepath);
-/// <summary>
-/// Loads the content of a file into a std::string.
-/// (NB : We use this ugly API instead of returning a string for performance reasons.)
-/// </summary>
-/// <param name="filepath">Path to the file to be loaded</param>
-/// <param name="dst">Pointer to the string to be filled with the content of the file</param>
-void ToString(std::string_view filepath, std::string* dst);
-/// <summary>
-/// Recursively creates all the folders so that at the end folderPath is a valid folder path. /!\ There shouldn't be any file name at the end of folderPath ; it there is, then use CreateFoldersForFileIfDoesntExist() instead.
-/// Returns true iff the folders now exist (be it because they already existed or because they have been successfully created)
-/// </summary>
-bool CreateFoldersIfDoesntExist(std::string_view folderPath);
-/// <summary>
-/// Recursively creates all the folders so that at the end folderAndFilePath is a valid file path.
-/// Returns true iff the folders now exist (be it because they already existed or because they have been successfully created)
-/// </summary>
-bool CreateFoldersForFileIfDoesntExist(std::string_view folderAndFilePath);
-/// <summary>
-/// Root directory where the program was started from.
-/// Use this to describe your ressources by an absolute path, especially if you play at runtime with the directories and your relative paths get messed up.
-/// </summary>
-const std::string RootDir = std::filesystem::current_path().string();
+class File {
+public:
+      /// <summary>
+      /// Returns true iff filepath corresponds to an existing file or folder
+      /// </summary>
+      static bool Exists(std::string_view filepath);
+      /// <summary>
+      /// Converts a relative or absolute path to an absolute path
+      /// </summary>
+      static std::string AbsolutePath(std::string_view filepath);
+      /// <summary>
+      /// Returns the name of the file + its extension (removes parent folders)
+      /// </summary>
+      static std::string FileName(const std::string& filepath);
+      /// <summary>
+      /// Returns the name of the file (removes parent folders and extension)
+      /// </summary>
+      static std::string FileNameWithoutExtension(const std::string& filepath);
+      /// <summary>
+      /// Returns the extension of the file, preceded by a dot : something like ".png"
+      /// </summary>
+      static std::string FileExtension(const std::string& filepath);
+      /// <summary>
+      /// Returns the filepath but whithout the extension.
+      /// </summary>
+      static std::string WhithoutFileExtension(const std::string& filepath);
+      /// <summary>
+      /// Returns the folders in the path, removes the file name if there is one at the end
+      /// </summary>
+      static std::string WhithoutFileName(const std::string& filepath);
+      /// <summary>
+      /// Loads the content of a file into a std::string.
+      /// (NB : We use this ugly API instead of returning a string for performance reasons.)
+      /// </summary>
+      /// <param name="filepath">Path to the file to be loaded</param>
+      /// <param name="dst">Pointer to the string to be filled with the content of the file</param>
+      static void ToString(std::string_view filepath, std::string* dst);
+      /// <summary>
+      /// Recursively creates all the folders so that at the end folderPath is a valid folder path. /!\ There shouldn't be any file name at the end of folderPath ; it there is, then use CreateFoldersForFileIfDoesntExist() instead.
+      /// Returns true iff the folders now exist (be it because they already existed or because they have been successfully created)
+      /// </summary>
+      static bool CreateFoldersIfDoesntExist(std::string_view folderPath);
+      /// <summary>
+      /// Recursively creates all the folders so that at the end folderAndFilePath is a valid file path.
+      /// Returns true iff the folders now exist (be it because they already existed or because they have been successfully created)
+      /// </summary>
+      static bool CreateFoldersForFileIfDoesntExist(std::string_view folderAndFilePath);
+      /// <summary>
+      /// Root directory where the program was started from.
+      /// Use this to describe your ressources by an absolute path, especially if you play at runtime with the directories and your relative paths get messed up.
+      /// </summary>
+      static const std::string& root_dir();
 
-} // namespace Cool::File
+      static void initialize_root_dir(std::string_view path);
+
+private:
+      static std::string _root_dir;
+#ifndef NDEBUG
+      static bool _root_dir_is_initialized;
+#endif
+};
+
+} // namespace Cool
