3441b8b9db15f2efa139da39f9b164253e7fefe4
Jules Fouchy <jules.fouchy@ntymail.com>
2021-06-03 20:49:33 +0200

    Log::Release renamed as Log::ToUser

--- src/Cool/FileWatcher/FileWatcher.cpp
+++ src/Cool/FileWatcher/FileWatcher.cpp
@@ -38,7 +38,7 @@ void FileWatcher::setPath(std::string_view path) {
 	m_bPathIsValid = false; // Forces checkAndUpdatePathValidity() to trigger an update (if the new path is valid)
 	checkAndUpdatePathValidity();
 	if (!m_bPathIsValid) {
-		Log::Release::Warn("[FileWatcher::setPath] Invalid file path : \"{}\"", path);
+		Log::ToUser::Warn("[FileWatcher::setPath] Invalid file path : \"{}\"", path);
 	}
 }
 
