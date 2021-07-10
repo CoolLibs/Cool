1124aac2f4f68d0364588ad618b7e36095a7a480
Jules Fouchy <jules.fouchy@ntymail.com>
2021-07-10 17:38:24 +0200

    [Camera::ViewController_Freefly] Update interface

--- src/Cool/Camera/ViewController_Freefly.h
+++ src/Cool/Camera/ViewController_Freefly.h
@@ -6,25 +6,7 @@ class Camera;
 
 class ViewController_Freefly {
 public:
-	/**
-	 * @brief 
-	 * 
-	 * @param camera The camera to control
-	 */
-	ViewController_Freefly(Camera& camera);
-
-	//inline void update() { _state->update(); }
-	//inline void on_wheel_down(int mods) { _state->on_wheel_down(mods); }
-	//inline void on_wheel_up() { _state->on_wheel_up(); }
-	//inline void on_wheel_scroll(float dl) { _state->on_wheel_scroll(dl); }
-
-private:
-	void update_transform_matrix();
-
-private:
-	Camera& _camera;
-	float   _angle_ground = 0.f;
-	float   _angle_up = 0.f;
+	void update(Camera& camera, float dt);
 };
 
 } // namespace Cool
