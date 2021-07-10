cd2b371ee9b063cb66b69ca43f913e636414e7ad
Jules Fouchy <jules.fouchy@ntymail.com>
2021-07-10 17:18:38 +0200

    [Camera::Camera] Add translate() and rotate_around()

--- src/Cool/Camera/Camera.h
+++ src/Cool/Camera/Camera.h
@@ -10,14 +10,30 @@ public:
 	inline       glm::mat4  view_matrix()       const { return glm::inverse(_transform_matrix); }
 	inline const glm::mat4& projection_matrix() const { return _projection_matrix; }
 	glm::vec3 right_axis() const;
-	glm::vec3 up_axis() const;
+	glm::vec3 up_axis   () const;
 	glm::vec3 front_axis() const;
-	glm::vec3 position() const;
+	glm::vec3 position  () const;
 
-	inline void set_transform_matrix (const glm::mat4& new_transform_matrix)  { _transform_matrix  = new_transform_matrix;	}
-	inline void set_view_matrix (const glm::mat4& view_matrix)  { _transform_matrix  = glm::inverse(view_matrix); }
-	inline void set_projection_matrix(const glm::mat4& new_projection_matrix) { _projection_matrix = new_projection_matrix;	}
+	inline void set_transform_matrix (const glm::mat4& transform_matrix)  { _transform_matrix  = transform_matrix;	}
+	inline void set_view_matrix      (const glm::mat4& view_matrix)       { _transform_matrix  = glm::inverse(view_matrix); }
+	inline void set_projection_matrix(const glm::mat4& projection_matrix) { _projection_matrix = projection_matrix;	}
 	
+	/**
+	 * @brief Translation expressed in world space
+	 * 
+	 * @param delta_position 
+	 */
+	void translate(glm::vec3 const& delta_position);
+
+	/**
+	 * @brief Rotation expressed in world space
+	 * 
+	 * @param center 
+	 * @param angle 
+	 * @param axis 
+	 */
+	void rotate_around(glm::vec3 const& center, float angle, glm::vec3 const& axis);
+
 	/**
 	 * @brief Returns a Ray passing through the given pixel and starting at the camera position. Typically what you would want in order to check if a 3D object is under the mouse cursor by casting a ray : ray_passing_through_pixel(Input::mouse_in_pixels())
 	 * 
@@ -27,8 +43,8 @@ public:
 	Ray ray_passing_through_pixel(const glm::vec2& position_in_pixels);
 
 private:
-	glm::mat4 _transform_matrix;
-	glm::mat4 _projection_matrix;
+	glm::mat4 _transform_matrix  = glm::mat4{1.f};
+	glm::mat4 _projection_matrix = glm::mat4{1.f};
 };
 
 } // namespace Cool
--- src/Cool/Camera/internal/Camera.cpp
+++ src/Cool/Camera/internal/Camera.cpp
@@ -9,6 +9,16 @@ glm::vec3 Camera::up_axis()    const { return glm::normalize(glm::column(_transf
 glm::vec3 Camera::front_axis() const { return glm::normalize(glm::column(_transform_matrix, 2)); }
 glm::vec3 Camera::position()   const { return                glm::column(_transform_matrix, 3); }
 
+void Camera::translate(glm::vec3 const& delta_position) {
+	_transform_matrix = glm::translate(glm::mat4{1.f}, delta_position) * _transform_matrix;
+}
+
+void Camera::rotate_around(glm::vec3 const& center, float angle, glm::vec3 const& axis) {
+	translate(-center);
+	_transform_matrix = glm::rotate(glm::mat4{1.f}, angle, axis) * _transform_matrix;
+	translate(+center);
+}
+
 Ray Camera::ray_passing_through_pixel(const glm::vec2& position_in_pixels) {
 	glm::vec3 pos = position();
 	glm::vec3 pixel_pos = glm::unProject(glm::vec3(position_in_pixels, 0.0f), view_matrix(), projection_matrix(), glm::vec4(0.0f, 0.0f, RenderState::Size().width(), RenderState::Size().height())); // TODO use the size of the screen ? isn't it bugued out if render size > on_scree size ?
