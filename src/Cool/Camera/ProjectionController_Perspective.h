29d6f3a09dec1892dc2cd995d45c162f4f9e2f43
Jules Fouchy <jules.fouchy@ntymail.com>
2021-07-10 17:47:40 +0200

    [Camera::ProjectionController_Perspective] Don't store a reference to the camera !

--- src/Cool/Camera/ProjectionController_Perspective.h
+++ src/Cool/Camera/ProjectionController_Perspective.h
@@ -6,25 +6,11 @@ class Camera;
 
 class ProjectionController_Perspective {
 public:
-	/**
-	 * @brief 
-	 * 
-	 * @param The camera to control 
-	 */
-	ProjectionController_Perspective(Camera& camera)
-		: _camera(camera)
-	{
-		update_projection_matrix();
-	}
-
+	void apply_to(Camera& camera) const;
 	inline float focal_length() const { return _focal_length; }
 
 private:
-	void update_projection_matrix();
-
-private:
-	float   _focal_length = 1.f;
-	Camera& _camera;
+	float _focal_length = 1.f;
 };
 
 } // namespace Cool
--- src/Cool/Camera/internal/ProjectionController_Perspective.cpp
+++ src/Cool/Camera/internal/ProjectionController_Perspective.cpp
@@ -1,13 +1,13 @@
 #include "../ProjectionController_Perspective.h"
-#include <Cool/App/RenderState.h>
 #include "../Camera.h"
+#include <Cool/App/RenderState.h>
 
 namespace Cool {
 
-void ProjectionController_Perspective::update_projection_matrix() {
+void ProjectionController_Perspective::apply_to(Camera& camera) const {
 	float near_plane = 0.1f;
 	float fov = 2.f * atan(1.f / _focal_length); // in radians
-	_camera.set_projection_matrix(
+	camera.set_projection_matrix(
 		glm::infinitePerspective(
 			glm::degrees(fov),
 			RenderState::Size().aspectRatio(),
