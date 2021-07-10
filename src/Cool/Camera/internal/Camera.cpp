cbed497cb4f62bbeb176267fb3f3efa8c97f6de5
Jules Fouchy <jules.fouchy@ntymail.com>
2021-07-10 17:19:04 +0200

    [Camera::front_axis()] Fix the sign

--- src/Cool/Camera/internal/Camera.cpp
+++ src/Cool/Camera/internal/Camera.cpp
@@ -4,9 +4,9 @@
 
 namespace Cool {
 
-glm::vec3 Camera::right_axis() const { return glm::normalize(glm::column(_transform_matrix, 0)); }
-glm::vec3 Camera::up_axis()    const { return glm::normalize(glm::column(_transform_matrix, 1)); }
-glm::vec3 Camera::front_axis() const { return glm::normalize(glm::column(_transform_matrix, 2)); }
+glm::vec3 Camera::right_axis() const { return  glm::normalize(glm::column(_transform_matrix, 0)); }
+glm::vec3 Camera::up_axis()    const { return  glm::normalize(glm::column(_transform_matrix, 1)); }
+glm::vec3 Camera::front_axis() const { return -glm::normalize(glm::column(_transform_matrix, 2)); }
 glm::vec3 Camera::position()   const { return                glm::column(_transform_matrix, 3); }
 
 void Camera::translate(glm::vec3 const& delta_position) {
