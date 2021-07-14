35768b3f573067a26e3cb52585a0b7734ab6ac33
Jules Fouchy <jules.fouchy@ntymail.com>
2021-07-15 00:08:40 +0200

    [Camera::ViewController_Orbital] Fix formatting

--- src/Cool/Camera/ViewController_Orbital.h
+++ src/Cool/Camera/ViewController_Orbital.h
@@ -23,22 +23,12 @@ public:
     inline void set_distance_to_orbit_center(float distance) { _distance_to_orbit_center = distance; }
     inline void set_state(OrbitalState state) { _state = state; }
 
-    inline void on_mouse_move(Camera& camera, glm::vec2 const& delta)
-    {
-        std::visit([&](auto&& state) { state.on_mouse_move(*this, camera, delta); }, _state);
-    }
-    inline void on_wheel_down(Camera& camera, int mods)
-    {
-        std::visit([&](auto&& state) { state.on_wheel_down(*this, camera, mods); }, _state);
-    }
-    inline void on_wheel_up(Camera& camera)
-    {
-        std::visit([&](auto&& state) { state.on_wheel_up(*this, camera); }, _state);
-    }
-    inline void on_wheel_scroll(Camera& camera, float dl)
-    {
-        std::visit([&](auto&& state) { state.on_wheel_scroll(*this, camera, dl); }, _state);
-    }
+    // clang-format off
+    inline void on_mouse_move  (Camera& camera, glm::vec2 const& delta) { std::visit([&](auto&& state) { state.on_mouse_move  (*this, camera, delta); }, _state); }
+	inline void on_wheel_down  (Camera& camera, int mods)               { std::visit([&](auto&& state) { state.on_wheel_down  (*this, camera, mods);  }, _state); }
+	inline void on_wheel_up    (Camera& camera)                         { std::visit([&](auto&& state) { state.on_wheel_up    (*this, camera);        }, _state); }
+	inline void on_wheel_scroll(Camera& camera, float dl)               { std::visit([&](auto&& state) { state.on_wheel_scroll(*this, camera, dl);    }, _state); }
+    // clang-format on
 
 private:
     friend class OrbitalState_Idle;
