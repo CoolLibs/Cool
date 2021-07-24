5e43a7d2f407a93356ceda6d78207296e5c22023
Jules Fouchy <jules.fouchy@ntymail.com>
2021-07-24 11:44:02 +0200

    [CMake] Vookoo : fix path to find SPIR-V

--- cmake/vookoo.cmake
+++ cmake/vookoo.cmake
@@ -5,13 +5,27 @@ include_directories(
     ${Vulkan_INCLUDE_DIR}
 )
 
-# Set spir-v support
+# Try to set SPIR-V support
+function(set_spirv_support)
+    message(STATUS "[Vookoo] Found " ${SPIR_V_FILE})
+    message(STATUS "[Vookoo] Setting VOOKOO_SPIRV_SUPPORT definition")
+    include_directories(${SPIR_V_INCLUDE_DIR})
+    add_definitions(-DVOOKOO_SPIRV_SUPPORT)
+endfunction()
+
 get_filename_component(VULKAN_DIR ${Vulkan_INCLUDE_DIR} DIRECTORY)
-set(SPIR_V_INCLUDE_DIR ${VULKAN_DIR}/spirv-tools/external/spirv-headers/include/spirv/)
-set(SPIR_V_FILE ${SPIR_V_INCLUDE_DIR}unified1/spirv.hpp11)
+# Try nb 1
+set(SPIR_V_INCLUDE_DIR ${VULKAN_DIR}/spirv-tools/external/spirv-headers/include/spirv/unified1)
+set(SPIR_V_FILE ${SPIR_V_INCLUDE_DIR}/spirv.hpp11)
 if(EXISTS ${SPIR_V_FILE})
-  message(STATUS "Found " ${SPIR_V_FILE})
-  message(STATUS "Setting VOOKOO_SPIRV_SUPPORT definition")
-  include_directories(${SPIR_V_INCLUDE_DIR})
-  add_definitions(-DVOOKOO_SPIRV_SUPPORT)
+    set_spirv_support()
+else()
+# Try nb 2
+    set(SPIR_V_INCLUDE_DIR ${VULKAN_DIR}/Include/spirv-headers)
+    set(SPIR_V_FILE ${SPIR_V_INCLUDE_DIR}/spirv.hpp11)
+    if(EXISTS ${SPIR_V_FILE})
+        set_spirv_support()
+    else()
+        message(STATUS "[Vookoo] Couldn't find SPIR-V")
+    endif()
 endif()
