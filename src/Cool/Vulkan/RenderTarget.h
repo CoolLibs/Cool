39bccda4d18e5ccd4e6115bbf046a6c2e8a5c276
Jules Fouchy <jules.fouchy@ntymail.com>
2021-08-17 00:04:45 +0200

    [RenderTarget] Handle the rendering

--- src/Cool/Vulkan/RenderTarget.h
+++ src/Cool/Vulkan/RenderTarget.h
@@ -9,23 +9,26 @@ namespace Cool::Vulkan {
 class RenderTarget {
 public:
     RenderTarget(uint32_t width, uint32_t height, vk::Format format = vk::Format::eR8G8B8A8Srgb);
+
+    void render(std::function<void(vk::CommandBuffer& cb)> render_fn);
+
     RenderTargetInfo info() const;
-
-    uint32_t   width() const { return _texture.image().extent().width; }
-    uint32_t   height() const { return _texture.image().extent().height; }
-    vk::Format format() const { return _texture.image().format(); }
-
-    const Texture& texture() const { return _texture; }
+    vk::RenderPass   render_pass() const { return *_render_pass; }
+    uint32_t         width() const { return _texture.image().extent().width; }
+    uint32_t         height() const { return _texture.image().extent().height; }
+    vk::Format       format() const { return _texture.image().format(); }
+    const Texture&   texture() const { return _texture; }
 
 private:
     void build();
     void build_render_pass();
     void build_framebuffer();
 
-public:
+private:
     Texture               _texture;
     vk::UniqueRenderPass  _render_pass;
     vk::UniqueFramebuffer _framebuffer;
+    vk::CommandBuffer     _command_buffer;
 };
 
 } // namespace Cool::Vulkan
--- src/Cool/Vulkan/internal/RenderTarget.cpp
+++ src/Cool/Vulkan/internal/RenderTarget.cpp
@@ -7,7 +7,65 @@ namespace Cool::Vulkan {
 RenderTarget::RenderTarget(uint32_t width, uint32_t height, vk::Format format)
     : _texture{width, height, format}
 {
+    VkCommandBufferAllocateInfo allocInfo{};
+    allocInfo.sType              = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+    allocInfo.commandPool        = Vulkan::context().command_pool;
+    allocInfo.level              = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+    allocInfo.commandBufferCount = 1;
+
+    VkCommandBuffer _cb_;
+    if (vkAllocateCommandBuffers(Vulkan::context().g_Device, &allocInfo, &_cb_) != VK_SUCCESS) {
+        throw std::runtime_error("failed to allocate command buffers!");
+    }
+    _command_buffer = _cb_;
+
     build();
+}
+
+void RenderTarget::render(std::function<void(vk::CommandBuffer& cb)> render_fn)
+{
+    VkRenderPassBeginInfo rp_begin_info    = {};
+    rp_begin_info.sType                    = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
+    rp_begin_info.renderPass               = render_pass();
+    rp_begin_info.framebuffer              = *_framebuffer;
+    rp_begin_info.renderArea.extent.width  = width();
+    rp_begin_info.renderArea.extent.height = height();
+    rp_begin_info.clearValueCount          = 1;
+    ImVec4       clear_color               = ImVec4(1.0f, 0.0f, 0.0f, 1.0f);
+    VkClearValue ClearValue;
+    ClearValue.color.float32[0] = 1.f;
+    ClearValue.color.float32[1] = 1.f;
+    ClearValue.color.float32[2] = 0.f;
+    ClearValue.color.float32[3] = 1.f;
+    rp_begin_info.pClearValues  = &ClearValue;
+
+    vk::CommandBufferBeginInfo bi{};
+    _command_buffer.begin(bi);
+    _command_buffer.beginRenderPass(rp_begin_info, vk::SubpassContents::eInline);
+    render_fn(_command_buffer);
+    _command_buffer.endRenderPass();
+
+    {
+        VkPipelineStageFlags wait_stage = {};
+        VkSubmitInfo         info       = {};
+        info.sType                      = VK_STRUCTURE_TYPE_SUBMIT_INFO;
+        info.waitSemaphoreCount         = 0;
+        // info.pWaitSemaphores            = &image_acquired_semaphore;
+        info.pWaitDstStageMask  = &wait_stage;
+        info.commandBufferCount = 1;
+        VkCommandBuffer mcb{_command_buffer};
+        info.pCommandBuffers      = &mcb;
+        info.signalSemaphoreCount = 0;
+        // info.pSignalSemaphores          = &render_complete_semaphore;
+
+        // err = vkEndCommandBuffer(fd->CommandBuffer);
+        // Vulkan::check_result(err);
+        // err = vkQueueSubmit(Vulkan::context().g_Queue, 1, &info, fd->Fence);
+        // Vulkan::check_result(err);
+
+        _command_buffer.end();
+        vkQueueSubmit(Vulkan::context().g_Queue, 1, &info, VkFence{});
+    }
 }
 
 RenderTargetInfo RenderTarget::info() const
