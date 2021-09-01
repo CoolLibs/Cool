50ded21c1678e9b1a73f8ec9e60ec183da2b8ec9
Jules Fouchy <jules.fouchy@ntymail.com>
2021-09-01 14:26:23 +0200

    [PreviewOptions] Use ImGuiExtras::checkbox_with_submenu() for a nicer UI

--- src/Cool/PreviewOptions/PreviewOptions.cpp
+++ src/Cool/PreviewOptions/PreviewOptions.cpp
@@ -1,4 +1,5 @@
 #include "PreviewOptions.h"
+#include <Cool/ImGuiExtras/ImGuiExtras.h>
 #include <Cool/Image/ImageSizeU.h>
 
 namespace Cool {
@@ -31,22 +32,18 @@ ImageSize PreviewOptions::preview_size(ImageSize frame_size) const
 void PreviewOptions::imgui()
 {
     // Aspect Ratio
-    ImGui::Checkbox("Control aspect ratio", &_is_controlling_aspect_ratio);
-    if (_is_controlling_aspect_ratio) {
+    ImGuiExtras::checkbox_with_submenu("Control aspect ratio", &_is_controlling_aspect_ratio, [&]() {
         _aspect_ratio.ImGuiPicker(168492224155754);
-    }
+    });
     // Nb Pixels
-    ImGui::Checkbox("Control number of pixels", &_is_controlling_nb_pixels);
-    if (_is_controlling_nb_pixels) {
+    ImGuiExtras::checkbox_with_submenu("Control number of pixels", &_is_controlling_nb_pixels, [&]() {
         int previewNbPixels = _nb_pixels;
         if (ImGui::SliderInt("Number of Pixels", &previewNbPixels, 10000, 100000 /*InAppRenderArea().area()*/)) {
             _nb_pixels = previewNbPixels;
         }
-    }
+    });
     // Interpolation mode
-    if (_is_controlling_aspect_ratio || _is_controlling_nb_pixels) {
-        ImGui::Combo("Interpolation Mode", reinterpret_cast<int*>(&_interpolation_mode), "Nearest\0Linear\0\0");
-    }
+    ImGui::Combo("Interpolation Mode", reinterpret_cast<int*>(&_interpolation_mode), "Nearest\0Linear\0\0");
 }
 
 } // namespace Cool
