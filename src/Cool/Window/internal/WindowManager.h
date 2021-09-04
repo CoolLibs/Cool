410cd556b7f197875fdb4f8e08442d1f59fcea31
Jules Fouchy <jules.fouchy@ntymail.com>
2021-09-04 22:35:38 +0200

    [WindowManager] Now knows who the main window is

--- src/Cool/Window/internal/WindowManager.h
+++ src/Cool/Window/internal/WindowManager.h
@@ -7,10 +7,17 @@ namespace Cool {
 class WindowManager {
 public:
     std::list<Window>& windows() { return _windows; }
+    void               set_main_window(Window& window) { _main_window = &window; }
     Window&            find(GLFWwindow* glfw_window);
+    Window&            main_window()
+    {
+        assert(_main_window != nullptr);
+        return *_main_window;
+    }
 
 private:
     std::list<Window> _windows;
+    Window*           _main_window;
 };
 
 } // namespace Cool
