fa2fbbb5c9da0d6e508691bbc077752dde9238fa
Jules Fouchy <jules.fouchy@ntymail.com>
2021-09-04 23:35:18 +0200

    [WindowManager] Add assertions

--- src/Cool/Window/internal/WindowManager.h
+++ src/Cool/Window/internal/WindowManager.h
@@ -7,17 +7,23 @@ namespace Cool {
 class WindowManager {
 public:
     std::list<Window>& windows() { return _windows; }
-    void               set_main_window(Window& window) { _main_window = &window; }
     Window&            find(GLFWwindow* glfw_window);
-    Window&            main_window()
+
+    Window& main_window()
     {
-        assert(_main_window != nullptr);
+        assert(_main_window != nullptr && "You forgot to create a main window !");
         return *_main_window;
+    }
+
+    void set_main_window(Window& window)
+    {
+        assert(_main_window == nullptr && "You can only have one main window !");
+        _main_window = &window;
     }
 
 private:
     std::list<Window> _windows;
-    Window*           _main_window;
+    Window*           _main_window = nullptr;
 };
 
 } // namespace Cool
