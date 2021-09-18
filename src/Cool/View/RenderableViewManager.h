359c372124adde6fd7e697d342f8667a0406e20a
Jules Fouchy <jules.fouchy@ntymail.com>
2021-09-18 17:53:02 +0200

    [RenderableViewManager] Add iterators

--- src/Cool/View/RenderableViewManager.h
+++ src/Cool/View/RenderableViewManager.h
@@ -7,6 +7,10 @@ namespace Cool {
 class RenderableViewManager {
 public:
     RenderableView& make_view(std::string_view name);
+    auto            begin() { return _views.begin(); }
+    auto            end() { return _views.end(); }
+    auto            begin() const { return _views.begin(); }
+    auto            end() const { return _views.end(); }
 
 private:
     std::list<RenderableView> _views;
