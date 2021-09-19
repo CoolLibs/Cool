d0c6c5fa07f271887879ab57c6543db56070703a
Jules Fouchy <jules.fouchy@ntymail.com>
2021-09-19 10:35:40 +0200

    [MouseDragManager] Remember which button started the drag

--- src/Cool/Events/MouseDragManager.h
+++ src/Cool/Events/MouseDragManager.h
@@ -15,18 +15,18 @@ public:
 
     void receive_mouse_button_event(const MouseButtonEvent<ImGuiWindowCoordinates>& event, bool is_inside_view)
     {
-        if (event.action == GLFW_PRESS && is_inside_view && !_is_dragging) {
-            _is_dragging = true;
+        if (event.action == GLFW_PRESS && is_inside_view && !_dragged_button.has_value()) {
+            _dragged_button.emplace(event.button);
             start().receive({event.position});
         }
-        if (event.action == GLFW_RELEASE && _is_dragging) {
-            _is_dragging = false;
+        if (event.action == GLFW_RELEASE && _dragged_button.has_value() && *_dragged_button == event.button) {
+            _dragged_button.reset();
             stop().receive({event.position});
         }
     }
 
 private:
-    bool                                          _is_dragging = false;
+    std::optional<int>                            _dragged_button{};
     EventDispatcher<MouseDragStartEvent<Coords>>  _drag_start_dispatcher;
     EventDispatcher<MouseDragUpdateEvent<Coords>> _drag_update_dispatcher;
     EventDispatcher<MouseDragStopEvent<Coords>>   _drag_stop_dispatcher;
