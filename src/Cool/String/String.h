94c993b4bfbd0d88764e5acc65a6d4a007771b72
Jules Fouchy <jules.fouchy@ntymail.com>
2021-10-04 17:31:06 +0200

    [String] Fix indentation

--- src/Cool/String/String.h
+++ src/Cool/String/String.h
@@ -2,37 +2,37 @@
 
 namespace Cool::String {
 /**
-	 * @brief 
-	 * 
-	 * @param str 
-	 * @return A new string with all characters converted to lower case
-	 */
+ * @brief 
+ * 
+ * @param str 
+ * @return A new string with all characters converted to lower case
+ */
 std::string to_lower(std::string_view str);
 
 /**
-	 * @brief 
-	 * 
-	 * @param to_find 
-	 * @param str 
-	 * @return true iff *str* starts with *to_find*
-	 */
+ * @brief 
+ * 
+ * @param to_find 
+ * @param str 
+ * @return true iff *str* starts with *to_find*
+ */
 bool starts_with(std::string_view to_find, std::string_view str);
 
 /**
-	 * @brief Modifies *str* by replacing all occurences of *from* with *to*
-	 * 
-	 * @param str 
-	 * @param from 
-	 * @param to 
-	 */
+ * @brief Modifies *str* by replacing all occurences of *from* with *to*
+ * 
+ * @param str 
+ * @param from 
+ * @param to 
+ */
 void replace_all(std::string& str, std::string_view from, std::string_view to);
 
 /**
-	 * @brief Converts a number to a string. Adds 0s to the left until the size of the string is greater or equal to min_nb_of_characters.
-	 * 
-	 * @param val The number to convert to a string.
-	 * @param min_nb_of_characters The minimum number of characters that will be present in the output string.
-	 */
+ * @brief Converts a number to a string. Adds 0s to the left until the size of the string is greater or equal to min_nb_of_characters.
+ * 
+ * @param val The number to convert to a string.
+ * @param min_nb_of_characters The minimum number of characters that will be present in the output string.
+ */
 template<typename T>
 std::string to_string(T val, int min_nb_of_characters = 0)
 {
