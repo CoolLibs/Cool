d08943419b98278be1f082ffe644578396c5ba67
Jules Fouchy <jules.fouchy@ntymail.com>
2021-10-05 13:10:22 +0200

    [Core::BuildMode] Add

---
+++ src/Cool/Core/BuildMode.h
@@ -0,0 +1,38 @@
+#pragma once
+
+namespace Cool {
+
+enum class BuildMode {
+    Debug,
+    Release,
+    Any,
+    None,
+};
+
+[[nodiscard]] inline constexpr bool is_building_as(BuildMode build_mode)
+{
+    switch (build_mode) {
+    case BuildMode::Debug:
+#if defined(DEBUG)
+        return true;
+#else
+        return false;
+#endif
+    case BuildMode::Release:
+#if defined(DEBUG)
+        return false;
+#else
+        return true;
+#endif
+    case BuildMode::Any:
+        return true;
+    case BuildMode::None:
+        return false;
+    default: {
+        Log::error("[is_building_as] Unknown enum value");
+        return false;
+    }
+    }
+}
+
+} // namespace Cool
