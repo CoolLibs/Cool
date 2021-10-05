703285e9db67b2ae71aaced63ec319f56cd509a0
Jules Fouchy <jules.fouchy@ntymail.com>
2021-10-05 21:00:24 +0200

    [WindowConfig] Replace BuildMode with bool to be more flexible

--- src/Cool/Window/internal/WindowFactoryU.cpp
+++ src/Cool/Window/internal/WindowFactoryU.cpp
@@ -4,11 +4,11 @@ namespace Cool::WindowFactoryU {
 
 void apply_config(const WindowConfig& config, Window& window)
 {
-    window.cap_framerate_if(is_building_as(config.cap_framerate_on_startup_if));
-    if (is_building_as(config.maximize_on_startup_if)) {
+    window.cap_framerate_if(config.cap_framerate_on_startup_if);
+    if (config.maximize_on_startup_if) {
         glfwMaximizeWindow(window.glfw());
     }
-    if (is_building_as(config.hide_on_startup_if)) {
+    if (config.hide_on_startup_if) {
         window.set_visibility(false);
     }
 }
