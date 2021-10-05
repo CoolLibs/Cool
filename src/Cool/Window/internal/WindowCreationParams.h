7f1f289c6420beb4b08a4aaea1fc6bd36189a219
Jules Fouchy <jules.fouchy@ntymail.com>
2021-10-05 13:30:08 +0200

    [WindowCreationParams] Add parameters

--- src/Cool/Window/internal/WindowCreationParams.h
+++ src/Cool/Window/internal/WindowCreationParams.h
@@ -1,12 +1,15 @@
 #pragma once
+#include <Cool/Core/BuildMode.h>
 
 namespace Cool {
 
 struct WindowCreationParams {
-    const char* title;                // The title that will be displayed in the title bar of the window
-    int         width         = 1280; // Initial width of the window
-    int         height        = 720;  // Initial height of the window
-    bool        cap_framerate = true; // Whether the framerate should be capped or not (this can later be changed at runtime through window.cap_framerate())
+    const char* title;                                       // The title that will be displayed in the title bar of the window
+    int         width                  = 1280;               // Initial width of the window
+    int         height                 = 720;                // Initial height of the window
+    BuildMode   cap_framerate_if       = BuildMode::Any;     // Whether the framerate should be capped or not (this can later be changed at runtime through window.cap_framerate())
+    BuildMode   maximize_on_startup_if = BuildMode::Release; // Whether the window will be maximized when the application starts.
+    BuildMode   hide_on_startup_if     = BuildMode::None;    // Whether the window will be hidden when the application starts.
 };
 
 } // namespace Cool
--- src/Cool/Window/internal/WindowFactoryU.cpp
+++ src/Cool/Window/internal/WindowFactoryU.cpp
@@ -5,6 +5,12 @@ namespace Cool::WindowFactoryU {
 void apply_config(Window& window, WindowCreationParams config)
 {
     window.cap_framerate(is_building_as(config.cap_framerate_if));
+    if (is_building_as(config.maximize_on_startup_if)) {
+        glfwMaximizeWindow(window.glfw());
+    }
+    if (is_building_as(config.hide_on_startup_if)) {
+        window.set_visibility(false);
+    }
 }
 
 } // namespace Cool::WindowFactoryU
