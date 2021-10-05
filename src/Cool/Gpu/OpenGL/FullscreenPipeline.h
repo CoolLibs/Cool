3f7ced5f662d2e38f27be7cdb2acf419f1beac87
Jules Fouchy <jules.fouchy@ntymail.com>
2021-10-05 18:07:25 +0200

    [OpenGL::FullscreenPipeline] Allow to recompile shaders on the fly

--- src/Cool/Gpu/OpenGL/FullscreenPipeline.h
+++ src/Cool/Gpu/OpenGL/FullscreenPipeline.h
@@ -12,6 +12,9 @@ public:
     FullscreenPipeline& operator=(const FullscreenPipeline&) = delete; // non-copyable because we store a handle to a VAO
     ~FullscreenPipeline();
 
+    void recompile_shader_from_file(std::string_view fragment_shader_path);
+    void recompile_shader_from_source(std::string_view fragment_source_code);
+
     Shader& shader() { return _shader; }
     void    draw();
 
--- src/Cool/Gpu/OpenGL/internal/FullscreenPipeline.cpp
+++ src/Cool/Gpu/OpenGL/internal/FullscreenPipeline.cpp
@@ -17,6 +17,18 @@ FullscreenPipeline::~FullscreenPipeline()
     GLDebug(glDeleteVertexArrays(1, &_dummy_vao_id));
 }
 
+void FullscreenPipeline::recompile_shader_from_file(std::string_view fragment_shader_path)
+{
+    _shader = Shader{File::root_dir() + "/Cool/res/shaders/fullscreen.vert",
+                     fragment_shader_path};
+}
+
+void FullscreenPipeline::recompile_shader_from_source(std::string_view fragment_source_code)
+{
+    _shader = Shader{{ShaderSource{File::to_string(File::root_dir() + "/Cool/res/shaders/fullscreen.vert"), ShaderKind::Vertex},
+                      ShaderSource{fragment_source_code, ShaderKind::Fragment}}};
+}
+
 void FullscreenPipeline::draw()
 {
     // We use a smart trick to render fullscreen, as explained here : https://stackoverflow.com/a/59739538
