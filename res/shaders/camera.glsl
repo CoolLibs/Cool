6e3575b9e4f028bcc34a04988e8335f33350b56c
Jules Fouchy <jules.fouchy@ntymail.com>
2021-10-08 18:00:15 +0200

    [camera.glsl] Add

---
+++ res/shaders/camera.glsl
@@ -0,0 +1,25 @@
+uniform mat4 cool_camera_transform;
+uniform mat4 cool_camera_inverse_projection;
+
+vec3 __apply_camera(vec3 pos)
+{
+    vec4 v = (cool_camera_transform * cool_camera_inverse_projection * vec4(pos, 1.));
+    return v.xyz / v.w;
+}
+
+vec2 __point_on_face()
+{
+    return mix(vec2(-1.), vec2(1.), vTexCoords);
+}
+
+vec3 cool_ray_origin()
+{
+    return __apply_camera(vec3(__point_on_face(), -1.));
+}
+
+vec3 cool_ray_direction()
+{
+    vec3 begin = vec3(__point_on_face(), -1.);
+    vec3 end   = vec3(__point_on_face(), 0.);
+    return normalize(__apply_camera(end) - __apply_camera(begin));
+}
