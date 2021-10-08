00f6ec52c22817bc5557678bc1f864580a573fba
Jules Fouchy <jules.fouchy@ntymail.com>
2021-10-08 17:55:54 +0200

    [RegExp] Add pattern for #include

---
+++ src/Cool/RegExp/RegExp.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <regex>
+
+namespace Cool::RegExp {
+
+static const std::regex includes{R"(#include *\"(.*)\")"};
+
+auto file_path_to_include(const std::string& text) -> std::optional<std::string>
+{
+    std::smatch matches;
+    if (std::regex_search(text, matches, includes)) {
+        return matches[1];
+    }
+    else {
+        return std::nullopt;
+    }
+}
+
+} // namespace Cool::RegExp
+
+TEST_CASE("[RegularExpressions] #include")
+{
+    // Given
+    const auto text =
+        "some random text;\n"
+        "#include   \"filename.ext\"ranbom_bits\n"
+        "more randomness\n";
+    // When
+    const auto result = Cool::RegExp::file_path_to_include(text);
+    // Then
+    CHECK(result.has_value());
+    CHECK(*result == "filename.ext");
+}
