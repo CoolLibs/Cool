96dd8c22a87efac2b6af4a039b29057c5e2b2574
Jules Fouchy <jules.fouchy@ntymail.com>
2021-10-10 00:15:01 +0200

    [OpenGL::FullscreenPipeline] Receive name as a string_view

--- src/Cool/Gpu/OpenGL/FullscreenPipeline.cpp
+++ src/Cool/Gpu/OpenGL/FullscreenPipeline.cpp
@@ -6,15 +6,15 @@
 
 namespace Cool::OpenGL {
 
-FullscreenPipeline::FullscreenPipeline(std::string_view fragment_shader_source_code, const std::string& name)
+FullscreenPipeline::FullscreenPipeline(std::string_view fragment_shader_source_code, std::string_view name)
 {
     compile(fragment_shader_source_code, name);
 }
 
-void FullscreenPipeline::compile(std::string_view fragment_shader_source_code, const std::string& name)
+void FullscreenPipeline::compile(std::string_view fragment_shader_source_code, std::string_view name)
 {
     _shader = try_make_shader({ShaderDescription{File::to_string(File::root_dir() + "/Cool/res/shaders/fullscreen.vert"), ShaderKind::Vertex, "Cool's fullscreen vertex shader"},
-                               ShaderDescription{std::string{fragment_shader_source_code}, ShaderKind::Fragment, name}});
+                               ShaderDescription{std::string{fragment_shader_source_code}, ShaderKind::Fragment, std::string{name}}});
 }
 
 void FullscreenPipeline::draw() const
--- src/Cool/Gpu/OpenGL/FullscreenPipeline.h
+++ src/Cool/Gpu/OpenGL/FullscreenPipeline.h
@@ -12,13 +12,13 @@ public:
     /**
      * @param name The name that will be displayed in the error message if the compilation fails 
      */
-    FullscreenPipeline(std::string_view fragment_shader_source_code, const std::string& name);
+    FullscreenPipeline(std::string_view fragment_shader_source_code, std::string_view name);
 
     /**
      * @brief Compiles the fragment shader source code and creates a new pipeline using this new shader.
      * @param name The name that will be displayed in the error message if the compilation fails 
      */
-    void compile(std::string_view fragment_shader_source_code, const std::string& name);
+    void compile(std::string_view fragment_shader_source_code, std::string_view name);
     void draw() const;
     auto shader() const -> const std::optional<Shader>& { return _shader; }
 
