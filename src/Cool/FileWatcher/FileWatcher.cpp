9cf8b73f9edc398dbe639ed43583dafc89c960ee
Jules Fouchy <jules.fouchy@ntymail.com>
2021-10-10 23:11:10 +0200

    [FileWatcher] Cleanup ; callback on path invalid ; properly calls callbacks only once

--- src/Cool/FileWatcher/FileWatcher.cpp
+++ src/Cool/FileWatcher/FileWatcher.cpp
@@ -1,56 +1,85 @@
 #include "FileWatcher.h"
 #include <Cool/File/File.h>
-#include <Cool/Log/ToUser.h>
 
 namespace Cool {
 
-FileWatcher::FileWatcher(std::function<void(std::string_view)> on_file_changed, float delay_between_checks)
-    : _on_file_changed(on_file_changed), _delay_between_checks(delay_between_checks)
+static bool is_valid_file_path(std::filesystem::path path)
+{
+    return File::exists(path.string()) && !std::filesystem::is_directory(path);
+}
+
+FileWatcher::FileWatcher(std::filesystem::path                 path,
+                         std::function<void(std::string_view)> on_file_changed_cb,
+                         std::function<void(std::string_view)> on_path_invalid_cb,
+                         float                                 delay_between_checks)
+    : _path{path}
+    , _time_of_last_change{std::filesystem::last_write_time(_path)}
+    , _on_file_changed{on_file_changed_cb}
+    , _on_path_invalid{on_path_invalid_cb}
+    , _delay_between_checks{delay_between_checks}
 {
 }
 
-void FileWatcher::update()
+void FileWatcher::update() const
 {
-    check_and_update_path_validity();
-    if (_path_is_valid) {
+    std::visit([&](auto&& validity) { update_validity(validity, is_valid_file_path(_path)); }, _path_validity);
+    if (path_is_valid()) {
         // Init static variable (it is static so that we don't need to include chrono in the header file)
         static auto _time_of_last_check = std::chrono::steady_clock::now();
         // Wait for delay between checks
-        auto                         now          = std::chrono::steady_clock::now();
+        const auto                   now          = std::chrono::steady_clock::now();
         std::chrono::duration<float> elapsed_time = now - _time_of_last_check;
         if (elapsed_time.count() > _delay_between_checks) {
             _time_of_last_check = now;
-            // Check file exists
-            if (File::exists(_path.string())) {
-                // Check file was updated since last check
-                auto last_change = std::filesystem::last_write_time(_path);
-                if (last_change != _time_of_last_change) {
-                    // Apply
-                    _time_of_last_change = last_change;
-                    _on_file_changed(_path.string());
-                }
+            // Check file was updated since last check
+            auto last_change = std::filesystem::last_write_time(_path);
+            if (last_change != _time_of_last_change) {
+                on_file_changed();
             }
         }
     }
 }
 
+void FileWatcher::on_file_changed() const
+{
+    _path_validity       = Valid{};
+    _time_of_last_change = std::filesystem::last_write_time(_path);
+    _on_file_changed(_path.string());
+}
+
+void FileWatcher::on_path_invalid() const
+{
+    _path_validity = Invalid{};
+    _on_path_invalid(_path.string());
+}
+
 void FileWatcher::set_path(std::string_view path)
 {
     _path          = path;
-    _path_is_valid = false; // Forces check_and_update_path_validity() to trigger an update (if the new path is valid)
-    check_and_update_path_validity();
-    if (!_path_is_valid) {
-        Log::ToUser::error("FileWatcher::set_path", "Invalid file path : \"{}\"", path);
+    _path_validity = Unknown{};
+}
+
+void FileWatcher::update_validity(Valid, bool is_valid) const
+{
+    if (!is_valid) {
+        on_path_invalid();
     }
 }
 
-void FileWatcher::check_and_update_path_validity()
+void FileWatcher::update_validity(Invalid, bool is_valid) const
 {
-    bool was_valid = _path_is_valid;
-    _path_is_valid = File::exists(_path.string()) && !std::filesystem::is_directory(_path);
-    if (!was_valid && _path_is_valid) {
-        _time_of_last_change = std::filesystem::last_write_time(_path);
-        _on_file_changed(_path.string());
+    if (is_valid) {
+        on_file_changed();
+    }
+}
+
+void FileWatcher::update_validity(Unknown, bool is_valid) const
+{
+    if (is_valid) {
+        on_file_changed();
+    }
+    else {
+        on_path_invalid();
     }
 }
 
--- src/Cool/FileWatcher/FileWatcher.h
+++ src/Cool/FileWatcher/FileWatcher.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <Cool/Log/ToUser.h>
 #include <filesystem>
 
 namespace Cool {
@@ -16,44 +17,56 @@ public:
 	 * @param on_file_changed Callback function called whenever the file changes. Receives the path as parameter, just for convenience
 	 * @param In seconds : time between two checks. The smaller the delay the bigger the performance cost 
 	 */
-    FileWatcher(std::function<void(std::string_view)> on_file_changed, float delay_between_checks = 0.5f);
+    explicit FileWatcher(
+        std::filesystem::path                 path,
+        std::function<void(std::string_view)> on_file_changed,
+        std::function<void(std::string_view)> on_path_invalid      = [](auto path) { Log::ToUser::error("FileWatcher", "Invalid file path : \"{}\"", path); },
+        float                                 delay_between_checks = 0.5f);
 
     /**
 	 * @brief Must be called on every frame. Checks if the delay is elapsed, if so checks if the file has changed, and if so calls the callback
-	 * 
 	 */
-    void update();
+    void update() const;
 
     /**
-	 * @brief 
-	 * 
-	 * @return The path to the file we are currently watching
+	 * @brief The path we are currently watching
 	 */
-    inline const std::filesystem::path path() const { return _path; }
+    auto path() const -> std::filesystem::path { return _path; }
 
     /**
-	 * @brief Sets the path to watch, and calls the on_file_changed callback if the path is valid
-	 * 
-	 * @param path Path of the file to watch
+     * @brief Whether the path we are watching is valid
+     */
+    auto path_is_valid() const -> bool { return std::holds_alternative<Valid>(_path_validity); };
+
+    /**
+	 * @brief Sets the path to watch.
+     * The appropriate callback will then be called in the next update(), depending on the path validity (either on_file_changed() or on_path_invalid())
 	 */
     void set_path(std::string_view path);
 
-    /**
-	 * @brief 
-	 * 
-	 * @return true iff we are currently watching a valid file path (meaning the file exists and we are allowed to read it)
-	 */
-    inline bool path_is_valid() const { return _path_is_valid; }
+private:
+    void on_file_changed() const;
+    void on_path_invalid() const;
 
 private:
-    void check_and_update_path_validity();
+    struct Valid {
+    };
+    struct Invalid {
+    };
+    struct Unknown {
+    };
+    using PathValidity = std::variant<Valid, Invalid, Unknown>;
+    void update_validity(Valid, bool is_valid) const;
+    void update_validity(Invalid, bool is_valid) const;
+    void update_validity(Unknown, bool is_valid) const;
 
 private:
-    std::filesystem::path                 _path          = "";
-    bool                                  _path_is_valid = false;
-    std::function<void(std::string_view)> _on_file_changed;
-    float                                 _delay_between_checks;
-    std::filesystem::file_time_type       _time_of_last_change;
+    std::filesystem::path                   _path;
+    mutable PathValidity                    _path_validity = Unknown{};
+    mutable std::filesystem::file_time_type _time_of_last_change;
+    std::function<void(std::string_view)>   _on_file_changed;
+    std::function<void(std::string_view)>   _on_path_invalid;
+    float                                   _delay_between_checks;
 };
 
 } // namespace Cool
