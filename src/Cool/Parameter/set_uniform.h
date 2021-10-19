45e6fe6021ee3f875934ae452322a33fc693bddc
Jules Fouchy <jules.fouchy@ntymail.com>
2021-10-20 00:17:32 +0200

    [Parameter] Rename set_uniform() as set_uniforms()

--- src/Cool/Parameter/set_uniform.h
+++ src/Cool/Parameter/set_uniform.h
@@ -12,7 +12,7 @@ void set_uniform(const OpenGL::Shader& shader, const Param& param)
     shader.set_uniform(param.name(), *param);
 }
 
-void set_uniform(const OpenGL::Shader& shader, const ParameterList& list)
+void set_uniforms(const OpenGL::Shader& shader, const ParameterList& list)
 {
     for (const auto& param : *list) {
         std::visit([&](auto&& param) { set_uniform(shader, param); }, param);
