6f2ee1d62858f5c51fce30881eaa3e571068b0ec
Jules Fouchy <jules.fouchy@ntymail.com>
2021-11-02 15:52:17 +0100

    [ParameterU] Rename is_matching_type() as is_matching_desc_type()

--- src/Cool/Parameter/ParameterU.cpp
+++ src/Cool/Parameter/ParameterU.cpp
@@ -2,9 +2,9 @@
 
 namespace Cool::ParameterU {
 
-bool is_matching_type(const Parameter::Any& param, const Parameter::AnyDesc& desc)
+bool is_matching_desc_type(const Parameter::Any& param, const Parameter::AnyDesc& desc)
 {
-    return std::visit([&](auto&& param) { return is_matching_type2(param, desc); }, param);
+    return std::visit([&](auto&& param) { return is_matching_desc_type(param, desc); }, param);
 }
 
 std::optional<size_t> index_of(const Parameter::AnyDesc& description, const ParameterList& list)
@@ -12,7 +12,7 @@ std::optional<size_t> index_of(const Parameter::AnyDesc& description, const Para
     for (size_t i = 0; i < list->size(); ++i) {
         const std::string& name      = std::visit([&](auto&& param) { return param.name(); }, (*list)[i]);
         const std::string& desc_name = std::visit([&](auto&& desc) { return desc.name; }, description);
-        if (desc_name == name && is_matching_type((*list)[i], description)) {
+        if (desc_name == name && is_matching_desc_type((*list)[i], description)) {
             return i;
         }
     }
@@ -21,9 +21,9 @@ std::optional<size_t> index_of(const Parameter::AnyDesc& description, const Para
 
 } // namespace Cool::ParameterU
 
-TEST_CASE("[Cool::ParameterU] is_matching_type()")
+TEST_CASE("[Cool::ParameterU] is_matching_desc_type()")
 {
-    CHECK(Cool::ParameterU::is_matching_type(Cool::Parameter::Float{}, Cool::Parameter::FloatDesc{}));
-    CHECK(Cool::ParameterU::is_matching_type(Cool::Parameter::Color{}, Cool::Parameter::ColorDesc{}));
-    CHECK(!Cool::ParameterU::is_matching_type(Cool::Parameter::Float{}, Cool::Parameter::ColorDesc{}));
+    CHECK(Cool::ParameterU::is_matching_desc_type(Cool::Parameter::Float{}, Cool::Parameter::FloatDesc{}));
+    CHECK(Cool::ParameterU::is_matching_desc_type(Cool::Parameter::Color{}, Cool::Parameter::ColorDesc{}));
+    CHECK(!Cool::ParameterU::is_matching_desc_type(Cool::Parameter::Float{}, Cool::Parameter::ColorDesc{}));
 }
--- src/Cool/Parameter/ParameterU.h
+++ src/Cool/Parameter/ParameterU.h
@@ -4,17 +4,17 @@
 
 namespace Cool::ParameterU {
 
-template<typename Param>
+template<ParameterType Param>
 Param make_param(const ParameterList& previous_parameters, const typename Param::Description& desc)
 {
 }
 
 std::optional<size_t> index_of(const Parameter::AnyDesc& description, const ParameterList& list);
 
-bool is_matching_type(const Parameter::Any& param, const Parameter::AnyDesc& desc);
+bool is_matching_desc_type(const Parameter::Any& param, const Parameter::AnyDesc& desc);
 
-template<typename Param>
-bool is_matching_type2(const Param& param, const Parameter::AnyDesc& desc)
+template<ParameterType Param>
+bool is_matching_desc_type(const Param& param, const Parameter::AnyDesc& desc)
 {
     return std::holds_alternative<typename Param::Description>(desc);
 }
