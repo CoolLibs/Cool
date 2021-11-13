bb78e21650f6a5dcc890b51f38387445fe1851b5
Jules Fouchy <jules.fouchy@ntymail.com>
2021-11-13 18:28:00 +0100

    [CMake::copy_to_output()] Fix: the files were not copied when the main target was launched

--- cmake/copy_to_output.cmake
+++ cmake/copy_to_output.cmake
@@ -1,11 +1,17 @@
+# NB: these functions require policy CMP0112 which was set to NEW in CMake 3.19 (https://cmake.org/cmake/help/latest/policy/CMP0112.html#policy:CMP0112)
+# If you are using a version of CMake lower than 3.19 then you need to call `cmake_policy(SET CMP0112 NEW)` just after `cmake_minimum_required(...)` in your main CMakeLists.txt file
+# Otherwise you will get a "Cyclic dependencies" error
+
 function(internal_cool_copy FILE_OR_DIR COMMAND_NAME)
-    string(SHA1 TARGET_HASH ${FILE_OR_DIR}) # Create a unique name for our target. We can't use ${FILE_OR_DIR} directly because it could contain invalid characters like spaces
-    add_custom_target(CopyToOutput_${TARGET_HASH} ALL
+    string(SHA1 TARGET_HASH ${FILE_OR_DIR})      # Create a unique name for our target. We can't use ${FILE_OR_DIR} directly because it could contain invalid characters like spaces
+    set(TARGET_NAME CopyToOutput_${TARGET_HASH}) #
+    add_custom_target(${TARGET_NAME}
         COMMENT "Copying \"${FILE_OR_DIR}\""
         COMMAND ${CMAKE_COMMAND} -E ${COMMAND_NAME}
             ${CMAKE_SOURCE_DIR}/${FILE_OR_DIR}
             $<TARGET_FILE_DIR:${PROJECT_NAME}>/${FILE_OR_DIR}
     )
+    add_dependencies(${PROJECT_NAME} ${TARGET_NAME}) # Mark our custom target as a dependency of the main project, so that whenever the main project is launched our custom target is triggered and files are copied
 endfunction()
 
 function(copy_file_to_output FILENAME)
