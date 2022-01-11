759fb36f1b11038436e8f7256d8a6de6a017545f
Jules Fouchy <jules.fouchy@ntymail.com>
2022-01-11 22:50:00 +0100

    [res::shaders] Add math.glsl

---
+++ res/shaders/math.glsl
@@ -0,0 +1,29 @@
+// See https://www.iquilezles.org/www/articles/smin/smin.htm
+float smooth_min_polynomial(float a, float b, float smoothing)
+{
+    float h = max(smoothing - abs(a - b), 0.0) / smoothing;
+    return min(a, b) - h * h * smoothing * 0.25;
+}
+
+// We use the fact that `smax(a, b) = - smin(-a, -b)` and reuse the formula above for smin
+float smooth_max_polynomial(float a, float b, float smoothing)
+{
+    float h = max(smoothing - abs(a - b), 0.0) / smoothing;
+    return max(a, b) + h * h * smoothing * 0.25;
+}
+
+float ndot(vec2 a, vec2 b)
+{
+    return a.x * b.x - a.y * b.y;
+}
+
+float hash(vec3 x)
+{
+    // based on: pcg3 by Mark Jarzynski: http://www.jcgt.org/published/0009/03/02/
+    uvec3 v = uvec3(x * 8192.0) * 1664525u + 1013904223u;
+    v += v.yzx * v.zxy;
+    v ^= v >> 16u;
+    return float(v.x + v.y * v.z) * (1.0 / float(0xffffffffu));
+}
+
+#define saturate(v) clamp(v, 0., 1.)
