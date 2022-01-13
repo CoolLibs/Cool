c69b054a240fce3b5563b5fa057764f99ebb83a1
Sujet Test <atiusem@gmail.com>
2022-01-13 09:39:19 +0100

    [res::shaders] Add new hash function
    Rename both hash funtion to describe their output

--- res/shaders/math.glsl
+++ res/shaders/math.glsl
@@ -17,13 +17,22 @@ float ndot(vec2 a, vec2 b)
     return a.x * b.x - a.y * b.y;
 }
 
-float hash(vec3 x)
+float hash_0_to_1(vec3 x)
 {
     // based on: pcg3 by Mark Jarzynski: http://www.jcgt.org/published/0009/03/02/
     uvec3 v = uvec3(x * 8192.0) * 1664525u + 1013904223u;
     v += v.yzx * v.zxy;
     v ^= v >> 16u;
     return float(v.x + v.y * v.z) * (1.0 / float(0xffffffffu));
+}
+
+vec3 hash_minus_1_to_1(vec3 p)
+{
+    p = vec3(dot(p, vec3(127.1, 311.7, 74.7)),
+             dot(p, vec3(269.5, 183.3, 246.1)),
+             dot(p, vec3(113.5, 271.9, 124.6)));
+
+    return -1.0 + 2.0 * fract(sin(p) * 43758.5453123);
 }
 
 #define saturate(v) clamp(v, 0., 1.)
