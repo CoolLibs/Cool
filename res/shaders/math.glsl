703f8655937cee2fa86c4e40cc3f076be076a0a7
Jules Fouchy <jules.fouchy@ntymail.com>
2022-01-17 10:33:04 +0100

    [math.glsl] Rename hash as hash_0_to_1 and change implementation

--- res/shaders/math.glsl
+++ res/shaders/math.glsl
@@ -17,13 +17,11 @@ float ndot(vec2 a, vec2 b)
     return a.x * b.x - a.y * b.y;
 }
 
-float hash(vec3 x)
+float hash_0_to_1(vec3 p)
 {
-    // based on: pcg3 by Mark Jarzynski: http://www.jcgt.org/published/0009/03/02/
-    uvec3 v = uvec3(x * 8192.0) * 1664525u + 1013904223u;
-    v += v.yzx * v.zxy;
-    v ^= v >> 16u;
-    return float(v.x + v.y * v.z) * (1.0 / float(0xffffffffu));
+    p = fract(p * .1031);
+    p += dot(p, p.zyx + 31.32);
+    return fract((p.x + p.y) * p.z);
 }
 
 #define saturate(v) clamp(v, 0., 1.)
