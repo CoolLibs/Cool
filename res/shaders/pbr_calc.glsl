7277a2fcac3ee6812c5b3623f858fa83b0b61e44
Jules Fouchy <jules.fouchy@ntymail.com>
2022-01-27 16:01:09 +0100

    [pbr_calc] Remove AmbReflect()

--- res/shaders/pbr_calc.glsl
+++ res/shaders/pbr_calc.glsl
@@ -1,19 +1,19 @@
 float G1V(float NdotV, float k)
 {
-    return 1.0/(NdotV*(1.0-k)+k); 
+    return 1.0 / (NdotV * (1.0 - k) + k);
 }
 
 float fresnel(vec3 normal, float metallic)
 {
-    float NdotV = clamp(dot(normal, vec3(0.0,0.0,-1.0)), 0.0, 1.0);
-    NdotV = pow(1.0 - NdotV, 5.0);
-    return metallic + ( 1.0 - metallic ) * (NdotV);
+    float NdotV = clamp(dot(normal, vec3(0.0, 0.0, -1.0)), 0.0, 1.0);
+    NdotV       = pow(1.0 - NdotV, 5.0);
+    return metallic + (1.0 - metallic) * (NdotV);
 }
 
 float Fresnel1Term(float LdotH, float metallic)
 {
     float LdotH5 = 1.0 - LdotH;
-    LdotH5 = LdotH5*LdotH5*LdotH5*LdotH5*LdotH5;
+    LdotH5       = LdotH5 * LdotH5 * LdotH5 * LdotH5 * LdotH5;
     return metallic + (1.0 - metallic) * (LdotH5);
 }
 
@@ -33,17 +33,12 @@ float GeoTerm(float roughtness, float NdotH)
 
 float specular(vec3 normal, vec3 lightdir, float roughtness, float metallic)
 {
-    vec3 v = vec3(0.0,0.0,-1.0);
-    vec3 h = normalize(v + lightdir);
+    vec3  v     = vec3(0.0, 0.0, -1.0);
+    vec3  h     = normalize(v + lightdir);
     float NdotL = clamp(dot(normal, lightdir), 0.0, 1.0);
     float NdotV = clamp(dot(normal, v), 0.0, 1.0);
     float NdotH = clamp(dot(normal, h), 0.0, 1.0);
     float LdotH = clamp(dot(lightdir, h), 0.0, 1.0);
 
     return NdotL * GeoTerm(roughtness, NdotH) * Fresnel1Term(LdotH, metallic) * VisibilityTerm(roughtness, NdotV, NdotL);
-}
-
-vec3 AmbReflect()
-{
-    return vec3(.10,.10,.10);
 }
