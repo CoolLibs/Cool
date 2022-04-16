d011b779d790a110370dda2cefb418008885efb5
Jules Fouchy <jules.fouchy@ntymail.com>
2022-04-16 23:34:13 +0200

    [InitConfig] Use exe_path to get the root path in release

--- src/Cool/Core/InitConfig.h
+++ src/Cool/Core/InitConfig.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <Cool/Path/Path.h>
+#include <exe_path/exe_path.h>
 #include <filesystem>
 #if defined(DEBUG)
 #define DOCTEST_CONFIG_IMPLEMENT
@@ -24,7 +25,7 @@ struct InitConfig {
 
     /// Sets the paths to the root directory and to Cool/res
     std::function<void()> set_paths = []() {
-#if defined(DEBUG)
+#if DEBUG
         // When launching from an IDE (which almost always means we are in Debug mode)
         // the initial current_path will be set to bin/Debug
         // but I prefer to have it at the root of the project, so that the assets we load
@@ -32,10 +33,12 @@ struct InitConfig {
         // This means that if for example we do hot reloading of shaders
         // we can modify the source assets (at the root), instead of the ones that have been copied.
         // This is important because only the ones at the root are tracked in Git.
-        std::filesystem::current_path(COOL_DEBUG_ONLY__CMAKE_SOURCE_DIR);
+        std::string root{COOL_DEBUG_ONLY__CMAKE_SOURCE_DIR};
+#else
+        std::string root{exe_path::dir().string()};
 #endif
-        Cool::Path::initialize_root(std::filesystem::current_path().string());
-        Cool::Path::initialize_cool_res(std::filesystem::current_path().string() + "/" + COOL_PATH + "/res");
+        Cool::Path::initialize_root(root);
+        Cool::Path::initialize_cool_res(root + "/" + COOL_PATH + "/res");
     };
 };
 
