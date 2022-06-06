2c74f3f99aeb72f6fc05db3702ce2c2de4467f24
Jules Fouchy <jules.fouchy@ntymail.com>
2022-06-07 00:13:28 +0200

    ✨ [Camera::Projection_Perspective::imgui()] Put all the sliders in a group
    Allwos us to call IsItemDeactivatedAfterEdit only once and get an answer for any of the sliders

--- src/Cool/Camera/Projection_Perspective.cpp
+++ src/Cool/Camera/Projection_Perspective.cpp
@@ -13,9 +13,11 @@ auto projection_matrix(const Projection_Perspective& proj, float aspect_ratio) -
 auto imgui(Projection_Perspective& proj) -> bool
 {
     bool b = false;
+    ImGui::BeginGroup();
     b |= ImGui::SliderAngle("Field of View", &proj.field_of_view_in_radians, 0.001f, 180.f);
     b |= ImGui::SliderFloat("Near Plane", &proj.near_plane, 0.001f, 1.f);
     b |= ImGui::SliderFloat("Far Plane", &proj.far_plane, proj.near_plane + 0.001f, 500.f);
+    ImGui::EndGroup();
     return b;
 }
 
