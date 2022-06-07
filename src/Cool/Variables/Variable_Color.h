8f698b4f1d34bfeb5e16740aeb2830dca017c2ba
Jules Fouchy <jules.fouchy@ntymail.com>
2022-06-08 01:01:26 +0200

    ✨ [Variable_Color] Added

---
+++ src/Cool/Variables/Variable_Color.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "Variable.h"
+
+namespace Cool {
+
+struct Color {
+    glm::vec3 rgb;
+};
+
+template<>
+struct VariableMetadata<Color> {
+    bool is_hdr = true;
+
+    friend auto operator<=>(const VariableMetadata<Color>&, const VariableMetadata<Color>&) = default;
+
+private:
+    //Serialization
+    friend class cereal::access;
+    template<class Archive>
+    void serialize(Archive& archive)
+    {
+        archive(cereal::make_nvp("Is HDR", is_hdr));
+    }
+};
+
+auto imgui_widget(Variable<Color>& var) -> bool
+{
+    return ImGui::ColorEdit3(var.name.c_str(),
+                             glm::value_ptr(var.value.rgb),
+                             ImGuiColorEditFlags_Float | (var.metadata.is_hdr
+                                                              ? ImGuiColorEditFlags_HDR
+                                                              : 0));
+}
+
+auto imgui_widget(VariableMetadata<Color>& meta) -> bool
+{
+    bool b = false;
+    b |= ImGui::Checkbox("HDR", &meta.is_hdr);
+    return b;
+}
+
+} // namespace Cool
