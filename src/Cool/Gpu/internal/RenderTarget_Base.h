967168dbf2706e04e98fca9c440f4a930b6a89e0
Jules Fouchy <jules.fouchy@ntymail.com>
2022-06-12 11:36:23 +0200

    ✨ [RenderTarget] Add a getter for the underlying OpenGL implementation

--- src/Cool/Gpu/internal/RenderTarget_Base.h
+++ src/Cool/Gpu/internal/RenderTarget_Base.h
@@ -8,14 +8,15 @@ namespace Cool {
 template<typename RenderTarget_Impl>
 class RenderTarget_Base {
 public:
-    void             render(typename RenderTarget_Impl::RenderFuncType render_fn);
-    img::Image       download_pixels() const { return _impl.download_pixels(); }
-    ImTextureID      imgui_texture_id() const { return _impl.imgui_texture_id(); }
-    RenderTargetInfo info() const { return _impl.info(); }
-    img::Size        current_size() const { return _impl.size(); }
-    img::Size        desired_size() const { return _desired_size; }
-    auto             needs_resizing() const -> bool { return _impl.size() != desired_size(); }
-    void             set_size(img::Size size) { _desired_size = size; }
+    void               render(typename RenderTarget_Impl::RenderFuncType render_fn);
+    img::Image         download_pixels() const { return _impl.download_pixels(); }
+    ImTextureID        imgui_texture_id() const { return _impl.imgui_texture_id(); }
+    RenderTargetInfo   info() const { return _impl.info(); }
+    img::Size          current_size() const { return _impl.size(); }
+    img::Size          desired_size() const { return _desired_size; }
+    void               set_size(img::Size size) { _desired_size = size; }
+    RenderTarget_Impl& get() { return _impl; } // TODO(JF) This is a temporary solution until the abstraction is fully done
+    auto               needs_resizing() const -> bool { return _impl.size() != desired_size(); }
 
 private:
     void resize_if_necessary();
