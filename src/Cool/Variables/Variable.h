6b2c24704d968ae29ed3962d4802a0690819c028
Jules Fouchy <jules.fouchy@ntymail.com>
2022-06-17 00:38:08 +0200

    ✨ [Variable] Add callbacks for the various events

--- src/Cool/Variables/Variable.h
+++ src/Cool/Variables/Variable.h
@@ -32,47 +32,77 @@ private:
     }
 };
 
+struct ImGuiVariableCallbacks {
+    std::function<void()> on_value_changed             = []() {};
+    std::function<void()> on_metadata_changed          = []() {};
+    std::function<void()> on_value_editing_finished    = []() {};
+    std::function<void()> on_metadata_editing_finished = []() {};
+};
+
 /// Returns true iff the `value` of the variable changed
 /// (Currently there is no way to know if the metadata changed)
 template<typename Value>
-auto imgui_variable_reset_buttons(Variable<Value>& var) -> bool
+auto imgui_variable_reset_buttons(Variable<Value>& var, ImGuiVariableCallbacks callbacks = {}) -> bool
 {
     bool b = false;
     ImGuiExtras::maybe_disabled(var.value == var.default_value, "Disabled because it is already equal to the default value", [&]() {
         if (ImGui::Button(("Reset to default value (" + stringify(var.default_value) + ")").c_str())) {
             var.value = var.default_value;
             b         = true;
+            callbacks.on_value_changed();
+            callbacks.on_value_editing_finished();
         }
     });
     if (var.metadata != var.default_metadata) {
         ImGui::SameLine();
         if (ImGui::Button("Reset Metadata")) {
             var.metadata = var.default_metadata;
+            callbacks.on_metadata_changed();
+            callbacks.on_metadata_editing_finished();
         }
     }
     return b;
 }
 
 /// Returns true iff the `value` of the variable changed
-/// (Currently there is no way to know if the metadata changed)
+/// Calls the corresponding callback if either `value` or `metadata` change
 template<typename Value>
-auto imgui(Variable<Value>& var) -> bool
+auto imgui(Variable<Value>&       var,
+           ImGuiVariableCallbacks callbacks = {}) -> bool
 {
     bool b = false;
+
     ImGui::PushID(&var);
+    ImGui::BeginGroup();
     {
-        b |= imgui_widget(var);
-    }
-    ImGui::PopID();
-    ImGui::PushID(&var + 17);
-    {
-        if (ImGui::BeginPopupContextItem(var.name.c_str())) {
-            imgui_widget(var.metadata);
-            b |= imgui_variable_reset_buttons(var);
-            ImGui::EndPopup();
+        if (imgui_widget(var)) {
+            b = true;
+            callbacks.on_value_changed();
         }
     }
+    ImGui::EndGroup();
     ImGui::PopID();
+    if (ImGui::IsItemDeactivatedAfterEdit()) {
+        callbacks.on_value_editing_finished();
+    }
+
+    if (ImGui::BeginPopupContextItem(var.name.c_str())) {
+        ImGui::PushID(&var + 1);
+        ImGui::BeginGroup();
+        {
+            if (imgui_widget(var.metadata)) {
+                callbacks.on_metadata_changed();
+            }
+        }
+        ImGui::EndGroup();
+        ImGui::PopID();
+        if (ImGui::IsItemDeactivatedAfterEdit()) {
+            callbacks.on_metadata_editing_finished();
+        }
+        b |= imgui_variable_reset_buttons(var, callbacks);
+        ImGui::EndPopup();
+    }
+
     return b;
 }
 
