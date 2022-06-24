7e2dc7183a919a239abbe36897224ff3fde892f7
Jules Fouchy <jules.fouchy@ntymail.com>
2022-06-24 13:56:59 +0200

    🤏 [IApp] on_keyboard_event() is now optional to implement

--- src/Cool/AppManager/IApp.h
+++ src/Cool/AppManager/IApp.h
@@ -42,7 +42,7 @@ public:
      * 
      * @param event 
      */
-    virtual void on_keyboard_event(const KeyboardEvent& event) = 0;
+    virtual void on_keyboard_event(const KeyboardEvent& event){};
 
     /**
      * @brief This is where you receive and handle mouse click events.
