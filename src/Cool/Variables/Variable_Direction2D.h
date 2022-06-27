8aa29eed8966d05a4563a6a2994057364d371104
TaliaKah <anaissaratagougne@tutanota.com>
2022-06-27 15:31:48 +0200

    ✨ [Variable_Direction2D] Added

---
+++ src/Cool/Variables/Variable_Direction2D.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <Cool/ImGui/ImGuiExtras.h>
+#include <Cool/StrongTypes/Direction2D.h>
+#include "Variable.h"
+
+namespace Cool {
+
+template<>
+struct VariableMetadata<Direction2D> {
+    friend auto operator<=>(const VariableMetadata<Direction2D>&, const VariableMetadata<Direction2D>&) = default;
+
+private:
+    // Serialization
+    friend class cereal::access;
+    template<class Archive>
+    void serialize(Archive&)
+    {
+    }
+};
+
+inline auto imgui_widget(Variable<Direction2D>& var) -> bool
+{
+    return ImGuiExtras::angle_wheel(
+        var.name.c_str(),
+        &var.value.value.value.value
+    );
+}
+
+inline auto imgui_widget(VariableMetadata<Direction2D>&) -> bool
+{
+    return false;
+}
+
+} // namespace Cool
