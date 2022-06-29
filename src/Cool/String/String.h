96811b772087bd641997c4b5793ba0e09ef43ea6
Lucas DAVID <lucas29.david@gmail.com>
2022-06-29 16:50:08 +0200

    🤏 [String] Add default delimiter

--- src/Cool/String/String.h
+++ src/Cool/String/String.h
@@ -43,7 +43,7 @@ struct ReplacementInput {
     std::string_view                                        delimiter_end   = "}";
 };
 
-std::string replace(const ReplacementInput& in);
+std::string replace_between_delimiters(const ReplacementInput& in);
 
 std::pair<std::string, std::optional<size_t>> replace_next(const ReplacementInput& in, size_t start_pos);
 
@@ -64,13 +64,13 @@ std::optional<std::pair<size_t, size_t>> find_matching_pair(std::string_view tex
  * @brief Returns the indices of the beginning and end of the next word in "text" after position "offset".
  * Words are considered to be separated by one or more characters of "delimiters".
  */
-std::optional<std::pair<size_t, size_t>> find_next_word(std::string_view text, std::string_view delimiters, size_t offset);
+std::optional<std::pair<size_t, size_t>> find_next_word(std::string_view text, size_t offset, std::string_view delimiters = " \n\t\r");
 
 /**
  * @brief Splits the text and returns the list of words. A new word is created whenever one or more characters of "delimiters" are encountered in the text
  * The words appear in the same order in the list as they do in the text
  */
-std::vector<std::string> split_into_words(std::string_view text, std::string_view delimiters = " ");
+std::vector<std::string> split_into_words(std::string_view text, std::string_view delimiters = " \n\t\r");
 
 std::string remove_whitespaces(std::string_view text);
 
