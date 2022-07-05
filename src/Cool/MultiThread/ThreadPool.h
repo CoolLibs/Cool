8ff4f7affa8b0d084b22ce580077aad4a42f8b0d
Roberty Jordan <jordan.roberty@outlook.fr>
2022-07-05 12:46:01 +0200

    [Thread] back to commit 94f29be

--- src/Cool/MultiThread/ThreadPool.h
+++ src/Cool/MultiThread/ThreadPool.h
@@ -3,51 +3,67 @@
 namespace Cool {
 
 /**
- * @brief Manages a given number of threads and gives them jobs
- *
+ * @brief Manages a given number of threads and give them jobs
+ * 
  */
 template<typename Job>
 class ThreadPool {
 public:
     /**
-     * @brief Creates a thread pool using the maximum number of concurrent threads supported by the machine
-     *
-     */
+	 * @brief Creates a thread pool using the maximum number of concurrent threads supported by the machine
+	 * 
+	 */
     ThreadPool();
 
     /**
-     * @brief
-     *
-     * @param nb_threads Number of threads to create in the pool
-     */
+	 * @brief
+	 * 
+	 * @param nb_threads Number of threads to create in the pool
+	 */
     ThreadPool(size_t nb_threads);
 
     ~ThreadPool();
 
     /**
-     * @brief
-     *
-     * @return The number of threads in the pool
-     */
-    auto size() { return _threads.size(); }
+	 * @brief 
+	 * 
+	 * @return The number of threads in the pool
+	 */
+    inline size_t size() { return _nb_threads; }
 
     bool has_available_worker() { return _jobs_queue.size() < size(); }
 
     /**
-     * @brief Adds a job to the queue.
-     *
-     * @param job Any function pointer / lambda with signature void -> void
-     */
+	 * @brief Starts the pool : creates the threads
+	 * 
+	 */
+    void start();
+
+    /**
+	 * @brief Stops the pool : finishes all jobs and then destroys the threads
+	 * 
+	 */
+    void stop();
+
+    /**
+	 * @brief Adds a job to the queue.
+	 * 
+	 * @param job Any function pointer / lambda with signature void -> void
+	 */
     void push_job(Job&& job);
 
 private:
-    void worker_thread_loop(std::stop_token stop_token);
+    void check_for_jobs();
+    void wait_for_all_jobs_to_finish();
 
 private:
-    std::vector<std::jthread> _threads;
-    std::condition_variable   _wake_up_thread;
-    std::deque<Job>           _jobs_queue;
-    std::mutex                _jobs_queue_mutex;
+    size_t                   _nb_threads;
+    std::vector<std::thread> _threads;
+    std::condition_variable  _condition_to_pop_from_queue;
+    std::condition_variable  _condition_to_check_queue_size_is_small_enough;
+    std::deque<Job>          _jobs_queue;
+    std::mutex               _jobs_queue_mutex;
+    bool                     _running = false;
 };
 
 } // namespace Cool
