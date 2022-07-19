3e7e112ce435630df555944ed2751b1cf147f56b
Jules Fouchy <jules.fouchy@ntymail.com>
2022-07-19 09:50:42 +0200

    🚨 Fix compilation

--- src/Cool/String/String.h
+++ src/Cool/String/String.h
@@ -158,13 +158,12 @@ auto value_from_string(std::string_view) -> std::optional<T>
             std::is_same_v<T, glm::ivec3> == false ||
             std::is_same_v<T, glm::ivec4> == false ||
             std::is_same_v<T, Cool::RgbColor> == false ||
-            std::is_same_v<T, Cool::Camera> == false ||
             std::is_same_v<T, Cool::Angle> == false ||
             std::is_same_v<T, Cool::Direction2D> == false ||
             std::is_same_v<T, Cool::Hue> == false,
         "Type not supported yet!"
     );
-    /// NB: Use the folliwing code if you need to know the type which is failing:
+    /// NB: Use the following code if you need to know the type which is failing:
     // const auto debug_name = std::string{"Type not supported yet: "} + typeid(T).name();
     // std::ignore           = debug_name;
     // assert(false);
