44037e05404fc8b8e05498c10bb51c824da9155e
Jules Fouchy <jules.fouchy@ntymail.com>
2022-08-05 22:24:23 +0200

    🐛 [AutoSerializer] Don't throw by default, add a parameter to control that behaviour

--- src/Cool/Serialization/AutoSerializer.h
+++ src/Cool/Serialization/AutoSerializer.h
@@ -6,14 +6,14 @@ namespace Cool {
 template<typename T>
 class AutoSerializer {
 public:
-    AutoSerializer(std::string_view path, std::string_view key_name_in_json, T& object_to_serialize, bool load_from_file = true)
+    AutoSerializer(std::string_view path, std::string_view key_name_in_json, T& object_to_serialize, bool is_allowed_to_throw = false, bool load_from_file = true)
         : _path{path}
         , _key_name_in_json{key_name_in_json}
         , _object_to_serialize{object_to_serialize}
     {
         if (load_from_file)
         {
-            Serialization::from_json(_object_to_serialize, _path, true);
+            Serialization::from_json(_object_to_serialize, _path, is_allowed_to_throw);
         }
     }
 
