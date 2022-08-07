97a16e5cb4c61615f7e18ab0ce184d02ef053529
Lucas DAVID <lucas29.david@gmail.com>
2022-08-07 09:17:59 +0200

    ♻️ [Serialization] Convert std::string to filesystem::path

--- src/Cool/Serialization/AutoSerializer.h
+++ src/Cool/Serialization/AutoSerializer.h
@@ -6,7 +6,7 @@ namespace Cool {
 template<typename T>
 class AutoSerializer {
 public:
-    AutoSerializer(std::string_view path, std::string_view key_name_in_json, T& object_to_serialize, bool is_allowed_to_throw = false, bool load_from_file = true)
+    AutoSerializer(std::filesystem::path path, std::string_view key_name_in_json, T& object_to_serialize, bool is_allowed_to_throw = false, bool load_from_file = true)
         : _path{path}
         , _key_name_in_json{key_name_in_json}
         , _object_to_serialize{object_to_serialize}
@@ -23,9 +23,9 @@ public:
     }
 
 private:
-    std::string _path;
-    std::string _key_name_in_json;
-    T&          _object_to_serialize;
+    std::filesystem::path _path;
+    std::string           _key_name_in_json;
+    T&                    _object_to_serialize;
 };
 
 } // namespace Cool
