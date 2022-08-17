be097e8c63a01a9a40dfe91d6a4ea6933cab1591
TaliaKah <anaissaratagougne@tutanota.com>
2022-08-17 14:53:55 +0200

    ⚰️ [Gradient] Remove code commented

--- src/Cool/StrongTypes/Gradient.h
+++ src/Cool/StrongTypes/Gradient.h
@@ -18,20 +18,6 @@ struct Gradient
 
     friend auto operator==(const Gradient& a, const Gradient& b) -> bool { return a.value.gradient() == b.value.gradient(); }
 
-    // auto inigo_formula(const glm::vec3 a, const glm::vec3 b, const glm::vec3 c, const glm::vec3 d) -> Cool::RgbColor
-    // {
-    //     return Cool::RgbColor(a + b * glm::cos(tau * (c * value + d)));
-    // }
-    // auto get_color() -> Cool::RgbColor
-    // {
-    //     const glm::vec3 a = glm::vec3(0.5, 0.5, 0.5);
-    //     const glm::vec3 b = glm::vec3(0.5, 0.5, 0.5);
-    //     const glm::vec3 c = glm::vec3(1., 1., 1.);
-    //     const glm::vec3 d = glm::vec3(0., 0.33, 0.67);
-    //     return inigo_formula(a, b, c, d);
-    // }
-    // void set_color(const float position) { value = position; }
-
 private:
     // Serialization
     friend class cereal::access;
