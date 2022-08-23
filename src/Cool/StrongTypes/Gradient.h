1f9502147c548f680de529cb3ca2d27115f2a761
TaliaKah <anaissaratagougne@tutanota.com>
2022-08-23 11:26:50 +0200

    🤏 [Gradient::gradient_widget()] Use designated initializers

--- src/Cool/StrongTypes/Gradient.h
+++ src/Cool/StrongTypes/Gradient.h
@@ -47,10 +47,13 @@ inline auto to_string(Cool::Gradient) -> std::string
 
 inline auto gradient_widget(std::string_view name, Cool::Gradient& gradient, ImGuiColorEditFlags flags) -> bool
 {
-    ImGG::Settings settings{}; // TODO(ASG) Use designated initializers
-    settings.color_edit_flags = flags;
-    settings.flags            = ImGG::Flag::NoResetButton;
-    return gradient.value.widget(name.data(), settings);
+    return gradient.value.widget(
+        name.data(),
+        {
+            .flags            = ImGG::Flag::NoResetButton,
+            .color_edit_flags = flags,
+        }
+    );
 }
 
 } // namespace Cool
