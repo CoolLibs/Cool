583ef5f225f458f6a28f71db0e3111a801168669
TaliaKah <anaissaratagougne@tutanota.com>
2022-08-24 13:17:20 +0200

    🐛 [Gradient] Serialiaze interpolatio mode

--- src/Cool/StrongTypes/Gradient.h
+++ src/Cool/StrongTypes/Gradient.h
@@ -27,10 +27,12 @@ private:
     template<class Archive>
     void save(Archive& archive) const
     {
-        auto wrap_mode_size_t = static_cast<size_t>(wrap_mode);
+        auto wrap_mode_size_t          = static_cast<size_t>(wrap_mode);
+        auto interpolation_mode_size_t = static_cast<size_t>(value.gradient().interpolation_mode());
         archive(
             cereal::make_nvp("Gradient", value.gradient().get_marks()),
-            cereal::make_nvp("Wrap Mode", wrap_mode_size_t)
+            cereal::make_nvp("Wrap Mode", wrap_mode_size_t),
+            cereal::make_nvp("Interpolation Mode", interpolation_mode_size_t)
         );
     }
     friend class cereal::access;
@@ -39,9 +41,11 @@ private:
     {
         std::list<ImGG::Mark> gradient_loaded{};
         size_t                wrap_mode_size_t{};
-        archive(gradient_loaded, wrap_mode_size_t);
-        value.gradient() = ImGG::Gradient{gradient_loaded};
-        wrap_mode        = static_cast<ImGG::WrapMode>(wrap_mode_size_t);
+        size_t                interpolation_mode_size_t{};
+        archive(gradient_loaded, wrap_mode_size_t, interpolation_mode_size_t);
+        value.gradient()                      = ImGG::Gradient{gradient_loaded};
+        wrap_mode                             = static_cast<ImGG::WrapMode>(wrap_mode_size_t);
+        value.gradient().interpolation_mode() = static_cast<ImGG::Interpolation>(interpolation_mode_size_t);
     }
 };
 
