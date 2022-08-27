b7c6a488be0b45ffe179177faa47f5875fb3ae19
TaliaKah <anaissaratagougne@tutanota.com>
2022-08-27 11:55:17 +0200

    🤏 [rgb_color_utils] Add in Cool::internal namespace

--- src/Cool/Variables/Variable_Gradient.h
+++ src/Cool/Variables/Variable_Gradient.h
@@ -15,13 +15,13 @@ inline auto imgui_widget(Variable<Gradient>& var) -> bool
         var.name,
         var.value,
         var.metadata.should_use_a_random_color_for_the_new_marks,
-        rgb_color_imgui_flags(var.metadata.is_hdr)
+        internal::rgb_color_imgui_flags(var.metadata.is_hdr)
     );
 }
 
 inline auto imgui_widget(VariableMetadata<Gradient>& meta) -> bool
 {
-    return rgb_color_metadata_widget(meta.is_hdr) |
+    return internal::rgb_color_metadata_widget(meta.is_hdr) |
            ImGG::random_mode_widget("Use random colors when adding new marks", &meta.should_use_a_random_color_for_the_new_marks);
 }
 
