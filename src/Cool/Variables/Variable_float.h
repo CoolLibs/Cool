6c22f73a73512b7012041be2cd725ba52f18fe4a
TaliaKah <anaissaratagougne@tutanota.com>
2022-09-03 10:06:36 +0200

    🤏 [Variable_Float] Reduce metadata drag sensitivity

--- src/Cool/Variables/Variable_float.h
+++ src/Cool/Variables/Variable_float.h
@@ -18,12 +18,15 @@ inline auto imgui_widget(Variable<float>& var) -> bool
 
 inline auto imgui_widget(VariableMetadata<float>& meta) -> bool
 {
+    static constexpr auto drag_speed = 0.01f;
+
     bool b = false;
-    b |= ImGui::DragFloat("##1", &meta.min_value);
+    b |= ImGui::DragFloat("##1", &meta.min_value, drag_speed);
     ImGui::SameLine();
     ImGui::Text("to");
     ImGui::SameLine();
-    b |= ImGui::DragFloat("##2", &meta.max_value);
+    b |= ImGui::DragFloat("##2", &meta.max_value, drag_speed);
+
     return b;
 }
 
