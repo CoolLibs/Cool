345da52e9a45453ec5361cbf1f529a1bd69fb355
Jules Fouchy <jules.fouchy@ntymail.com>
2022-09-29 12:43:49 +0200

    🤏 [UserSettings] Make sure autosave delay is always > 1

--- src/Cool/UserSettings/UserSettings.cpp
+++ src/Cool/UserSettings/UserSettings.cpp
@@ -1,12 +1,16 @@
 #include "UserSettings.h"
 #include <Cool/ImGui/ImGuiExtras.h>
+#include <smart/smart.hpp>
 
 namespace Cool {
 
 auto UserSettings::imgui_autosave() -> bool
 {
     return ImGuiExtras::checkbox_with_submenu("Autosave", &autosave_enabled, [&]() {
-        return ImGui::InputFloat("Delay (in seconds)", &autosave_delay_in_seconds);
+        bool b                    = ImGui::InputFloat("Delay (in seconds)", &autosave_delay_in_seconds);
+        autosave_delay_in_seconds = smart::keep_above(1.f, autosave_delay_in_seconds); // Make sure the delay is at least 1 second
+
+        return b;
     });
 }
 
