18d96443bb834e977ceff18ecfd69307e859497c
Jules Fouchy <jules.fouchy@ntymail.com>
2022-10-13 13:22:41 +0200

    🤏 [ImageSizeU] Remove check that is now unnecessary before setting w or h

--- src/Cool/Image/ImageSizeU.cpp
+++ src/Cool/Image/ImageSizeU.cpp
@@ -16,20 +16,14 @@ bool imgui(img::Size& size)
         if (ImGuiExtras::input_uint("W", &w))
         {
             was_used = true;
-            if (w >= 1)
-            {
-                size.set_width(w);
-            }
+            size.set_width(w);
         }
         ImGui::SameLine();
         auto h = size.height();
         if (ImGuiExtras::input_uint("H", &h))
         {
             was_used = true;
-            if (h >= 1)
-            {
-                size.set_height(h);
-            }
+            size.set_height(h);
         }
     }
     return was_used;
