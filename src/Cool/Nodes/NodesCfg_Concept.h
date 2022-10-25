b4aacc74fa8904b9e93e92025d6858ffeefdb102
Jules Fouchy <jules.fouchy@ntymail.com>
2022-10-25 23:28:34 +0200

    🎨 Apply clang-format

--- src/Cool/Nodes/NodesCfg_Concept.h
+++ src/Cool/Nodes/NodesCfg_Concept.h
@@ -6,14 +6,13 @@ namespace Cool {
 
 template<typename Cfg>
 concept NodesCfg_Concept =
-    Node_Concept<typename Cfg::NodeT> &&
-    NodeDefinition_Concept<typename Cfg::NodeDefinitionT> &&
-    requires(typename Cfg::NodeDefinitionT def, typename Cfg::NodeT node)
-{
-    // clang-format off
-    { Cfg::make_node(def) } -> std::convertible_to<typename Cfg::NodeT>;
-    { Cfg::name(node) } -> std::convertible_to<std::string>;
-    // clang-format on
-};
+    Node_Concept<typename Cfg::NodeT>
+    && NodeDefinition_Concept<typename Cfg::NodeDefinitionT>
+    && requires(typename Cfg::NodeDefinitionT def, typename Cfg::NodeT node) {
+           // clang-format off
+            { Cfg::make_node(def) } -> std::convertible_to<typename Cfg::NodeT>;
+            { Cfg::name(node) } -> std::convertible_to<std::string>;
+           // clang-format on
+       };
 
 } // namespace Cool
