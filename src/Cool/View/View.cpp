f287dfbeeac4f053490067306536d370de3005c1
Jules Fouchy <jules.fouchy@ntymail.com>
2022-11-22 00:20:01 +0100

    🤏 Use a grey background around the views instead of pure black

--- src/Cool/View/View.cpp
+++ src/Cool/View/View.cpp
@@ -9,12 +9,14 @@ void View::imgui_window(ImTextureID image_texture_id, ImageSizeInsideView image_
     if (_is_open)
     {
         ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, {0.f, 0.f}); // TODO add a parameter in the UI to control the padding specifically for the views
+        ImGui::PushStyleColor(ImGuiCol_WindowBg, ImGui::GetStyleColorVec4(ImGuiCol_FrameBg));
         ImGui::Begin(_name.c_str(), &_is_open, ImGuiWindowFlags_NoScrollbar);
         store_window_size();
         store_window_position();
         _window_is_hovered = ImGui::IsWindowHovered();
         display_image(image_texture_id, image_size_inside_view);
         ImGui::End();
+        ImGui::PopStyleColor();
         ImGui::PopStyleVar();
     }
     else
