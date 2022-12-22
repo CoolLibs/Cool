695bd50d4646a860b96559700135dea6a7dd5245
Jules Fouchy <jules.fouchy@ntymail.com>
2022-12-22 12:49:41 +0100

    🤏 [Settings_Ref] Setup boilerplate to support Inputs

--- src/Cool/Variables/Settings_Ref.cpp
+++ src/Cool/Variables/Settings_Ref.cpp
@@ -3,6 +3,10 @@
 
 namespace Cool {
 
+/* ------------------*/
+/* Settings_Ref */
+/* ------------------*/
+
 Settings_Ref::Settings_Ref(Settings& settings)
     : _ref{settings}
 {
@@ -12,6 +16,10 @@ Settings_Ref::Settings_Ref(SettingsInInputs& settings)
     : _ref{settings}
 {
 }
+
+/* ------------*/
+/* assign_from */
+/* ------------*/
 
 static void assign_from_IMPL(std::reference_wrapper<Settings> ref, Settings const& settings)
 {
@@ -27,6 +35,10 @@ void Settings_Ref::assign_from(Settings const& settings)
 {
     std::visit([&](auto&& ref) { assign_from_IMPL(ref, settings); }, _ref);
 }
+
+/* ------------------------------*/
+/* display_all_variables_widgets */
+/* ------------------------------*/
 
 static auto display_all_variables_widgets_IMPL(std::reference_wrapper<Settings> ref) -> bool
 {
@@ -48,6 +60,10 @@ auto Settings_Ref::display_all_variables_widgets() -> bool
     return std::visit([](auto&& ref) { return display_all_variables_widgets_IMPL(ref); }, _ref);
 }
 
+/* ------------------*/
+/* Settings_ConstRef */
+/* ------------------*/
+
 Settings_ConstRef::Settings_ConstRef(Settings const& settings)
     : _ref{settings}
 {
@@ -63,16 +79,44 @@ Settings_ConstRef::Settings_ConstRef(Settings_Ref ref)
 {
 }
 
+/* -------------*/
+/* are_equal_to */
+/* -------------*/
+
+static auto are_equal_to_IMPL(std::reference_wrapper<Settings const> ref, Settings const& settings) -> bool
+{
+    return ref.get() == settings;
+}
+
+static auto are_equal_to_IMPL(std::reference_wrapper<SettingsInInputs const> ref, Settings const& settings) -> bool
+{
+    return false;
+    // return ref.get() == settings;
+}
+
 auto Settings_ConstRef::are_equal_to(Settings const& settings) const -> bool
 {
+    return std::visit([&](auto&& ref) { return are_equal_to_IMPL(ref, settings); }, _ref);
+}
+
+/* ------------*/
+/* as_settings */
+/* ------------*/
+
+static auto as_settings_IMPL(std::reference_wrapper<Settings const> ref) -> Settings
+{
+    return ref.get();
+}
+
+static auto as_settings_IMPL(std::reference_wrapper<SettingsInInputs const> ref) -> Settings
+{
     return {};
-    // return _ref.get() == settings;
+    // return ref.get() == settings;
 }
 
 auto Settings_ConstRef::as_settings() const -> Settings
 {
-    return {};
-    // return _ref.get();
+    return std::visit([](auto&& ref) { return as_settings_IMPL(ref); }, _ref);
 }
 
 } // namespace Cool
