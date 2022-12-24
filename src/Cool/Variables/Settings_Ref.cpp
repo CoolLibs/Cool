d197f71975ff8f26a530502597b8573e76131ad2
Jules Fouchy <jules.fouchy@ntymail.com>
2022-12-24 12:41:59 +0100

    ⏪️ Revert to "✨ [Settings_Ref] Support a simple reference to some Settings"
    This reverts commits:
    671c72d869f2bd405a98e54ba28b0e7b259f91a6,
    695bd50d4646a860b96559700135dea6a7dd5245.
    266d9133c010d9207d9940bbb4d371646abf7d82.

--- src/Cool/Variables/Settings_Ref.cpp
+++ src/Cool/Variables/Settings_Ref.cpp
@@ -1,129 +1,45 @@
 #include "Settings_Ref.h"
-#include "Cool/Variables/Settings_Ref.h"
 
 namespace Cool {
-
-/* ------------------*/
-/* Settings_Ref */
-/* ------------------*/
 
 Settings_Ref::Settings_Ref(Settings& settings)
     : _ref{settings}
 {
 }
 
-Settings_Ref::Settings_Ref(SettingsInInputs settings)
-    : _ref{settings}
-{
-}
-
-/* ------------*/
-/* assign_from */
-/* ------------*/
-
-static void assign_from_IMPL(std::reference_wrapper<Settings> ref, Settings const& settings)
-{
-    ref.get() = settings;
-}
-
-static void assign_from_IMPL(SettingsInInputs ref, Settings const& settings)
-{
-    // TODO(JF) Use a CommandGroup
-    for (auto& input : ref.inputs.get())
-    {
-        std::visit([&](auto&& input) {
-            input // TODO(JF) Set the variable through a command (ideally, but might just be set with the registry for now)
-        },
-                   input);
-    }
-}
-
 void Settings_Ref::assign_from(Settings const& settings)
 {
-    std::visit([&](auto&& ref) { assign_from_IMPL(ref, settings); }, _ref);
+    _ref.get() = settings;
 }
 
-/* ------------------------------*/
-/* display_all_variables_widgets */
-/* ------------------------------*/
-
-static auto display_all_variables_widgets_IMPL(std::reference_wrapper<Settings> ref) -> bool
+auto Settings_Ref::display_all_variables_widgets() -> bool
 {
     bool was_used{false};
-    for (auto& variable : ref.get())
+    for (auto& variable : _ref.get())
     {
         was_used |= std::visit([](auto&& real_variable) { return imgui_widget(real_variable); }, variable);
     }
     return was_used;
 }
 
-static auto display_all_variables_widgets_IMPL(SettingsInInputs ref) -> bool
-{
-    return false;
-}
-
-auto Settings_Ref::display_all_variables_widgets() -> bool
-{
-    return std::visit([](auto&& ref) { return display_all_variables_widgets_IMPL(ref); }, _ref);
-}
-
-/* ------------------*/
-/* Settings_ConstRef */
-/* ------------------*/
-
 Settings_ConstRef::Settings_ConstRef(Settings const& settings)
     : _ref{settings}
 {
 }
 
-Settings_ConstRef::Settings_ConstRef(SettingsInInputs settings)
-    : _ref{settings}
-{
-}
-
 Settings_ConstRef::Settings_ConstRef(Settings_Ref ref)
-    : _ref{std::visit([](auto&& ref) { return Ref{ref}; }, ref._ref)}
+    : _ref{ref._ref}
 {
-}
-
-/* -------------*/
-/* are_equal_to */
-/* -------------*/
-
-static auto are_equal_to_IMPL(std::reference_wrapper<Settings const> ref, Settings const& settings) -> bool
-{
-    return ref.get() == settings;
-}
-
-static auto are_equal_to_IMPL(SettingsInInputsConst ref, Settings const& settings) -> bool
-{
-    return false;
-    // return ref.get() == settings;
 }
 
 auto Settings_ConstRef::are_equal_to(Settings const& settings) const -> bool
 {
-    return std::visit([&](auto&& ref) { return are_equal_to_IMPL(ref, settings); }, _ref);
-}
-
-/* ------------*/
-/* as_settings */
-/* ------------*/
-
-static auto as_settings_IMPL(std::reference_wrapper<Settings const> ref) -> Settings
-{
-    return ref.get();
-}
-
-static auto as_settings_IMPL(SettingsInInputsConst ref) -> Settings
-{
-    return {};
-    // return ref.get() == settings;
+    return _ref.get() == settings;
 }
 
 auto Settings_ConstRef::as_settings() const -> Settings
 {
-    return std::visit([](auto&& ref) { return as_settings_IMPL(ref); }, _ref);
+    return _ref.get();
 }
 
 } // namespace Cool
--- src/Cool/Variables/Settings_Ref.h
+++ src/Cool/Variables/Settings_Ref.h
@@ -1,37 +1,20 @@
 #pragma once
-#include <Cool/Dependencies/Input.h>
 #include <functional>
-#include ""
 #include "Settings.h"
 
-
 namespace Cool {
-
-struct SettingsInInputs {
-    std::reference_wrapper<std::vector<Cool::AnyInput>> inputs;
-    CommandExecutor_TopLevel_Ref                        command_executor;
-};
-
-struct SettingsInInputsConst {
-    std::reference_wrapper<std::vector<Cool::AnyInput> const> inputs;
-    std::reference_wrapper<Cool::VariableRegistries const>    registry;
-};
 
 /// Interface that can wrap several "container of variables"-like objects (aka "settings"-like)
 /// and expose what is needed by the presets manager.
 class Settings_Ref {
 public:
-    Settings_Ref(Settings&);         // NOLINT(google-explicit-constructor, hicpp-explicit-conversions)
-    Settings_Ref(SettingsInInputs);  // NOLINT(google-explicit-constructor, hicpp-explicit-conversions)
+    Settings_Ref(Settings&); // NOLINT(google-explicit-constructor, hicpp-explicit-conversions)
 
     void assign_from(Settings const&);
     auto display_all_variables_widgets() -> bool;
 
 private:
-    using Ref = std::variant<
-        std::reference_wrapper<Settings>,
-        SettingsInInputs>;
-    Ref _ref;
+    std::reference_wrapper<Settings> _ref;
 
     friend class Settings_ConstRef;
 };
@@ -40,18 +23,14 @@ private:
 /// and expose what is needed by the presets manager.
 class Settings_ConstRef {
 public:
-    Settings_ConstRef(Settings const&);         // NOLINT(google-explicit-constructor, hicpp-explicit-conversions)
-    Settings_ConstRef(SettingsInInputsConst);   // NOLINT(google-explicit-constructor, hicpp-explicit-conversions)
-    Settings_ConstRef(Settings_Ref);            // NOLINT(google-explicit-constructor, hicpp-explicit-conversions)
+    Settings_ConstRef(Settings const&); // NOLINT(google-explicit-constructor, hicpp-explicit-conversions)
+    Settings_ConstRef(Settings_Ref);    // NOLINT(google-explicit-constructor, hicpp-explicit-conversions)
 
     [[nodiscard]] auto are_equal_to(Settings const&) const -> bool;
     [[nodiscard]] auto as_settings() const -> Settings;
 
 private:
-    using Ref = std::variant<
-        std::reference_wrapper<Settings const>,
-        SettingsInInputsConst>;
-    Ref _ref;
+    std::reference_wrapper<Settings const> _ref;
 };
 
 } // namespace Cool
