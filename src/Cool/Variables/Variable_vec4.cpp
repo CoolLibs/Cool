be02e66e0322d3b89216dc798bf23464e2be3a3b
Jules Fouchy <jules.fouchy@ntymail.com>
2023-01-06 12:51:10 +0100

    ✨ [Variables] Finish giving the extended float metadata to vec2/3/4

--- src/Cool/Variables/Variable_vec4.cpp
+++ src/Cool/Variables/Variable_vec4.cpp
@@ -1,19 +1,32 @@
 #include "Variable_vec4.h"
+#include "Cool/Variables/internal/float_utils.h"
 
 namespace Cool {
 
 auto imgui_widget(Variable<glm::vec4>& var) -> bool
 {
-    return ImGui::DragFloat4(
-        var.name.c_str(),
-        glm::value_ptr(var.value),
-        var.metadata.drag_speed
-    );
+    if (var.metadata.bounded)
+    {
+        return ImGui::SliderFloat4(
+            var.name.c_str(),
+            glm::value_ptr(var.value),
+            var.metadata.min_value,
+            var.metadata.max_value
+        );
+    }
+    else // NOLINT(readability-else-after-return)
+    {
+        return ImGui::DragFloat4(
+            var.name.c_str(),
+            glm::value_ptr(var.value),
+            var.metadata.drag_speed
+        );
+    }
 }
 
 auto imgui_widget(VariableMetadata<glm::vec4>& meta) -> bool
 {
-    return ImGui::DragFloat("Drag speed", &meta.drag_speed, 0.01f, 0.001f, FLT_MAX / static_cast<float>(INT_MAX));
+    return internal::float_metadata_widget(meta.min_value, meta.max_value, meta.bounded, meta.drag_speed);
 }
 
 } // namespace Cool
