a8d003af9631e8df2f9cd00d940c25efe95f2d3d
Jules Fouchy <jules.fouchy@ntymail.com>
2023-01-13 16:40:19 +0100

    ✨ [Icon] Icon documented and set_icon function

--- src/Cool/Window/internal/WindowFactoryU.cpp
+++ src/Cool/Window/internal/WindowFactoryU.cpp
@@ -2,6 +2,18 @@
 
 namespace Cool::WindowFactoryU {
 
+void set_window_icon(GLFWwindow* window)
+{
+#ifdef COOL_APP_ICON_FILE
+    auto      icon = img::load(Cool::Path::root() / COOL_APP_ICON_FILE, 4, false);
+    GLFWimage image;
+    image.width  = icon.width();
+    image.height = icon.height();
+    image.pixels = icon.data();
+    glfwSetWindowIcon(window, 1, &image);
+#endif
+}
+
 void apply_config(const WindowConfig& config, Window& window)
 {
     window.cap_framerate_if(config.cap_framerate_on_startup_if);
--- src/Cool/Window/internal/WindowFactoryU.h
+++ src/Cool/Window/internal/WindowFactoryU.h
@@ -7,6 +7,8 @@
 
 namespace Cool::WindowFactoryU {
 
+void set_window_icon(GLFWwindow* window);
+
 template<typename Window_Impl>
 Window& make_window_with_glfw(const WindowConfig& config, WindowManager& window_manager, GLFWwindow* window_to_share_opengl_context_with = nullptr)
 {
@@ -19,13 +21,8 @@ Window& make_window_with_glfw(const WindowConfig& config, WindowManager& window_
         glfwGetError(&error_description);
         Log::Debug::error("glfw", fmt::format("Window creation failed:\n{}", error_description));
     }
-    // TODO(WG) First check that COOL_APP_ICON_FILE has been set
-    auto      icon = img::load(Cool::Path::root() / COOL_APP_ICON_FILE, 4, false);
-    GLFWimage image;
-    image.width  = icon.width();
-    image.height = icon.height();
-    image.pixels = icon.data();
-    glfwSetWindowIcon(window.glfw(), 1, &image);
+
+    set_window_icon(window.glfw());
     return window;
 }
 
