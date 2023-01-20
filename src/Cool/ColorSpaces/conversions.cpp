154234cb5c61da8cea2acec13b8ed75ab88d2eb3
Jules Fouchy <jules.fouchy@ntymail.com>
2023-01-20 21:02:11 +0100

    🐛 [ColorConversions] Fix HSLuv formulas

--- src/Cool/ColorSpaces/conversions.cpp
+++ src/Cool/ColorSpaces/conversions.cpp
@@ -129,46 +129,76 @@ auto LinearRGB_from_CIELAB(glm::vec3 const& c) -> glm::vec3
 // End of [Block2]
 
 // Start of [Block3]
-// From https://www.hsluv.org/
+/*
+ * HSLuv-C: Human-friendly HSL
+ * <https://github.com/hsluv/hsluv-c>
+ * <https://www.hsluv.org/>
+ *
+ * Copyright (c) 2015 Alexei Boronine (original idea, JavaScript implementation)
+ * Copyright (c) 2015 Roger Tallada (Obj-C implementation)
+ * Copyright (c) 2017 Martin Mitas (C implementation, based on Obj-C implementation)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+/* for RGB */
+static const glm::vec3 m[3] = {
+    {3.24096994190452134377, -1.53738317757009345794, -0.49861076029300328366},
+    {-0.96924363628087982613, 1.87596750150772066772, 0.04155505740717561247},
+    {0.05563007969699360846, -0.20397695888897656435, 1.05697151424287856072}};
+
+/* for XYZ */
+static const glm::vec3 m_inv[3] = {
+    {0.41239079926595948129, 0.35758433938387796373, 0.18048078840183428751},
+    {0.21263900587151035754, 0.71516867876775592746, 0.07219231536073371500},
+    {0.01933081871559185069, 0.11919477979462598791, 0.95053215224966058086}};
+
+static const float ref_u = 0.19783000664283680764f;
+static const float ref_v = 0.46831999493879100370f;
+
+static const float kappa   = 903.29629629629629629630f;
+static const float epsilon = 0.00885645167903563082f;
+
 struct Bounds {
     float a;
     float b;
 };
 
-static const float kappa   = 903.29629629629629629630f;
-static const float epsilon = 0.00885645167903563082f;
-
-static const float ref_u = 0.19783000664283680764f;
-static const float ref_v = 0.46831999493879100370f;
-
-static const std::array<glm::vec3, 3> m{glm::vec3(3.24096994190452134377f, -1.53738317757009345794f, -0.49861076029300328366f), glm::vec3(-0.96924363628087982613f, 1.87596750150772066772f, 0.04155505740717561247f), glm::vec3(0.05563007969699360846f, -0.20397695888897656435f, 1.05697151424287856072f)};
-
-static auto y2l(float y) -> float
+static void get_bounds(float l, Bounds bounds[6])
 {
-    if(y <= epsilon)
-        return y * kappa;
-    else
-        return 116. * cbrt(y) - 16.;
-}
-
-static auto get_bounds(float l, Cool::Bounds bounds[6]) -> void
-{
-    float tl   = l + 16.f;
+    float  tl   = l + 16.0f;
     float sub1 = (tl * tl * tl) / 1560896.0f;
-    float sub2 = (sub1 > Cool::epsilon ? sub1 : (l / Cool::kappa));
+    float sub2 = (sub1 > epsilon ? sub1 : (l / kappa));
+    int   channel;
     int   t;
 
-    for (int channel = 0; channel < 3; channel++)
+    for (channel = 0; channel < 3; channel++)
     {
-        float m1 = Cool::m[channel].x;
-        float m2 = Cool::m[channel].y;
-        float m3 = Cool::m[channel].z;
+        float m1 = m[channel].x;
+        float m2 = m[channel].y;
+        float m3 = m[channel].z;
 
         for (t = 0; t < 2; t++)
         {
-            float top1   = (284517.0f * m1 - 94839.0f * m3) * sub2;
-            float top2   = (838422.0f * m3 + 769860.0f * m2 + 731718.0f * m1) * l * sub2 - 769860.0f * t * l;
-            float bottom = (632260.0f * m3 - 126452.0f * m2) * sub2 + 126452.0f * t;
+            float top1   = (284517.f * m1 - 94839.f * m3) * sub2;
+            float top2   = (838422.f * m3 + 769860.f * m2 + 731718.f * m1) * l * sub2 - 769860.f * t * l;
+            float bottom = (632260.f * m3 - 126452.f * m2) * sub2 + 126452.f * t;
 
             bounds[channel * 2 + t].a = top1 / bottom;
             bounds[channel * 2 + t].b = top2 / bottom;
@@ -176,20 +206,54 @@ static auto get_bounds(float l, Cool::Bounds bounds[6]) -> void
     }
 }
 
-static auto ray_length_until_intersect(float theta, const Bounds* line) -> float
+static float intersect_line_line(Bounds const& line1, Bounds const& line2)
 {
-    return line->b / (sin(theta) - line->a * cos(theta));
+    return (line1.b - line2.b) / (line2.a - line1.a);
 }
 
-static auto max_chroma_for_lh(float l, float h) -> float
+static float dist_from_pole_squared(float x, float y)
 {
-    float        min_len = FLT_MAX;
-    float        hrad    = h * 0.01745329251994329577f; /* (2 * pi / 360) */
-    Cool::Bounds bounds[6];
+    return x * x + y * y;
+}
+
+static float ray_length_until_intersect(float theta, Bounds const& line)
+{
+    return line.b / (sin(theta) - line.a * cos(theta));
+}
+
+static float max_safe_chroma_for_l(float l)
+{
+    float  min_len_squared = FLT_MAX;
+    Bounds bounds[6];
+
     get_bounds(l, bounds);
     for (int i = 0; i < 6; i++)
     {
-        float len = Cool::ray_length_until_intersect(hrad, &bounds[i]);
+        float m1 = bounds[i].a;
+        float b1 = bounds[i].b;
+        /* x where line intersects with perpendicular running though (0, 0) */
+        Bounds line2    = {-1.f / m1, 0.f};
+        float  x        = intersect_line_line(bounds[i], line2);
+        float  distance = dist_from_pole_squared(x, b1 + x * m1);
+
+        if (distance < min_len_squared)
+            min_len_squared = distance;
+    }
+
+    return std::sqrt(min_len_squared);
+}
+
+static float max_chroma_for_lh(float l, float h)
+{
+    float  min_len = FLT_MAX;
+    float  hrad    = h * 0.01745329251994329577f; /* (2 * pi / 360) */
+    Bounds bounds[6];
+    int    i;
+
+    get_bounds(l, bounds);
+    for (i = 0; i < 6; i++)
+    {
+        float len = ray_length_until_intersect(hrad, bounds[i]);
 
         if (len >= 0 && len < min_len)
             min_len = len;
@@ -197,127 +261,204 @@ static auto max_chroma_for_lh(float l, float h) -> float
     return min_len;
 }
 
-auto HSLuv_from_XYZ(glm::vec3 c) -> glm::vec3
+/* https://en.wikipedia.org/wiki/CIELUV
+ * In these formulas, Yn refers to the reference white point. We are using
+ * illuminant D65, so Yn (see refY in Maxima file) equals 1. The formula is
+ * simplified accordingly.
+ */
+static float y2l(float y)
 {
-    c *= 100.;
-    // XYZ -> LUV
-    float var_u = (4.f * c.x) / (c.x + (15.f * c.y) + (3.f * c.z));
-    float var_v = (9.f * c.y) / (c.x + (15.f * c.y) + (3.f * c.z));
-    float l     = y2l(c.y);
-    float u     = 13.f * l * (var_u - Cool::ref_u);
-    float v     = 13.f * l * (var_v - Cool::ref_v);
+    if (y <= epsilon)
+        return y * kappa;
+    else
+        return 116.f * std::cbrt(y) - 16.f;
+}
 
-    c.x = l;
-    if (l < 0.00000001f)
+static float l2y(float l)
+{
+    if (l <= 8.f)
     {
-        c.y = 0.f;
-        c.z = 0.f;
+        return l / kappa;
     }
-
     else
     {
-        c.y = u;
-        c.z = v;
+        float x = (l + 16.f) / 116.f;
+        return x * x * x;
+    }
+}
+
+static void xyz2luv(glm::vec3& in_out)
+{
+    float var_u = (4.f * in_out.x) / (in_out.x + (15.f * in_out.y) + (3.f * in_out.z));
+    float var_v = (9.f * in_out.y) / (in_out.x + (15.f * in_out.y) + (3.f * in_out.z));
+    float l     = y2l(in_out.y);
+    float u     = 13.f * l * (var_u - ref_u);
+    float v     = 13.f * l * (var_v - ref_v);
+
+    in_out.x = l;
+    if (l < 0.00000001f)
+    {
+        in_out.y = 0.f;
+        in_out.z = 0.f;
+    }
+    else
+    {
+        in_out.y = u;
+        in_out.z = v;
+    }
+}
+
+static void luv2xyz(glm::vec3& in_out)
+{
+    if (in_out.x <= 0.00000001f)
+    {
+        /* Black will create a divide-by-zero error. */
+        in_out.x = 0.f;
+        in_out.y = 0.f;
+        in_out.z = 0.f;
+        return;
     }
 
-    // LUV -> lch
+    float var_u = in_out.y / (13.f * in_out.x) + ref_u;
+    float var_v = in_out.z / (13.f * in_out.x) + ref_v;
+    float y     = l2y(in_out.x);
+    float x     = -(9.f * y * var_u) / ((var_u - 4.f) * var_v - var_u * var_v);
+    float z     = (9.f * y - (15.f * var_v * y) - (var_v * x)) / (3.f * var_v);
+    in_out.x    = x;
+    in_out.y    = y;
+    in_out.z    = z;
+}
+
+static void luv2lch(glm::vec3& in_out)
+{
+    float l = in_out.x;
+    float u = in_out.y;
+    float v = in_out.z;
     float h;
-    float c2 = std::sqrt(u * u + v * v);
+    float c = std::sqrt(u * u + v * v);
 
     /* Grays: disambiguate hue */
-    if (c2 < 0.00000001f)
+    if (c < 0.00000001f)
     {
         h = 0.f;
     }
     else
     {
-        h = std::atan2(v, u) * 57.29577951308232087680f; /* (180 / pi) */
+        h = atan2(v, u) * 57.29577951308232087680f; /* (180 / pi) */
         if (h < 0.f)
             h += 360.f;
     }
 
-    // lch -> HSLuv
-    float s;
-
-    /* White and black: disambiguate saturation */
-    if (l > 0.9999999f || l < 0.00000001f)
-        s = 0.f;
-    else
-        s = c2 / Cool::max_chroma_for_lh(l, h) * 100.f;
-
-    /* Grays: disambiguate hue */
-    if (c2 < 0.00000001f)
-        h = 0.f;
-
-    return glm::vec3(h / 360., s / 100., l / 100.);
+    in_out.x = l;
+    in_out.y = c;
+    in_out.z = h;
 }
 
-auto XYZ_from_HSLuv(glm::vec3 c) -> glm::vec3
+static void lch2luv(glm::vec3& in_out)
 {
-    // HSLuv -> lch
-    float h = c.x;
-    float s = c.y;
-    float l = c.z;
-    float c2;
+    float hrad = in_out.z * 0.01745329251994329577f; /* (pi / 180.0) */
+    float u    = std::cos(hrad) * in_out.y;
+    float v    = std::sin(hrad) * in_out.y;
+
+    in_out.y = u;
+    in_out.z = v;
+}
+
+static void hsluv2lch(glm::vec3& in_out)
+{
+    float h = in_out.x;
+    float s = in_out.y;
+    float l = in_out.z;
+    float c;
 
     /* White and black: disambiguate chroma */
     if (l > 99.9999999f || l < 0.00000001f)
-        c2 = 0.f;
+        c = 0.f;
     else
-        c2 = Cool::max_chroma_for_lh(l, h);
+        c = max_chroma_for_lh(l, h) / 100.f * s;
 
     /* Grays: disambiguate hue */
     if (s < 0.00000001f)
-    {
         h = 0.f;
-    }
 
-    // lch -> LUV
-    float hrad = h * tau;
-    float u    = cos(hrad) * c2;
-    float v    = sin(hrad) * c2;
+    in_out.x = l;
+    in_out.y = c;
+    in_out.z = h;
+}
 
-    // LUV -> XYZ
-    if (l <= 0.00000001f) return glm::vec3(0.f);
+static void lch2hsluv(glm::vec3& in_out)
+{
+    float l = in_out.x;
+    float c = in_out.y;
+    float h = in_out.z;
+    float s;
 
-    float var_u = u / (13.f * l) + Cool::ref_u;
-    float var_v = v / (13.f * l) + Cool::ref_v;
-    float y     = (l <= 8.f) ? l / Cool::kappa : ((l + 16.f) / 116.f) * ((l + 16.f) / 116.f) * ((l + 16.f) / 116.f);
-    float x     = -(9.f * y * var_u) / ((var_u - 4.f) * var_v - var_u * var_v);
-    float z     = (9.f * y - (15.f * var_v * y) - (var_v * x)) / (3.f * var_v);
+    /* White and black: disambiguate saturation */
+    if (l > 99.9999999f || l < 0.00000001f)
+        s = 0.f;
+    else
+        s = c / max_chroma_for_lh(l, h) * 100.f;
 
-    return glm::vec3(x, y, z) / 100.f;
+    /* Grays: disambiguate hue */
+    if (c < 0.00000001f)
+        h = 0.f;
+
+    in_out.x = h;
+    in_out.y = s;
+    in_out.z = l;
+}
+
+auto XYZ_from_HSLuv(glm::vec3 const& col) -> glm::vec3
+{
+    glm::vec3 tmp = col;
+
+    hsluv2lch(tmp);
+    lch2luv(tmp);
+    luv2xyz(tmp);
+
+    return tmp;
+}
+
+auto HSLuv_from_XYZ(glm::vec3 const& col) -> glm::vec3
+{
+    glm::vec3 tmp = col;
+
+    xyz2luv(tmp);
+    luv2lch(tmp);
+    lch2hsluv(tmp);
+
+    return tmp;
 }
 
 // SRGB
-auto HSLuv_from_sRGB(glm::vec3 c) -> glm::vec3
+auto HSLuv_from_sRGB(glm::vec3 const& c) -> glm::vec3
 {
     return HSLuv_from_XYZ(XYZ_from_sRGB(c));
 }
 
-auto sRGB_from_HSLuv(glm::vec3 c) -> glm::vec3
+auto sRGB_from_HSLuv(glm::vec3 const& c) -> glm::vec3
 {
     return sRGB_from_XYZ(XYZ_from_HSLuv(c));
 }
 
 // LinearRGB
-auto HSLuv_from_LinearRGB(glm::vec3 c) -> glm::vec3
+auto HSLuv_from_LinearRGB(glm::vec3 const& c) -> glm::vec3
 {
     return HSLuv_from_XYZ(XYZ_from_LinearRGB(c));
 }
 
-auto LinearRGB_from_HSLuv(glm::vec3 c) -> glm::vec3
+auto LinearRGB_from_HSLuv(glm::vec3 const& c) -> glm::vec3
 {
     return LinearRGB_from_XYZ(XYZ_from_HSLuv(c));
 }
 
 // CIELAB
-auto HSLuv_from_CIELAB(glm::vec3 c) -> glm::vec3
+auto HSLuv_from_CIELAB(glm::vec3 const& c) -> glm::vec3
 {
     return HSLuv_from_XYZ(XYZ_from_CIELAB(c));
 }
 
-auto CIELAB_from_HSLuv(glm::vec3 c) -> glm::vec3
+auto CIELAB_from_HSLuv(glm::vec3 const& c) -> glm::vec3
 {
     return CIELAB_from_XYZ(XYZ_from_HSLuv(c));
 }
--- src/Cool/ColorSpaces/conversions.h
+++ src/Cool/ColorSpaces/conversions.h
@@ -14,13 +14,13 @@ namespace Cool {
 [[nodiscard]] auto sRGB_from_XYZ(glm::vec3 const&) -> glm::vec3;
 [[nodiscard]] auto sRGB_from_CIELAB(glm::vec3 const&) -> glm::vec3;
 [[nodiscard]] auto LinearRGB_from_CIELAB(glm::vec3 const&) -> glm::vec3;
-[[nodiscard]] auto HSLuv_from_XYZ(glm::vec3 c) -> glm::vec3;
-[[nodiscard]] auto XYZ_from_HSLuv(glm::vec3 c) -> glm::vec3;
-[[nodiscard]] auto HSLuv_from_sRGB(glm::vec3 c) -> glm::vec3;
-[[nodiscard]] auto sRGB_from_HSLuv(glm::vec3 c) -> glm::vec3;
-[[nodiscard]] auto HSLuv_from_LinearRGB(glm::vec3 c) -> glm::vec3;
-[[nodiscard]] auto LinearRGB_from_HSLuv(glm::vec3 c) -> glm::vec3;
-[[nodiscard]] auto HSLuv_from_CIELAB(glm::vec3 c) -> glm::vec3;
-[[nodiscard]] auto CIELAB_from_HSLuv(glm::vec3 c) -> glm::vec3;
+[[nodiscard]] auto HSLuv_from_XYZ(glm::vec3 const& c) -> glm::vec3;
+[[nodiscard]] auto XYZ_from_HSLuv(glm::vec3 const& c) -> glm::vec3;
+[[nodiscard]] auto HSLuv_from_sRGB(glm::vec3 const& c) -> glm::vec3;
+[[nodiscard]] auto sRGB_from_HSLuv(glm::vec3 const& c) -> glm::vec3;
+[[nodiscard]] auto HSLuv_from_LinearRGB(glm::vec3 const& c) -> glm::vec3;
+[[nodiscard]] auto LinearRGB_from_HSLuv(glm::vec3 const& c) -> glm::vec3;
+[[nodiscard]] auto HSLuv_from_CIELAB(glm::vec3 const& c) -> glm::vec3;
+[[nodiscard]] auto CIELAB_from_HSLuv(glm::vec3 const& c) -> glm::vec3;
 
 } // namespace Cool
