3b61b9f55ab30540c032ee9bcb2f9f1ec06a08db
Jules Fouchy <jules.fouchy@ntymail.com>
2023-01-23 00:50:24 +0100

    🚧 [Variable_TextureWrapper] Load texture from path

--- src/Cool/StrongTypes/TextureWrapper.cpp
+++ src/Cool/StrongTypes/TextureWrapper.cpp
@@ -1,7 +1,12 @@
 #include "TextureWrapper.h"
+#include <exception>
+#include <memory>
 #include "Cool/Gpu/Texture.h"
 #include "Cool/ImGui/ImGuiExtras.h"
+#include "Cool/Log/Message.h"
+#include "Cool/Log/ToUser.h"
 #include "Cool/NfdFileFilter/NfdFileFilter.h"
+#include "Cool/StrongTypes/TextureWrapper.h"
 
 namespace Cool {
 
@@ -23,7 +28,31 @@ static auto gen_dummy_texture() -> Texture
 auto TextureWrapper::imgui_widget() -> bool
 {
     bool const b = ImGuiExtras::file_and_folder("Image Path", &_absolute_path, NfdFileFilter::Image);
+
+    if (b)
+        try_load_texture_from_path();
+
     return b;
+}
+
+void TextureWrapper::try_load_texture_from_path()
+{
+    try
+    {
+        _texture = std::make_shared<Texture>(_absolute_path);
+        Cool::Log::ToUser::console().remove(_error_id);
+    }
+    catch (std::exception const& e)
+    {
+        Cool::Log::ToUser::console().send(
+            _error_id,
+            Message{
+                .category = "Load Image",
+                .message  = fmt::format("Failed to load '{}':\n{}", _absolute_path, e.what()),
+                .severity = MessageSeverity::Error,
+            }
+        );
+    }
 }
 
 } // namespace Cool
--- src/Cool/StrongTypes/TextureWrapper.h
+++ src/Cool/StrongTypes/TextureWrapper.h
@@ -1,11 +1,29 @@
 #pragma once
 #include <Cool/Gpu/Texture.h>
 #include <memory>
+#include "Cool/Log/MessageId.h"
 
 namespace Cool {
 
 class TextureWrapper {
 public:
+    TextureWrapper()                                             = default;
+    TextureWrapper(TextureWrapper&&) noexcept                    = default;
+    auto operator=(TextureWrapper&&) noexcept -> TextureWrapper& = default;
+    TextureWrapper(TextureWrapper const& o)
+        : _absolute_path{o._absolute_path}
+        , _texture{o._texture}
+    {}
+    auto operator=(TextureWrapper const& o) -> TextureWrapper&
+    {
+        if (&o != this)
+        {
+            _absolute_path = o._absolute_path;
+            _texture       = o._texture;
+        }
+        return *this;
+    }
+
     auto imgui_widget() -> bool;
 
     [[nodiscard]] auto texture() const -> Texture const&;
@@ -13,8 +31,12 @@ public:
     friend auto operator==(TextureWrapper const& a, TextureWrapper const& b) -> bool { return a._absolute_path == b._absolute_path; }
 
 private:
+    void try_load_texture_from_path();
+
+private:
     std::filesystem::path    _absolute_path{};
     std::shared_ptr<Texture> _texture{}; // We need a shared_ptr to make the class copyable
+    MessageId                _error_id{};
 
 private:
     // Serialization
@@ -23,6 +45,7 @@ private:
     void serialize(Archive& archive)
     {
         archive(cereal::make_nvp("Path", _absolute_path));
+        try_load_texture_from_path();
     }
 };
 
