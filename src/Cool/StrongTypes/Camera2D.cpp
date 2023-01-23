ce79447903b2fe694ef50b5fc1c1fb11dadde2fa
loucoir00 <lou.couard@gmail.com>
2023-01-23 09:21:41 +0100

    ✨ [Camera2D] Camera 2D reset button

--- src/Cool/StrongTypes/Camera2D.cpp
+++ src/Cool/StrongTypes/Camera2D.cpp
@@ -1,4 +1,5 @@
 #include "Camera2D.h"
+#include <Cool/Icons/Icons.h>
 #include <glm/gtx/matrix_transform_2d.hpp>
 #include <stringify/stringify.hpp>
 #include "Cool/ImGui/ImGuiExtras.h"
@@ -40,6 +41,14 @@ auto imgui_widget(std::string_view name, Camera2D& cam, int number_of_snaps, flo
     b |= ImGui::DragFloat2("Translation", glm::value_ptr(cam.translation), 0.01f);
     b |= imgui_widget("Rotation", cam.rotation, number_of_snaps, snaps_offset, always_snap);
     b |= ImGui::DragFloat("Zoom", &cam.zoom, 0.01f, 0.001f, FLT_MAX / static_cast<float>(INT_MAX));
+    if (ImGuiExtras::button_with_icon(Icons::reset().imgui_texture_id()))
+    {
+        cam.rotation    = Cool::Angle{Cool::Radians{0.}};
+        cam.zoom        = 1.f;
+        cam.translation = glm::vec2(0.);
+        b               = true;
+    }
+    Cool::ImGuiExtras::tooltip("Reset Camera");
 
     ImGui::PopID();
     ImGui::EndGroup();
