a4b07181e43060eed5ca877ad3e2d1cbbccc6adc
Jules Fouchy <jules.fouchy@ntymail.com>
2023-01-24 13:48:24 +0100

    🚧 [Texture] Wrap

--- src/Cool/Gpu/OpenGL/Texture.cpp
+++ src/Cool/Gpu/OpenGL/Texture.cpp
@@ -1,121 +1,56 @@
-#include "img/src/SizeU.h"
+#include "glpp/Functions/Texture.h"
+#include "glpp/Enums/TextureKind.h"
 #if defined(COOL_OPENGL)
 
-#include "../Texture.h"
 #include <img/img.hpp>
+#include "../Texture.h"
+#include "img/src/SizeU.h"
 
 namespace Cool::OpenGL {
 
-Texture::~Texture()
+Texture::Texture(Config config)
 {
-    DestroyTexture(m_textureID);
+    set_wrap_mode(config.wrap_mode);
+    set_interpolation_mode(config.interpolation_mode);
 }
 
-Texture::Texture(Texture&& rhs) noexcept
-    : m_textureID{rhs.m_textureID}
-    , _aspect_ratio{rhs._aspect_ratio}
-#if DEBUG
-    , m_bDataUploaded{rhs.m_bDataUploaded}
-#endif
+Texture::Texture(img::Size const& size, Config config)
+    : Texture{config}
 {
-    rhs.m_textureID = static_cast<GLuint>(-1);
+    set_size(size);
 }
 
-Texture& Texture::operator=(Texture&& rhs) noexcept
+Texture::Texture(img::Image const& image, Config config)
+    : Texture{config}
 {
-    if (this != &rhs)
-    {
-        m_textureID     = rhs.m_textureID;
-        _aspect_ratio   = rhs._aspect_ratio;
-        rhs.m_textureID = static_cast<GLuint>(-1);
-#if DEBUG
-        m_bDataUploaded = rhs.m_bDataUploaded;
-#endif
-    }
-    return *this;
+    set_image(image);
 }
 
-GLuint Texture::CreateTextureID(GLint interpolationMode, GLint wrapMode)
+void Texture::set_size(img::Size const&)
 {
-    GLuint textureID; // NOLINT
-    GLDebug(glGenTextures(1, &textureID));
-    GLDebug(glBindTexture(GL_TEXTURE_2D, textureID));
-    GLDebug(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, interpolationMode));
-    GLDebug(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, interpolationMode));
-    GLDebug(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, wrapMode));
-    GLDebug(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, wrapMode));
-    GLDebug(glBindTexture(GL_TEXTURE_2D, 0));
-    return textureID;
+    glpp::
 }
 
-void Texture::DestroyTexture(GLuint texID)
+void Texture::set_image(img::Image const&)
 {
-    GLDebug(glDeleteTextures(1, &texID));
 }
 
-Texture::Texture(std::filesystem::path filepath, GLint interpolationMode, GLint wrapMode)
+void Texture::set_interpolation_mode(glpp::Interpolation interpolation_mode)
 {
-    m_textureID = LoadTexture(filepath, interpolationMode, wrapMode, &_aspect_ratio);
-#if DEBUG
-    m_bDataUploaded = true;
-#endif
 }
 
-GLuint Texture::LoadTexture(std::filesystem::path filepath, GLint interpolationMode, GLint wrapMode, float* out_aspect_ratio)
+void Texture::set_wrap_mode(glpp::Wrap wrap_mode)
 {
-    // Load image
-    const auto image = img::load(filepath, 4);
-    // Create texture
-    GLuint texID = Texture::CreateTextureID(interpolationMode, wrapMode);
-    // Upload data
-    GLDebug(glBindTexture(GL_TEXTURE_2D, texID));
-    GLDebug(glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, static_cast<GLsizei>(image.size().width()), static_cast<GLsizei>(image.size().height()), 0, GL_RGBA, GL_UNSIGNED_BYTE, image.data()));
-    //
-    if (out_aspect_ratio)
-    {
-        *out_aspect_ratio = img::SizeU::aspect_ratio(image.size());
-    }
-
-    return texID;
 }
 
-void Texture::genTexture(GLint interpolationMode, GLint wrapMode)
+void Texture::bind()
 {
-#if DEBUG
-    if (m_textureID != static_cast<GLuint>(-1))
-    {
-        Log::Debug::error("Texture::genTexture", "You have already generated that texture!");
-    }
-#endif
-    m_textureID = CreateTextureID(interpolationMode, wrapMode);
+    glpp::bind_texture(_id);
 }
 
-void Texture::uploadRGB(int width, int height, unsigned char* data)
+void Texture::unbind()
 {
-#if DEBUG
-    m_bDataUploaded = true;
-    if (m_textureID == static_cast<GLuint>(-1))
-    {
-        Log::Debug::error("Texture::uploadRGB", "You haven't generated that texture yet!");
-    }
-#endif
-    GLDebug(glBindTexture(GL_TEXTURE_2D, m_textureID));
-    GLDebug(glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB8, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data));
-    GLDebug(glBindTexture(GL_TEXTURE_2D, 0));
-}
-
-void Texture::uploadRGBA(int width, int height, unsigned char* data)
-{
-#if DEBUG
-    m_bDataUploaded = true;
-    if (m_textureID == static_cast<GLuint>(-1))
-    {
-        Log::Debug::error("Texture::uploadRGBA", "You haven't generated that texture yet!");
-    }
-#endif
-    GLDebug(glBindTexture(GL_TEXTURE_2D, m_textureID));
-    GLDebug(glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data));
-    GLDebug(glBindTexture(GL_TEXTURE_2D, 0));
+    glpp::bind_texture<glpp::TextureKind::Tex2D>(0);
 }
 
 void Texture::attachToSlot(int slot) const
--- src/Cool/Gpu/OpenGL/Texture.h
+++ src/Cool/Gpu/OpenGL/Texture.h
@@ -1,107 +1,41 @@
 #pragma once
 #if defined(COOL_OPENGL)
 
-#include <filesystem>
+#include <glpp/glpp.hpp>
+#include "img/src/Image.h"
+#include "img/src/Size.h"
 
 namespace Cool::OpenGL {
 
-/// <summary>
 /// A wrapper for an OpenGL texture2D
-/// </summary>
 class Texture {
 public:
-    Texture() = default;
-    ~Texture();
-    Texture(Texture&&) noexcept;
-    Texture& operator=(Texture&&) noexcept;
+    struct Config {
+        glpp::Interpolation interpolation_mode{glpp::Interpolation::Linear};
+        glpp::Wrap          wrap_mode{glpp::Wrap::ClampToEdge};
+    };
 
-    Texture(const Texture&)            = delete; // Non-copyable
-    Texture& operator=(const Texture&) = delete; // Non-copyable
+    /// Creates an empty texture.
+    explicit Texture(Config = {});
+    /// Create a texture with the given size and no pixel data.
+    explicit Texture(img::Size const&, Config = {});
+    /// Create a texture containing a copy of the image's data.
+    explicit Texture(img::Image const&, Config = {});
+
+    void set_size(img::Size const&);
+    void set_image(img::Image const&);
+    void set_interpolation_mode(glpp::Interpolation interpolation_mode);
+    void set_wrap_mode(glpp::Wrap wrap_mode);
+
+    void        bind();
+    static void unbind();
+
+    /// The native OpenGL ID of the texture.
+    [[nodiscard]] auto id() const -> GLuint { return *_id; }
+    /// The ID that ImGui expects
+    [[nodiscard]] auto imgui_texture_id() const -> ImTextureID { return reinterpret_cast<ImTextureID>(static_cast<uint64_t>(id())); } // Double-cast to fix a warning : first we convert to the correct size (uint32_t -> uint64_t) then from integral type to pointer type (uint64_t -> ImTextureID)
 
     [[nodiscard]] auto aspect_ratio() const -> float { return _aspect_ratio; }
-
-    ImTextureID imgui_texture_id() const { return reinterpret_cast<ImTextureID>(static_cast<uint64_t>(m_textureID)); } // Double-cast to fix a warning : first we convert to the correct size (uint32_t -> uint64_t) then from integral type to pointer type (uint64_t -> ImTextureID)
-
-    /// <summary>
-    /// Constructs a texture from an image.
-    /// </summary>
-    /// <param name="filepath">The path to the image to be loaded in the texture</param>
-    /// <param name="interpolationMode">The OpenGL interpolation mode (how the texture will be resized if needed).
-    /// The default value is GL_LINEAR.
-    /// Can be either GL_LINEAR or GL_NEAREST (nearest neighbour, a.k.a. no real interpolation)
-    /// </param>
-    /// <param name="wrapMode">The OpenGL wrap mode (how the texture will be sampled if you look at coordinates outside the [0, 1] range).
-    /// The default value is GL_CLAMP_TO_EDGE.
-    /// Can be either GL_CLAMP_TO_EDGE, GL_REPEAT, GL_MIRRORED_REPEAT, GL_MIRROR_CLAMP_TO_EDGE (one repetition and then we clamp) or GL_CLAMP_TO_BORDER (like clamp, but uses a fixed color instead of the color at the edge of the texture)
-    /// </param>
-    Texture(std::filesystem::path filepath, GLint interpolationMode = GL_LINEAR, GLint wrapMode = GL_CLAMP_TO_EDGE);
-
-    /// <summary>
-    /// Loads an image into a texture and returns the OpenGL ID. You are then responsible for destroying the texture with Texture::DestroyTexture()
-    /// </summary>
-    /// <param name="filepath">The path to the image to be loaded</param>
-    /// <param name="interpolationMode">The OpenGL interpolation mode (how the texture will be resized if needed).
-    /// The default value is GL_LINEAR.
-    /// Can be either GL_LINEAR or GL_NEAREST (nearest neighbour, a.k.a. no real interpolation)
-    /// </param>
-    /// <param name="wrapMode">The OpenGL wrap mode (how the texture will be sampled if you look at coordinates outside the [0, 1] range).
-    /// The default value is GL_CLAMP_TO_EDGE.
-    /// Can be either GL_CLAMP_TO_EDGE, GL_REPEAT, GL_MIRRORED_REPEAT, GL_MIRROR_CLAMP_TO_EDGE (one repetition and then we clamp) or GL_CLAMP_TO_BORDER (like clamp, but uses a fixed color instead of the color at the edge of the texture)
-    /// </param>
-    /// <returns></returns>
-    static GLuint LoadTexture(std::filesystem::path filepath, GLint interpolationMode = GL_LINEAR, GLint wrapMode = GL_CLAMP_TO_EDGE, float* out_aspect_ratio = nullptr);
-
-    /// <summary>
-    /// Creates an OpenGL texture. You are then responsible for destroying it with Texture::DestroyTexture()
-    /// </summary>
-    /// <param name="interpolationMode">The OpenGL interpolation mode (how the texture will be resized if needed).
-    /// The default value is GL_LINEAR.
-    /// Can be either GL_LINEAR or GL_NEAREST (nearest neighbour, a.k.a. no real interpolation)
-    /// </param>
-    /// <param name="wrapMode">The OpenGL wrap mode (how the texture will be sampled if you look at coordinates outside the [0, 1] range).
-    /// The default value is GL_CLAMP_TO_EDGE.
-    /// Can be either GL_CLAMP_TO_EDGE, GL_REPEAT, GL_MIRRORED_REPEAT, GL_MIRROR_CLAMP_TO_EDGE (one repetition and then we clamp) or GL_CLAMP_TO_BORDER (like clamp, but uses a fixed color instead of the color at the edge of the texture)
-    /// </param>
-    /// <returns>The OpenGL ID of the texture.</returns>
-    static GLuint CreateTextureID(GLint interpolationMode = GL_LINEAR, GLint wrapMode = GL_CLAMP_TO_EDGE);
-
-    /// <summary>
-    /// Destroys the OpenGL texture
-    /// </summary>
-    /// <param name="texID"></param>
-    static void DestroyTexture(GLuint texID);
-
-    /// <summary>
-    /// Actually constructs the OpenGL texture.
-    /// We don't do this in the constructor to allow the use of static textures (those would fail because OpenGL is only initialized once AppManager has been constructed)
-    /// </summary>
-    /// <param name="interpolationMode">The OpenGL interpolation mode (how the texture will be resized if needed).
-    /// The default value is GL_LINEAR.
-    /// Can be either GL_LINEAR or GL_NEAREST (nearest neighbour, a.k.a. no real interpolation)
-    /// </param>
-    /// <param name="wrapMode">The OpenGL wrap mode (how the texture will be sampled if you look at coordinates outside the [0, 1] range).
-    /// The default value is GL_CLAMP_TO_EDGE.
-    /// Can be either GL_CLAMP_TO_EDGE, GL_REPEAT, GL_MIRRORED_REPEAT, GL_MIRROR_CLAMP_TO_EDGE (one repetition and then we clamp) or GL_CLAMP_TO_BORDER (like clamp, but uses a fixed color instead of the color at the edge of the texture)
-    /// </param>
-    void genTexture(GLint interpolationMode = GL_LINEAR, GLint wrapMode = GL_CLAMP_TO_EDGE);
-
-    /// <summary>
-    /// Upload the image data on the GPU (or just the size if you plan on writting on the texture through shaders).
-    /// </summary>
-    /// <param name="width">Width in pixels of the image represented by data</param>
-    /// <param name="height">Height in pixels of the image represented by data</param>
-    /// <param name="data">An array of unsigned chars. You can leave as nullptr if you just want to set the size of the texture. 3 consecutive unsigned chars make a pixel (Red Green Blue).
-    /// The OpenGL convention is that the first pixel in the array is the bottom-left of the image, then the second is on the first row second column and so on.</param>
-    void uploadRGB(int width, int height, unsigned char* data = nullptr);
-
-    /// <summary>
-    /// Upload the image data on the GPU (or just the size if you plan on writting on the texture through shaders).
-    /// </summary>
-    /// <param name="width">Width in pixels of the image represented by data</param>
-    /// <param name="height">Height in pixels of the image represented by data</param>
-    /// <param name="data">An array of unsigned chars. You can leave as nullptr if you just want to set the size of the texture. 4 consecutive unsigned chars make a pixel (Red Green Blue Alpha).
-    /// The OpenGL convention is that the first pixel in the array is the bottom-left of the image, then the second is on the first row second column and so on.</param>
-    void uploadRGBA(int width, int height, unsigned char* data = nullptr);
 
     /// <summary>
     /// Attaches your texture to a slot, so that it is ready to be read by a shader.
@@ -109,16 +43,11 @@ public:
     /// <param name="slot">The slot. This should match the "uniform sampler2D u_TextureSlot" in your shader that is set through set_uniform(slot)</param>
     void attachToSlot(int slot) const;
 
-    /// <summary>
-    /// The OpenGL ID of the texture.
-    /// </summary>
-    inline GLuint ID() { return m_textureID; }
-
 private:
-    GLuint m_textureID = static_cast<GLuint>(-1);
+    glpp::UniqueTexture2D _id{};
     float  _aspect_ratio{};
 #if DEBUG
-    bool m_bDataUploaded = false;
+    bool m_bDataUploaded{false};
 #endif
 };
 
