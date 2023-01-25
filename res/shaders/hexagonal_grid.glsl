462c6977add5baaef82327535ece4bbf3d2ec84a
Jules Fouchy <jules.fouchy@ntymail.com>
2023-01-26 00:15:01 +0100

    🧼 [shader] Hexagonal grid: use ivec2 for id

--- res/shaders/hexagonal_grid.glsl
+++ res/shaders/hexagonal_grid.glsl
@@ -3,41 +3,36 @@
 // https://www.youtube.com/watch?v=VmrIDyYiJBA&ab_channel=TheArtofCode
 // https://andrewhungblog.wordpress.com/2018/07/28/shader-art-tutorial-hexagonal-grids/
 
-// pixel -> id of the first grid
-vec2 internal_Cool_pix_to_grid_ID1(vec2 uv)
+ivec2 internal_Cool_pix_to_grid_id1(vec2 uv)
 {
-    return floor((uv + vec2(0, 0.25)) / vec2(1., sqrt_3) - vec2(0, 0.25));
+    return ivec2(floor((uv + vec2(0, 0.25)) / vec2(1., sqrt_3) - vec2(0, 0.25)));
 }
-vec2 internal_Cool_id1_to_center(vec2 id)
+ivec2 internal_Cool_pix_to_grid_id2(vec2 uv)
 {
-    return (id + 0.5) * vec2(1., sqrt_3);
+    return ivec2(floor((uv - vec2(0.5) + vec2(0, 0.25)) / vec2(1., sqrt_3) - vec2(0, 0.25)));
 }
-
-// pixel -> id of the second grid
-vec2 internal_Cool_id2_to_center(vec2 id)
+vec2 internal_Cool_id1_to_center(ivec2 id)
 {
-    return (id + 1.) * vec2(1., sqrt_3);
+    return (vec2(id) + 0.5) * vec2(1., sqrt_3);
 }
-vec2 internal_Cool_pix_to_grid_id2(vec2 uv)
+vec2 internal_Cool_id2_to_center(ivec2 id)
 {
-    return floor((uv - vec2(0.5) + vec2(0, 0.25)) / vec2(1., sqrt_3) - vec2(0, 0.25));
+    return (vec2(id) + 1.) * vec2(1., sqrt_3);
 }
 
-
-// TODO Use ivec2
-vec2 Cool_hex_id(vec2 uv, float grid_size)
+ivec2 Cool_hex_id(vec2 uv, float grid_size)
 {
     uv *= grid_size;
 
-    vec2 id1 = internal_Cool_pix_to_grid_ID1(uv);
-    vec2 id2 = internal_Cool_pix_to_grid_id2(uv);
+    ivec2 id1 = internal_Cool_pix_to_grid_id1(uv);
+    ivec2 id2 = internal_Cool_pix_to_grid_id2(uv);
 
     vec2 center1 = internal_Cool_id1_to_center(id1);
     vec2 center2 = internal_Cool_id2_to_center(id2);
 
-    vec2 id = length_squared(uv - center1) < length_squared(uv - center2)
-                  ? id1.x * vec2(1, 0) + id1.y * vec2(1, 2) + vec2(0, 1)
-                  : id2.x * vec2(1, 0) + id2.y * vec2(1, 2) + vec2(1, 2);
+    ivec2 id = length_squared(uv - center1) < length_squared(uv - center2)
+                  ? id1.x * ivec2(1, 0) + id1.y * ivec2(1, 2) + ivec2(0, 1)
+                  : id2.x * ivec2(1, 0) + id2.y * ivec2(1, 2) + ivec2(1, 2);
     return id;
 }
 
@@ -56,7 +51,7 @@ vec2 Cool_hex_uv(vec2 uv, float grid_size)
                : b;
 }
 
-vec2 Cool_hex_center_from_hex_id(vec2 id, float grid_size)
+vec2 Cool_hex_center_from_hex_id(ivec2 id, float grid_size)
 {
     return ((id.x + 1) * vec2(1,0) + id.y * vec2(-0.5,sqrt_3/2)) / grid_size;
 }
