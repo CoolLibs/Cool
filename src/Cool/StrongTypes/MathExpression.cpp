4768703acffb1806d0a691581c2bdfb5df9a26c6
Jules Fouchy <jules.fouchy@ntymail.com>
2023-02-03 12:42:53 +0100

    🤏 [MathExpression] Trigger update whenever anything changes

--- src/Cool/StrongTypes/MathExpression.cpp
+++ src/Cool/StrongTypes/MathExpression.cpp
@@ -7,8 +7,7 @@ namespace Cool {
 
 auto imgui_widget(std::string_view name, MathExpression& math_expression) -> bool
 {
-    return ImGui::InputText(name.data(), &math_expression.expression, ImGuiInputTextFlags_EnterReturnsTrue)
-           || ImGui::IsItemActivated(); // Required for the history to work properly. Since we only return true when enter is pressed, it is possible that the history misses some changes to the variable. This is why we force the history to save the state of the variable when someone is about to type into the text input.
+    return ImGui::InputText(name.data(), &math_expression.expression);
 }
 
 } // namespace Cool
