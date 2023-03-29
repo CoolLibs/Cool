68b6f19c17773d336963693213913fe2bd3c4897
Jules Fouchy <jules.fouchy@ntymail.com>
2023-03-29 18:25:02 +0200

    🚚 [Gradient] Move implementation to a .cpp

--- src/Cool/StrongTypes/Gradient.h
+++ src/Cool/StrongTypes/Gradient.h
@@ -47,40 +47,12 @@ private:
     }
 };
 
-inline auto to_string(Cool::Gradient) -> std::string
-{
-    return "Gradient [...]";
-}
+auto to_string(Cool::Gradient const&) -> std::string;
 
-inline auto imgui_widget(std::string_view name, Cool::Gradient& gradient, bool should_use_a_random_color_for_the_new_marks, ImGuiColorEditFlags flags) -> bool
-{
-    auto modified = gradient.value.widget(
-        name.data(),
-        {
-            .flags                                       = ImGG::Flag::NoResetButton,
-            .color_edit_flags                            = flags,
-            .should_use_a_random_color_for_the_new_marks = should_use_a_random_color_for_the_new_marks,
-            .plus_button_text                            = "+",
-            .minus_button_text                           = "-",
-        }
-    );
-    modified |= ImGG::interpolation_mode_widget("Interpolation Mode", &gradient.value.gradient().interpolation_mode());
-    ImGui::SameLine();
-    modified |= ImGG::wrap_mode_widget("Wrap Mode", &gradient.wrap_mode);
-    ImGui::SameLine();
-    if (ImGui::Button("Spread marks evenly"))
-    {
-        gradient.value.gradient().spread_marks_evenly();
-        modified = true;
-    }
-    return modified;
-}
+auto imgui_widget(std::string_view name, Cool::Gradient& gradient, bool should_use_a_random_color_for_the_new_marks, ImGuiColorEditFlags flags) -> bool;
 
 namespace internal {
-inline auto gradient_marks_array_name(std::string_view name) -> std::string
-{
-    return fmt::format("{}_", name);
-}
+auto gradient_marks_array_name(std::string_view name) -> std::string;
 } // namespace internal
 
 } // namespace Cool
