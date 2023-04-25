43210a69360c43d6d4e0bf7f7b9d2b18dae93cdf
Jules Fouchy <jules.fouchy@ntymail.com>
2023-04-25 14:23:22 +0200

    🚧 WIP

--- src/Cool/Path/Path.cpp
+++ src/Cool/Path/Path.cpp
@@ -4,12 +4,14 @@ namespace Cool {
 
 std::filesystem::path Path::_root;
 std::filesystem::path Path::_cool_res;
+std::filesystem::path Path::_default_texture;
 #if DEBUG
-bool Path::_root_is_initialized     = false;
-bool Path::_cool_res_is_initialized = false;
+bool Path::_root_is_initialized            = false;
+bool Path::_cool_res_is_initialized        = false;
+bool Path::_default_texture_is_initialized = false;
 #endif
 
-const std::filesystem::path& Path::root()
+auto Path::root() -> const std::filesystem::path&
 {
 #if DEBUG // On some compilers asserts are compiled even when not in DEBUG
     assert(_root_is_initialized);
@@ -26,7 +28,7 @@ void Path::initialize_root(std::filesystem::path path)
     _root = path;
 }
 
-const std::filesystem::path& Path::cool_res()
+auto Path::cool_res() -> const std::filesystem::path&
 {
 #if DEBUG // On some compilers asserts are compiled even when not in DEBUG
     assert(_cool_res_is_initialized);
@@ -41,6 +43,23 @@ void Path::initialize_cool_res(std::filesystem::path path)
     _cool_res_is_initialized = true;
 #endif
     _cool_res = path;
+}
+
+auto Path::default_texture() -> const std::filesystem::path&
+{
+#if DEBUG // On some compilers asserts are compiled even when not in DEBUG
+    assert(_default_texture_is_initialized);
+#endif
+    return _default_texture;
+}
+
+void Path::initialize_default_texture(std::filesystem::path path)
+{
+#if DEBUG
+    assert(!_default_texture_is_initialized);
+    _default_texture_is_initialized = true;
+#endif
+    _default_texture = path;
 }
 
 } // namespace Cool
--- src/Cool/Path/Path.h
+++ src/Cool/Path/Path.h
@@ -7,10 +7,13 @@ namespace Cool {
 class Path {
 public:
     /// Root directory where the program was started from. Use this to describe your ressources by an absolute path, especially if you play at runtime with the directories and your relative paths get messed up
-    static std::filesystem::path const& root();
+    [[nodiscard]] static auto root() -> std::filesystem::path const&;
 
     /// Directory where "Cool/res" is located, or where the Cool resources have been copied. This path will always be valid to refer to "Cool/res"
-    static std::filesystem::path const& cool_res();
+    [[nodiscard]] static auto cool_res() -> std::filesystem::path const&;
+
+    /// Path to the default texture used by TextureInfo.
+    [[nodiscard]] static auto default_texture() -> std::filesystem::path const&;
 
     /// Initializes root. This must be called once, at the start of your application
     static void initialize_root(std::filesystem::path);
@@ -18,12 +21,17 @@ public:
     /// Initializes cool_res. This must be called once, at the start of your application
     static void initialize_cool_res(std::filesystem::path);
 
+    /// Initializes default_texture. This must be called once, at the start of your application
+    static void initialize_default_texture(std::filesystem::path);
+
 private:
     static std::filesystem::path _root;
     static std::filesystem::path _cool_res;
+    static std::filesystem::path _default_texture;
 #if DEBUG
     static bool _root_is_initialized;
     static bool _cool_res_is_initialized;
+    static bool _default_texture_is_initialized;
 #endif
 };
 
