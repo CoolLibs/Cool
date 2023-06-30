de1565c046086d6efef85c404f3db8e4e2302ba7
TRISTAN <debeaunetristan@gmail.com>
2023-06-30 17:58:33 +0200

    🚧 Widget Webcam node now has Combo for Webcam displayed

--- src/Cool/Webcam/WebcamInfo.cpp
+++ src/Cool/Webcam/WebcamInfo.cpp
@@ -1,4 +1,7 @@
 #include "WebcamInfo.h"
+#include <imgui.h>
+#include <charconv>
+#include <string>
 #include "Cool/ImGui/ImGuiExtras.h"
 #include "Cool/NfdFileFilter/NfdFileFilter.h"
 
@@ -7,6 +10,16 @@ namespace Cool {
 auto WebcamInfo::imgui_widget(const int number_of_webcam) -> bool
 {
     bool b = false;
+
+    std::string text;
+    for (int i = 0; i < number_of_webcam; i++)
+    {
+        char val = static_cast<char>(i);
+        text.append("0" + i);
+        text.push_back('\0');
+    }
+
+    b |= ImGui::Combo("Camera Displayed", &webcam_index, text.c_str());
 
     b |= ImGui::Combo("Repeat Mode", reinterpret_cast<int*>(&sampler.repeat_mode), "None\0Mirror\0Mosaic\0Clamp\0\0");
 
