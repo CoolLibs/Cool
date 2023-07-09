2a8dbb3df6799d58065b2521f765618dd72bac45
Jules Fouchy <jules.fouchy@ntymail.com>
2023-07-09 11:52:32 +0200

    🐛 [Exporter] Store the GUI in a unique_ptr so that the address doesn't change

--- src/Cool/Exporter/Exporter.h
+++ src/Cool/Exporter/Exporter.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <memory>
 #include "ExporterGui.h"
 #include "VideoExportProcess.h"
 
@@ -16,23 +17,23 @@ public:
 
     [[nodiscard]] auto is_exporting() const -> bool { return Cool::ExporterGui::is_exporting(_video_export_process); }
 
-    auto image_export_window() -> ImGuiWindow& { return _gui.image_export_window(); }
-    auto video_export_window() -> ImGuiWindow& { return _gui.video_export_window(); }
+    auto image_export_window() -> ImGuiWindow& { return _gui->image_export_window(); }
+    auto video_export_window() -> ImGuiWindow& { return _gui->video_export_window(); }
 
-    void set_aspect_ratio(AspectRatio const& aspect_ratio) { _gui.set_aspect_ratio(aspect_ratio); }
-    void maybe_set_aspect_ratio(std::optional<AspectRatio> const& aspect_ratio) { _gui.maybe_set_aspect_ratio(aspect_ratio); }
+    void set_aspect_ratio(AspectRatio const& aspect_ratio) { _gui->set_aspect_ratio(aspect_ratio); }
+    void maybe_set_aspect_ratio(std::optional<AspectRatio> const& aspect_ratio) { _gui->maybe_set_aspect_ratio(aspect_ratio); }
 
     /// Displays all the currently active windows.
-    void imgui_windows(Polaroid const& polaroid, float time) { _gui.imgui_windows(polaroid, time, _video_export_process); }
+    void imgui_windows(Polaroid const& polaroid, float time) { _gui->imgui_windows(polaroid, time, _video_export_process); }
 
     /// The buttons to open the different exporter windows.
-    void imgui_menu_items(imgui_menu_items_Params const& params = {}, std::optional<std::string> const& longest_text = {}) { _gui.imgui_menu_items(params, longest_text); }
+    void imgui_menu_items(imgui_menu_items_Params const& params = {}, std::optional<std::string> const& longest_text = {}) { _gui->imgui_menu_items(params, longest_text); }
 
     /// Call this after your rendering code. If we are exporting it will export the current frame and decide if the export should continue.
-    void update(Polaroid const& polaroid) { _gui.update(polaroid, _video_export_process); }
+    void update(Polaroid const& polaroid) { _gui->update(polaroid, _video_export_process); }
 
 private:
-    ExporterGui                       _gui;
+    std::unique_ptr<ExporterGui>      _gui{std::make_unique<ExporterGui>()}; // Pointer so that the adress doesn't change when we move the Exporter (important because we have events that store references to things in the gui)
     std::optional<VideoExportProcess> _video_export_process;
 
 private:
