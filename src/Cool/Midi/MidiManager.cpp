389eb6dae1ba05f5ff35b9c48d60415bbd20f701
Jules Fouchy <jules.fouchy@ntymail.com>
2023-09-24 15:10:55 +0200

    ✨ [Midi] Added imgui_visualize_channels()

--- src/Cool/Midi/MidiManager.cpp
+++ src/Cool/Midi/MidiManager.cpp
@@ -9,13 +9,23 @@ RtMidi::Api MidiManager::chooseMidiApi()
     return static_cast<RtMidi::Api>(0); // i );
     // ok return RtMidi::Api::WINDOWS_MM;
 }
-auto MidiManager::get_value(MidiCc const& midiCc) -> float
+
+auto MidiManager::get_value(MidiCc const& midiCc) const -> float
 {
-    return mIndexToValue[midiCc.index];
+    auto const it = mIndexToValue.find(midiCc.index);
+    if (it == mIndexToValue.end())
+        return 0.f;
+    return it->second;
 }
-void MidiManager::midiCallback(double deltatime, std::vector<unsigned char>* message, void* userData)
+
+void MidiManager::set_value(MidiCc const& cc, float value)
 {
-    MidiManager* midiManager = static_cast<MidiManager*>(userData);
+    mIndexToValue[cc.index] = value;
+}
+
+void MidiManager::midiCallback(double /* delta_time */, std::vector<unsigned char>* message, void* user_data)
+{
+    MidiManager* midiManager = static_cast<MidiManager*>(user_data);
     unsigned int nBytes      = message->size();
     if (nBytes > 2)
     {
@@ -96,17 +106,43 @@ void MidiManager::imgui()
     // }
 }
 
+auto MidiManager::max_index() const -> int
+{
+    auto const it = std::max_element(mIndexToValue.begin(), mIndexToValue.end(), [](auto const& p1, auto const& p2) {
+        return p1.first < p2.first; // Compare the indices
+    });
+
+    if (it == mIndexToValue.end())
+        return 0;
+    return it->first;
+}
+
 void MidiManager::imgui_emulate_midi_keyboard()
 {
-    static auto values = std::vector<float>(6);
-    for (size_t i = 0; i < values.size(); ++i)
+    static auto values = std::vector<float>(31);
+    for (int i = 0; i < static_cast<int>(values.size()); ++i)
     {
-        if (ImGui::SliderFloat(fmt::format("Index {}", i).c_str(), &values[i], 0.f, 1.f))
+        ImGui::PushID(i);
+        if (ImGui::VSliderFloat("", ImVec2(30, 160), &values[i], 0.f, 1.f, ""))
         {
             std::vector<unsigned char> message{0, static_cast<unsigned char>(i), static_cast<unsigned char>(values[i] * 127)};
             midiCallback(0.f, &message, this);
         }
+        ImGui::SetItemTooltip("%i", i);
+        if (i != static_cast<int>(values.size()) - 1)
+            ImGui::SameLine();
+        ImGui::PopID();
     }
+}
+
+void MidiManager::imgui_visualize_channels()
+{
+    ImGui::Text(buf);
+    auto values = std::vector<float>(max_index() + 1);
+    for (int i = 0; i < static_cast<int>(values.size()); ++i)
+        values[i] = get_value({i});
+
+    ImGui::PlotHistogram("Channels", values.data(), static_cast<int>(values.size()), 0, nullptr, 0.f, 1.f, ImVec2(0, 80.0f));
 }
 
 } // namespace Cool
--- src/Cool/Midi/MidiManager.h
+++ src/Cool/Midi/MidiManager.h
@@ -11,7 +11,16 @@ namespace Cool {
 
 class MidiManager {
 public:
-    auto get_value(MidiCc const&) -> float;
+    friend auto midi_manager() -> MidiManager&;
+
+    /// Returns a number between 0 and 1 representing the current state of the controller.
+    /// /!\ Note that if a knob hasn't been used yet we won't know it's current state and return 0 instead.
+    [[nodiscard]] auto get_value(MidiCc const&) const -> float;
+
+    /// You can use this if you want to emulate a midi keyboard.
+    /// `value` must be between 0 and 1.
+    void set_value(MidiCc const&, float value);
+
     void connect();
     void disconnect();
     void imgui();
@@ -19,6 +28,12 @@ public:
     void set_additional_midi_callback(std::function<void()> callback) { _extra_midi_callback = std::move(callback); }
 
     void imgui_emulate_midi_keyboard();
+    void imgui_visualize_channels();
+
+private:
+    MidiManager() = default; // Use `mini_manager()` to get the unique instance of MidiManager.
+
+    [[nodiscard]] auto max_index() const -> int;
 
 private:
     RtMidiIn*    mMidiIn;
@@ -28,8 +43,7 @@ private:
     static void  midiCallback(double deltatime, std::vector<unsigned char>* message, void* userData);
     RtMidi::Api  chooseMidiApi();
     // imgui
-    char buf[64];
-    // TODO(Midi) map index vers float
+    char                           buf[64];
     std::unordered_map<int, float> mIndexToValue;
     std::function<void()>          _extra_midi_callback{[] {
     }};
