51876b0bcdf5fbb93d061c67ec7ccc1cca9389ae
Jules Fouchy <jules.fouchy@ntymail.com>
2023-09-28 13:56:34 +0200

    🚧 [Midi] MidiChannel gui improvements

--- src/Cool/Variables/Variable_MidiChannel.cpp
+++ src/Cool/Variables/Variable_MidiChannel.cpp
@@ -1,10 +1,21 @@
 #include "Variable_MidiChannel.h"
+#include <imgui.h>
+#include "Cool/ImGui/IcoMoonCodepoints.h"
+#include "Cool/ImGui/ImGuiExtras.h"
 
 namespace Cool {
 
 auto imgui_widget(Variable<MidiChannel>& var) -> bool
 {
-    return ImGui::InputInt(var.name().c_str(), &var.value().index, 1, 1);
+    if (ImGuiExtras::button_with_text_icon(ICOMOON_COG))
+    {
+        // TODO(Midi) Open the Midi Config window
+    }
+    ImGui::SetItemTooltip("%s", "Open Midi Config to select the controller in use and verify we receive messages from it.");
+
+    bool const b = ImGui::InputScalar(var.name().c_str(), ImGuiDataType_S32, &var.value().index);
+
+    return b;
 }
 
 auto imgui_widget(VariableMetadata<MidiChannel>&) -> bool
