b22fbfee1370ecd4c65975b60d7ac7a40d819654
TotoShampoin <thomas.garnier@totoshampoin.fr>
2023-12-14 15:56:13 +0100

    Added "camera view projection" (not the inverse)

--- src/Cool/Camera/CameraShaderU.h
+++ src/Cool/Camera/CameraShaderU.h
@@ -10,6 +10,7 @@ namespace Cool::CameraShaderU {
 inline void set_uniform(const OpenGL::Shader& shader, const Camera& camera, float aspect_ratio)
 {
     shader.set_uniform("cool_camera_inverse_view_projection", camera.inverse_view_projection_matrix(aspect_ratio));
+    shader.set_uniform("cool_camera_view_projection", camera.view_projection_matrix(aspect_ratio));
     shader.set_uniform("cool_camera_far_plane", camera.projection().far_plane);
 }
 
