639394b1257d5c5dc21589a1ee54d3f579fffc17
TotoShampoin <thomas.garnier@totoshampoin.fr>
2023-12-14 15:53:52 +0100

    added a camera for the uniforms

--- src/Cool/Camera/CameraShaderU.h
+++ src/Cool/Camera/CameraShaderU.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <Cool/Gpu/OpenGL/Shader.h>
+#include "Cool/Camera/Camera.h"
 
 namespace Cool::CameraShaderU {
 
@@ -10,6 +11,7 @@ namespace Cool::CameraShaderU {
 inline void set_uniform(const OpenGL::Shader& shader, const Camera& camera, float aspect_ratio)
 {
     shader.set_uniform("cool_camera_inverse_view_projection", camera.inverse_view_projection_matrix(aspect_ratio));
+    shader.set_uniform("cool_camera_view_projection", camera.view_projection_matrix(aspect_ratio));
     shader.set_uniform("cool_camera_far_plane", camera.projection().far_plane);
 }
 
