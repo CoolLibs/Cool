29506661afec0374185cbb9783f4cafa53c5fdce
Jules Fouchy <jules.fouchy@ntymail.com>
2023-12-29 17:52:38 +0100

    ⚡️ [rand.glsl] Improved performance of hash_ND_to_ND() functions

--- res/shaders/rand.glsl
+++ res/shaders/rand.glsl
@@ -1,7 +1,5 @@
 // Inspired by https://stackoverflow.com/a/17479300
 
-const float UINT_MAX_AS_FLOAT = 4294967295.;
-
 uint uint_hash_1D(uint seed)
 {
     // A single iteration of Bob Jenkins' One-At-A-Time hashing algorithm.
@@ -25,68 +23,110 @@ uint uint_hash_4D(uvec4 seed)
     return uint_hash_1D(seed.x ^ uint_hash_3D(seed.yzw));
 }
 
+float float_0_to_1(uint x)
+{
+    const float UINT_MAX_AS_FLOAT = 4294967295.;
+    return float(x) / UINT_MAX_AS_FLOAT;
+}
+vec2 vec2_0_to_1(uvec2 x)
+{
+    const float UINT_MAX_AS_FLOAT = 4294967295.;
+    return vec2(x) / UINT_MAX_AS_FLOAT;
+}
+vec3 vec3_0_to_1(uvec3 x)
+{
+    const float UINT_MAX_AS_FLOAT = 4294967295.;
+    return vec3(x) / UINT_MAX_AS_FLOAT;
+}
+vec4 vec4_0_to_1(uvec4 x)
+{
+    const float UINT_MAX_AS_FLOAT = 4294967295.;
+    return vec4(x) / UINT_MAX_AS_FLOAT;
+}
+
 float hash_0_to_1_1D_to_1D(float seed)
 {
-    return float(uint_hash_1D(floatBitsToUint(seed))) / UINT_MAX_AS_FLOAT;
+    return float_0_to_1(uint_hash_1D(floatBitsToUint(seed)));
 }
 float hash_0_to_1_2D_to_1D(vec2 seed)
 {
-    return float(uint_hash_2D(floatBitsToUint(seed))) / UINT_MAX_AS_FLOAT;
+    return float_0_to_1(uint_hash_2D(floatBitsToUint(seed)));
 }
 float hash_0_to_1_3D_to_1D(vec3 seed)
 {
-    return float(uint_hash_3D(floatBitsToUint(seed))) / UINT_MAX_AS_FLOAT;
+    return float_0_to_1(uint_hash_3D(floatBitsToUint(seed)));
 }
 float hash_0_to_1_4D_to_1D(vec4 seed)
 {
-    return float(uint_hash_4D(floatBitsToUint(seed))) / UINT_MAX_AS_FLOAT;
+    return float_0_to_1(uint_hash_4D(floatBitsToUint(seed)));
 }
 
 vec2 hash_0_to_1_1D_to_2D(float seed)
 {
-    float rand1 = hash_0_to_1_1D_to_1D(seed);
-    float rand2 = hash_0_to_1_1D_to_1D(rand1);
-    return vec2(rand1, rand2);
+    uint rand1 = uint_hash_1D(floatBitsToUint(seed));
+    uint rand2 = uint_hash_1D(rand1);
+    return vec2_0_to_1(uvec2(
+        rand1,
+        rand2
+    ));
 }
 vec3 hash_0_to_1_1D_to_3D(float seed)
 {
-    float rand1 = hash_0_to_1_1D_to_1D(seed);
-    float rand2 = hash_0_to_1_1D_to_1D(rand1);
-    float rand3 = hash_0_to_1_1D_to_1D(rand2);
-    return vec3(rand1, rand2, rand3);
+    uint rand1 = uint_hash_1D(floatBitsToUint(seed));
+    uint rand2 = uint_hash_1D(rand1);
+    uint rand3 = uint_hash_1D(rand2);
+    return vec3_0_to_1(uvec3(
+        rand1,
+        rand2,
+        rand3
+    ));
 }
 vec4 hash_0_to_1_1D_to_4D(float seed)
 {
-    float rand1 = hash_0_to_1_1D_to_1D(seed);
-    float rand2 = hash_0_to_1_1D_to_1D(rand1);
-    float rand3 = hash_0_to_1_1D_to_1D(rand2);
-    float rand4 = hash_0_to_1_1D_to_1D(rand3);
-    return vec4(rand1, rand2, rand3, rand4);
+    uint rand1 = uint_hash_1D(floatBitsToUint(seed));
+    uint rand2 = uint_hash_1D(rand1);
+    uint rand3 = uint_hash_1D(rand2);
+    uint rand4 = uint_hash_1D(rand3);
+    return vec4_0_to_1(uvec4(
+        rand1,
+        rand2,
+        rand3,
+        rand4
+    ));
 }
 
-vec2 hash_0_to_1_2D_to_2D(vec2 p)
+vec2 hash_0_to_1_2D_to_2D(vec2 seed)
 {
-    return vec2(
-        hash_0_to_1_2D_to_1D(p.xy),
-        hash_0_to_1_2D_to_1D(p.yx)
-    );
+    uint rand1 = uint_hash_2D(floatBitsToUint(seed));
+    uint rand2 = uint_hash_1D(rand1);
+    return vec2_0_to_1(uvec2(
+        rand1,
+        rand2
+    ));
 }
-vec3 hash_0_to_1_3D_to_3D(vec3 p)
+vec3 hash_0_to_1_3D_to_3D(vec3 seed)
 {
-    return vec3(
-        hash_0_to_1_3D_to_1D(p.xyz),
-        hash_0_to_1_3D_to_1D(p.yxz),
-        hash_0_to_1_3D_to_1D(p.zxy)
-    );
+    uint rand1 = uint_hash_3D(floatBitsToUint(seed));
+    uint rand2 = uint_hash_1D(rand1);
+    uint rand3 = uint_hash_1D(rand2);
+    return vec3_0_to_1(uvec3(
+        rand1,
+        rand2,
+        rand3
+    ));
 }
-vec4 hash_0_to_1_4D_to_4D(vec4 p)
+vec4 hash_0_to_1_4D_to_4D(vec4 seed)
 {
-    return vec4(
-        hash_0_to_1_4D_to_1D(p.xyzw),
-        hash_0_to_1_4D_to_1D(p.yxzw),
-        hash_0_to_1_4D_to_1D(p.zxyw),
-        hash_0_to_1_4D_to_1D(p.wxyz)
-    );
+    uint rand1 = uint_hash_4D(floatBitsToUint(seed));
+    uint rand2 = uint_hash_1D(rand1);
+    uint rand3 = uint_hash_1D(rand2);
+    uint rand4 = uint_hash_1D(rand3);
+    return vec4_0_to_1(uvec4(
+        rand1,
+        rand2,
+        rand3,
+        rand4
+    ));
 }
 
 vec2 random_point_on_disk(float seed)
