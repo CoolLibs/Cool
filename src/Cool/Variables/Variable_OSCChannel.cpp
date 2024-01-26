f9be8f885c1ca49ecef8d9ff533de547a8da321f
Jules Fouchy <jules.fouchy@ntymail.com>
2024-01-26 21:17:04 +0100

    🧼 [OSC] Cleanup

--- src/Cool/Variables/Variable_OSCChannel.cpp
+++ src/Cool/Variables/Variable_OSCChannel.cpp
@@ -10,7 +10,7 @@ auto imgui_widget(Variable<OSCChannel>& var) -> bool
 {
     if (ImGuiExtras::button_with_text_icon(ICOMOON_COG))
         osc_manager().open_config_window();
-    ImGui::SetItemTooltip("%s", "Open OSC config to choose which port and address to listen to.");
+    ImGui::SetItemTooltip("%s", "Open OSC config to choose which port and IP address to listen to.");
     osc_manager().imgui_error_message_for_invalid_endpoint("\nUse the  " ICOMOON_COG " button above to fix this.");
 
     return osc_manager().imgui_channel_widget(var.name().c_str(), var.value());
