631a79a6f1b4df5e360fa7cd13c87584d02c55cd
Jules Fouchy <jules.fouchy@ntymail.com>
2024-02-01 10:14:06 +0100

    🧼 [Variables] [InputParser] Remove dead code

--- src/Cool/InputParser/InputParser.h
+++ src/Cool/InputParser/InputParser.h
@@ -1,25 +1,7 @@
 #include <Cool/Dependencies/AnyInput.h>
 
 namespace Cool {
-
-/// Parses all the INPUT lines from `text`.
-/// See try_parse_input() to know what an INPUT line is.
-/// Might return an error message instead of the inputs if the parsing failed.
-auto parse_all_inputs(
-    std::string_view text,
-    DirtyFlag        dirty_flag,
-    DirtyFlag        secondary_dirty_flag
-) -> tl::expected<std::vector<AnyInput>, std::string>;
-
-/// Takes a line of the form "INPUT float x;"
-/// and returns an Input with the corresponding metadata (or std::nullopt if the parsing failed).
-auto try_parse_input(
-    std::string_view line,
-    DirtyFlag        dirty_flag,
-    DirtyFlag        secondary_dirty_flag
-) -> std::optional<AnyInput>;
-
-// TODO(Variables) Split in two files?
+// TODO(Variables) Rename file
 
 /// Defines the uniform in shader code; e.g. something like `uniform float name;` (or something more complex for some types like Gradient).
 auto gen_input_shader_code(AnyInput const&) -> std::string;
