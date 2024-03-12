6b5fff0789f45c7b70da24c4387a3a0e379838f6
Jules Fouchy <jules.fouchy@ntymail.com>
2024-03-12 18:47:26 +0100

    🧼 [TimeSpeed] Cleanup

--- src/Cool/Time/Clock.cpp
+++ src/Cool/Time/Clock.cpp
@@ -33,9 +33,16 @@ void Clock::toggle_play_pause()
 
 static auto as_float_in_seconds_from_nanoseconds(int64_t time_in_nanoseconds) -> float
 {
-    int64_t const bob = 1'000'000'000;
-    return static_cast<float>(time_in_nanoseconds / bob) // NOLINT(bugprone-integer-division)
-           + static_cast<float>(time_in_nanoseconds % bob) / static_cast<float>(bob);
+    return static_cast<float>(time_in_nanoseconds / 1'000'000'000) // NOLINT(bugprone-integer-division)
+           + static_cast<float>(time_in_nanoseconds % 1'000'000'000) / static_cast<float>(1'000'000'000);
+}
+static auto as_nanoseconds_from_float_in_seconds(float time_in_seconds) -> int64_t
+{
+    float const floor = std::floor(time_in_seconds);
+    float const fract = time_in_seconds - floor;
+
+    return static_cast<int64_t>(floor) * 1'000'000'000
+           + static_cast<int64_t>(fract * 1'000'000'000.f);
 }
 
 auto Clock::delta_time_in_seconds() const -> float
@@ -50,20 +57,24 @@ auto Clock::time_in_seconds() const -> float
 
 void Clock::set_time(float new_time_in_seconds, bool force_delta_time_to_ignore_the_change)
 {
-    auto const floor = std::floor(new_time_in_seconds);
-    auto const fract = new_time_in_seconds - floor;
-    auto const bob   = static_cast<int64_t>(floor) * 1'000'000'000
-                     + static_cast<int64_t>(fract * 1'000'000'000.f);
+    auto const new_time_in_nanoseconds = as_nanoseconds_from_float_in_seconds(new_time_in_seconds);
+
     if (!force_delta_time_to_ignore_the_change)
-        _extra_delta_time_in_nanoseconds += bob - _time_in_nanoseconds;
-    _time_in_nanoseconds = bob;
+        _extra_delta_time_in_nanoseconds += new_time_in_nanoseconds - _time_in_nanoseconds;
+    _time_in_nanoseconds = new_time_in_nanoseconds;
 }
 
 void Clock::update()
 {
-    int64_t const real_time_elapsed = _is_playing
-                                          ? static_cast<int64_t>(static_cast<float>(update_and_get_delta_time_in_nanoseconds()) * _time_speed.value().value)
-                                          : 0;
+    int64_t const real_time_elapsed = [&]() -> int64_t { // IIFE
+        if (!_is_playing)
+            return 0;
+
+        int64_t const delta_time = update_and_get_delta_time_in_nanoseconds();
+        if (_time_speed.value().value == 1.f) // This is the most common case, and we don't want to waste precision by converting back and forth between float and int64_t.
+            return delta_time;
+        return static_cast<int64_t>(static_cast<float>(delta_time) * _time_speed.value().value);
+    }();
     _time_in_nanoseconds += real_time_elapsed;
     _delta_time_in_nanoseconds = real_time_elapsed
                                  + _extra_delta_time_in_nanoseconds;
