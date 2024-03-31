176c4663b5cff1e67754a556e0753a47e253ec1c
Jules Fouchy <jules.fouchy@ntymail.com>
2024-03-31 16:42:47 +0200

    🤏 [AspetRatio] Better rounding of width and height to match aspect ratio

--- src/Cool/Exporter/ExportSize.cpp
+++ src/Cool/Exporter/ExportSize.cpp
@@ -16,16 +16,21 @@ void ExportSize::set_aspect_ratio(AspectRatio const& aspect_ratio)
 
 void ExportSize::apply_aspect_ratio()
 {
+    // Fun fact : our algorithm doesn't match https://www.adobe.com/uk/creativecloud/design/discover/a4-format.html
+    // For example, for an A4 ratio the website suggests 3508x2480
+    // but for a height of 2480, a width  of 3507 gives us a better approximation of the A4 ratio than 3508
+    // and for a width  of 3508, a height of 2481 gives us a better approximation of the A4 ratio than 2480
+    // So the A4 formats listed on the website are worse approximations than what this algorithm will give you.
     if (_last_changed_side == ImageSizeU::WH::Width)
     {
         _size.set_height(static_cast<decltype(_size)::DataType>(
-            static_cast<float>(_size.width()) / _aspect_ratio.get()
+            std::round(static_cast<float>(_size.width()) / _aspect_ratio.get())
         ));
     }
     else
     {
         _size.set_width(static_cast<decltype(_size)::DataType>(
-            static_cast<float>(_size.height()) * _aspect_ratio.get()
+            std::round(static_cast<float>(_size.height()) * _aspect_ratio.get())
         ));
     }
 }
