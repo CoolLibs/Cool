f59387422e2007346b16e0dba16cce8d71d3534e
Jules Fouchy <jules.fouchy@ntymail.com>
2024-04-11 00:42:11 +0200

    🐛 [WebGPU] FullscreenPipeline: Fix setting bool and mat3 uniforms

--- src/Cool/WebGPU/FullscreenPipeline.h
+++ src/Cool/WebGPU/FullscreenPipeline.h
@@ -5,6 +5,7 @@
 #include "Cool/WebGPU/Buffer.h"
 #include "Cool/WebGPU/ShaderModule.h"
 #include "Cool/WebGPU/WGPUUnique.h"
+#include "glm/glm.hpp"
 
 namespace Cool {
 
@@ -18,9 +19,27 @@ public:
     template<typename T>
     void set_uniform_with_offset(uint64_t offset, T const& value) const
     {
-        webgpu_context().queue.writeBuffer(_uniforms_buffer_fragment_shader, offset, &value, sizeof(value));
+        if constexpr (std::is_same_v<T, bool>)
+        {
+            do_set_uniform_with_offset(offset, value ? 1 : 0); // Set bools as ints instead of bools, otherwise this crashes because WebGPU disallows bool uniforms.
+        }
+        else if constexpr (std::is_same_v<T, glm::mat3>)
+        {
+            do_set_uniform_with_offset(offset, glm::mat3x4{value}); // Needed for alignment reasons
+        }
+        else
+        {
+            do_set_uniform_with_offset(offset, value);
+        }
     }
     void set_aspect_ratio_uniform(float aspect_ratio) const; // TODO(WebGPU) Remove? Get aspect ratio automatically from the render target?
+
+private:
+    template<typename T>
+    void do_set_uniform_with_offset(uint64_t offset, T const& value) const
+    {
+        webgpu_context().queue.writeBuffer(_uniforms_buffer_fragment_shader, offset, &value, sizeof(value));
+    }
 
 private:
     Buffer    _uniforms_buffer_fragment_shader;
