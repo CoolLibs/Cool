70c5ec0938e7b2c4134a11990da8e802f8126344
Jules Fouchy <jules.fouchy@ntymail.com>
2024-05-30 18:15:03 +0200

    🤏 [VideoExporter] Don't show both Time and Frame units at the same time

--- src/Cool/Exporter/VideoExportParams.cpp
+++ src/Cool/Exporter/VideoExportParams.cpp
@@ -1,7 +1,8 @@
 #include "VideoExportParams.h"
-#include <Cool/ImGui/ImGuiExtras.h>
 #include <imgui.h>
 #include <cstdint>
+#include "Cool/ImGui/IcoMoonCodepoints.h"
+#include "Cool/ImGui/ImGuiExtras.h"
 #include "internal/origin_of_frames.h"
 
 namespace Cool {
@@ -22,36 +23,45 @@ void VideoExportParams::imgui()
     ImGui::SameLine();
     ImGui::InputScalar("##FPS", ImGuiDataType_Double, &fps, nullptr, nullptr, "%.2f fps");
 
-    // TODO(Time) Do two tabs (or a button-icon to switch the mode?), one for seconds and other for frames. To reduce confusion and indicate users that they need to input as one or the other.
-    ImGui::PushItemWidth(0.4f * (ImGui::GetContentRegionAvail().x - ImGui::CalcTextSize("Fromto").x));
-    ImGui::Text("From");
-    ImGui::SameLine();
-    imgui_drag_time("##beginning", &beginning);
-    ImGui::SameLine();
-    ImGui::Text("to");
-    ImGui::SameLine();
-    imgui_drag_time("##end", &end);
-    ImGui::PopItemWidth();
-
-    ImGui::PushItemWidth(75);
-    ImGui::Text("From");
-    ImGui::SameLine();
+    static bool input_as_time_instead_of_frames{true}; // A static is what we want here, this is a global option that the user can choose and that should be shared across all widgets.
+    if (input_as_time_instead_of_frames)
     {
-        int64_t beginning_frame = time_to_frames(beginning, fps);
-        if (ImGui::InputScalar("##beginning2", ImGuiDataType_S64, &beginning_frame))
-            beginning = frames_to_time(beginning_frame, fps);
+        ImGui::PushItemWidth(0.4f * (ImGui::GetContentRegionAvail().x - ImGui::CalcTextSize("Fromto").x));
+        ImGui::Text("From");
+        ImGui::SameLine();
+        imgui_drag_time("##beginning", &beginning);
+        ImGui::SameLine();
+        ImGui::Text("to");
+        ImGui::SameLine();
+        imgui_drag_time("##end", &end);
+        ImGui::PopItemWidth();
+    }
+    else
+    {
+        ImGui::PushItemWidth(ImGui::CalcTextSize(std::to_string(origin_of_frames).c_str()).x + 2.f * ImGui::GetStyle().FramePadding.x);
+        ImGui::Text("From");
+        ImGui::SameLine();
+        {
+            int64_t beginning_frame = time_to_frames(beginning, fps);
+            if (ImGui::InputScalar("##beginning2", ImGuiDataType_S64, &beginning_frame))
+                beginning = frames_to_time(beginning_frame, fps);
+        }
+        ImGui::SameLine();
+        ImGui::Text("to");
+        ImGui::SameLine();
+        {
+            int64_t end_frame = time_to_frames(end, fps);
+            if (ImGui::InputScalar("##end2", ImGuiDataType_S64, &end_frame))
+                end = frames_to_time(end_frame, fps);
+        }
+        ImGui::SameLine();
+        ImGui::Text("frames");
+        ImGui::PopItemWidth();
     }
     ImGui::SameLine();
-    ImGui::Text("to");
-    ImGui::SameLine();
-    {
-        int64_t end_frame = time_to_frames(end, fps);
-        if (ImGui::InputScalar("##end2", ImGuiDataType_S64, &end_frame))
-            end = frames_to_time(end_frame, fps);
-    }
-    ImGui::SameLine();
-    ImGui::Text("frames");
-    ImGui::PopItemWidth();
+    if (ImGuiExtras::button_with_text_icon(ICOMOON_STACK))
+        input_as_time_instead_of_frames = !input_as_time_instead_of_frames;
+    ImGui::SetItemTooltip("Switch units (Time or Frames)");
 }
 
 } // namespace Cool
