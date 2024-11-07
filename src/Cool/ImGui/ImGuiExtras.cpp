dde6c932e4a33a2c3b3bd938ca70d053dac3515a
Jules Fouchy <jules.fouchy@ntymail.com>
2024-11-07 22:02:53 +0100

    🧼 [ImGuiExtras::toggle] Cleanup

--- src/Cool/ImGui/ImGuiExtras.cpp
+++ src/Cool/ImGui/ImGuiExtras.cpp
@@ -718,9 +718,7 @@ auto toggle(const char* label, bool* v) -> bool
     const ImRect total_bb(pos, pos + ImVec2(width + (label_size.x > 0.0f ? style.ItemInnerSpacing.x + label_size.x : 0.0f), label_size.y + style.FramePadding.y * 2.0f));
     ImGui::ItemSize(total_bb, style.FramePadding.y);
     if (!ImGui::ItemAdd(total_bb, id))
-    {
         return false;
-    }
 
     bool       hovered, held; // NOLINT
     const bool pressed = ImGui::ButtonBehavior(total_bb, id, &hovered, &held, ImGuiButtonFlags_PressedOnClick);
@@ -733,27 +731,26 @@ auto toggle(const char* label, bool* v) -> bool
     const ImRect check_bb(pos, pos + ImVec2(width, height));
     ImGui::RenderNavHighlight(total_bb, id);
 
-    const float radius = height * 0.50f;
-    float       t      = *v ? 1.0f : 0.0f;
-    if (g.LastActiveId == id) // && g.LastActiveIdTimer < ANIM_SPEED)
+    const float radius = height * 0.5f;
+    float       t      = *v ? 1.f : 0.f;
+
+    static constexpr float ANIM_SPEED = 0.08f;
+    if (g.LastActiveId == id && g.LastActiveIdTimer < ANIM_SPEED)
     {
-        static constexpr float ANIM_SPEED = 0.08f;
-        float                  t_anim     = ImSaturate(g.LastActiveIdTimer / ANIM_SPEED);
-        t                                 = *v ? (t_anim) : (1.0f - t_anim);
+        float const t_anim = ImSaturate(g.LastActiveIdTimer / ANIM_SPEED);
+        t                  = *v ? t_anim : (1.f - t_anim);
     }
 
-    ImU32 col_bg;
-    if (ImGui::IsItemHovered())
-        col_bg = ImGui::GetColorU32(ImLerp(GetStyle().toggle_bg_hovered, GetStyle().toggle_hovered, t));
-    else
-        col_bg = ImGui::GetColorU32(ImLerp(GetStyle().toggle_bg, GetStyle().toggle, t));
+    ImU32 const col_bg = ImGui::IsItemHovered()
+                             ? ImGui::GetColorU32(ImLerp(GetStyle().toggle_bg_hovered, GetStyle().toggle_hovered, t))
+                             : ImGui::GetColorU32(ImLerp(GetStyle().toggle_bg, GetStyle().toggle, t));
 
-    ImDrawList* draw_list = ImGui::GetWindowDrawList();
+    ImDrawList* const draw_list = ImGui::GetWindowDrawList();
     draw_list->AddRectFilled(p, ImVec2(p.x + width, p.y + height), col_bg, height * 0.5f);
     draw_list->AddCircleFilled(ImVec2(p.x + radius + t * (width - radius * 2.0f), p.y + radius), radius, ImGui::GetColorU32(hovered ? GetStyle().toggle_hovered : GetStyle().toggle));
     draw_list->AddCircleFilled(ImVec2(p.x + radius + t * (width - radius * 2.0f), p.y + radius), radius - 2.5f, ImGui::GetColorU32(hovered ? GetStyle().toggle_bg_hovered : GetStyle().toggle_bg));
 
-    ImVec2 label_pos = ImVec2(check_bb.Max.x + style.ItemInnerSpacing.x, check_bb.Min.y + style.FramePadding.y);
+    ImVec2 const label_pos = ImVec2(check_bb.Max.x + style.ItemInnerSpacing.x, check_bb.Min.y + style.FramePadding.y);
     if (label_size.x > 0.0f)
         ImGui::RenderText(label_pos, label);
 
