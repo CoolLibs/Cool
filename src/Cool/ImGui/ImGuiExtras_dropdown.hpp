b4abc601cc8265e26a95226bf230ed086cb09806
Jules Fouchy <jules.fouchy@ntymail.com>
2024-12-15 17:23:06 +0100

    🤏 [ImGuiExtras_dropdown] Can now scroll inside the dropdown too

--- src/Cool/ImGui/ImGuiExtras_dropdown.hpp
+++ src/Cool/ImGui/ImGuiExtras_dropdown.hpp
@@ -7,6 +7,25 @@ auto dropdown(const char* label, const char* preview, std::vector<DropdownEntry>
 {
     bool b{false};
 
+    auto const check_for_scroll_on_item = [&]() {
+        if (ImGui::IsItemHovered() && ImGui::GetIO().MouseWheel != 0.f)
+        {
+            bool const find_bigger = ImGui::GetIO().MouseWheel > 0.f;
+            int const  sz          = static_cast<int>(entries.size());
+            bool       select_next{false};
+            for (int i = find_bigger ? sz - 1 : 0; find_bigger ? (i >= 0) : (i < sz); i += find_bigger ? -1 : 1)
+            {
+                if (select_next)
+                {
+                    entries[i].apply_value();
+                    b = true;
+                    break;
+                }
+                select_next = entries[i].is_selected();
+            }
+        }
+    };
+
     if (ImGui::BeginCombo(label, preview))
     {
         for (auto& entry : entries)
@@ -19,26 +38,11 @@ auto dropdown(const char* label, const char* preview, std::vector<DropdownEntry>
             }
             if (selected)
                 ImGui::SetItemDefaultFocus();
+            check_for_scroll_on_item();
         }
         ImGui::EndCombo();
     }
-
-    if (ImGui::IsItemHovered() && ImGui::GetIO().MouseWheel != 0.f)
-    {
-        bool const find_bigger = ImGui::GetIO().MouseWheel > 0.f;
-        int const  sz          = static_cast<int>(entries.size());
-        bool       select_next{false};
-        for (int i = find_bigger ? sz - 1 : 0; find_bigger ? (i >= 0) : (i < sz); i += find_bigger ? -1 : 1)
-        {
-            if (select_next)
-            {
-                entries[i].apply_value();
-                b = true;
-                break;
-            }
-            select_next = entries[i].is_selected();
-        }
-    }
+    check_for_scroll_on_item();
 
     return b;
 }
